4468dd8a5f67407138ad1865d9c5e0420ac9a8ce
Julian Kornberger <juliank@tzi.de>
2012-03-08 13:03:59 +0100

    stop auto-removal of flash messages on mouse over

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,11 +1,30 @@
 $(function(){
-  $(".flash").each(function(){
+  var messages = $(".flash")
+  var timeout = null;
+  
+  var close = function(message){
+    message.fadeOut();
+  }
+  
+  messages.hover(function(){
+    if(timeout){
+      window.clearTimeout(timeout);
+      timeout = null;
+      
+      $(this).append("<button>×</button>").click(function(){
+        this.disabled = true;
+        close($(this).closest(".flash"));
+      })
+    }
+  })
+  
+  messages.each(function(){
     var flash = $(this).closest(".flash");
     var words = flash.text().split(" ").length;
     var delay = words < 20 ? 3 : words/4;
     
-    window.setTimeout(function(){
-      flash.fadeOut();
+    timeout = window.setTimeout(function(){
+      close(flash);
     }, delay * 1000)
   });
 })
