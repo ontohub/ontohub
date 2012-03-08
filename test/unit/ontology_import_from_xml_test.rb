1a655c5d06cbb80dc2759e148d15e38432907f9c
henning mueller <henning@orgizm.net>
2012-03-08 13:55:09 +0100

    OntologyImportFromXMLTest Entity and Axiom count.

--- test/unit/ontology_import_from_xml_test.rb
+++ test/unit/ontology_import_from_xml_test.rb
@@ -14,5 +14,17 @@ class OntologyImportFromXMLTest < ActiveSupport::TestCase
     should 'save logic' do
       assert_equal 'OWL', @ontology.logic.name
     end
+
+    context 'entity count' do
+      should 'be 5' do
+        assert_equal 5, @ontology.entities.count
+      end
+    end
+
+    context 'axiom count' do
+      should 'be 1' do
+        assert_equal 1, @ontology.axioms.count
+      end
+    end
   end
 end
