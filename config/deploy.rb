0a62007d70edf7fa332d03f95e2e819d09d0e379
Julian Kornberger <juliank@tzi.de>
2012-03-08 11:47:55 +0100

    deployment works

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,10 @@
 require 'bundler/capistrano'
 
+# RVM
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.3@ontohub'
+
 hostname = 'ontohub.orgizm.net'
 
 set :application, 'ontohub'
@@ -15,3 +20,10 @@ set :deploy_via, :remote_cache
 role :app, hostname
 role :web, hostname
 role :db,  hostname, :primary => true
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
