5542668b61f7aab327d2666dbea9d9c1dea4cc99
Julian Kornberger <juliank@tzi.de>
2012-03-08 13:25:39 +0100

    [#26128523] real name can be given on sign up and edited on settings page.

--- app/models/user/authentication.rb
+++ app/models/user/authentication.rb
@@ -9,7 +9,7 @@ module User::Authentication
            :recoverable, :rememberable, :trackable, :validatable
     
     # Setup accessible (or protected) attributes for your model
-    attr_accessible :email, :password, :password_confirmation, :remember_me
+    attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   end
   
 end
