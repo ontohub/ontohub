a15df506ecd7e946d5b09574f9672cdcccb5b7e7
Julian Kornberger <juliank@tzi.de>
2012-03-09 01:52:28 +0100

    Design of counter_link changed

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -3,7 +3,7 @@ module LinkHelper
   def counter_link(url, counter, subject)
     
     text = content_tag(:strong, counter)
-    text << (counter==1 ? subject : subject.pluralize)
+    text << content_tag(:span, counter==1 ? subject : subject.pluralize)
     
     link_to text, url
   end
