c089c2e01fd049de4c25af718af1a26b8846c890
Julian Kornberger <juliank@tzi.de>
2012-03-12 18:40:54 +0100

    Authorization for Ontologies

--- test/unit/ontology_test.rb
+++ test/unit/ontology_test.rb
@@ -16,4 +16,38 @@ class OntologyTest < ActiveSupport::TestCase
       assert_equal @ontology.uri, @ontology.to_s
     end
   end
+  
+  context 'creating ontology with version' do
+    setup do
+      @user       = Factory :user
+      @source_uri = 'http://example.com/dummy.ontology'
+      @ontology   = Ontology.new \
+        :uri => 'fooo',
+        :versions_attributes => [{
+          source_uri: @source_uri
+        }]
+        
+      @ontology.versions.first.user = @user
+      @ontology.save!
+    end
+    
+    should 'create a version with source_uri' do
+      assert_equal @source_uri, @ontology.versions.first.source_uri
+    end
+    
+    context 'creating a permission' do
+      setup do
+        assert_not_nil @permission = @ontology.permissions.first
+      end
+      
+      should 'with subject' do
+        assert_equal @user, @permission.subject
+      end
+      
+      should 'with role owner' do
+        assert_equal 'owner', @permission.role
+      end
+    end
+    
+  end
 end
