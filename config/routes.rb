9830ae633a558bbee385ff3c9d34f54e7f6be6ab
Julian Kornberger <juliank@tzi.de>
2012-03-12 13:10:27 +0100

    prevent destroying the last admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Ontohub::Application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "users/registrations" }
   
   namespace :admin do
     resources :teams, :only => :index
