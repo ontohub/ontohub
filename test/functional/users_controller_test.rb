670bf306e8cf030775bccde8876f46da47ebdc9c
Julian Kornberger <juliank@tzi.de>
2012-03-13 17:35:17 +0100

    Display latest comments on user page

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -9,14 +9,36 @@ class UsersControllerTest < ActionController::TestCase
       @user = Factory :user
     end
     
-    context 'on GET to show' do
-      setup do
-        get :show, :id => @user.to_param
+    context 'without data' do
+      context 'on GET to show' do
+        setup do
+          get :show, :id => @user.to_param
+        end
+        
+        should assign_to :versions
+        should assign_to :comments
+        should respond_with :success
+        should render_template :show
       end
-      
-      should assign_to :versions
-      should respond_with :success
-      should render_template :show
+    end
+    
+    context 'with data' do
+      setup do
+        OntologyVersion.any_instance.expects(:parse_async).once
+        
+        Factory :comment, :user => @user
+        Factory :ontology_version, :user => @user
+      end
+      context 'on GET to show' do
+        setup do
+          get :show, :id => @user.to_param
+        end
+        
+        should assign_to :versions
+        should assign_to :comments
+        should respond_with :success
+        should render_template :show
+      end
     end
   end
   
