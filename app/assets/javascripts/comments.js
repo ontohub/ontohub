1002ccb3f8b4b22f8a246da1352755d94ff36905
Julian Kornberger <juliank@tzi.de>
2012-03-13 18:40:13 +0100

    [#26133607] Creating comments fixed again

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,22 +1,22 @@
 $(function(){
   var comments = $(".comments");
   
-  comments.find("form")
   // Comment created successfully
-  .on("ajax:success", function(xhr, data){
+  comments.on("ajax:success", "form", function(xhr, data){
     var form = $(this)
     var commentsListing = form.closest(".comments").children("ol");
     
     $(data).appendTo(commentsListing).hide().fadeIn('slow');
     form.html("Thanks for your comment.");
   })
+  
   // Comment NOT created, render the returned form
-  .on("ajax:error", function(xhr, status, error){
+  comments.on("ajax:error", "form", function(xhr, status, error){
     $(this).replaceWith(status.responseText);
   })
   
   // Comment deleted
-  comments.find("ol").on("ajax:success", "a[data-method=delete]", function(){
+  comments.on("ajax:success", "a[data-method=delete]", function(){
     var li = $(this).closest(".actions").closest("li");
     li.fadeOut(function(){
       li.remove();
