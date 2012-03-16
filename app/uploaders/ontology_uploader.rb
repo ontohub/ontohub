2a7b71df508b99944b9c1eb62534d840c4994171
Julian Kornberger <juliank@tzi.de>
2012-03-16 20:13:44 +0100

    seperate upload directories for tests

--- app/uploaders/ontology_uploader.rb
+++ app/uploaders/ontology_uploader.rb
@@ -1,8 +1,10 @@
 class OntologyUploader < CarrierWave::Uploader::Base
   storage :file
-
+  
+  UPLOADS_DIR = Rails.env.test? ? "#{Rails.root}/tmp/uploads" : "uploads"
+  
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{UPLOADS_DIR}/#{model.ontology_id}/#{model.id}/#{mounted_as}"
   end
 
   # XML is white-listed here to avoid creating another Uploader.
