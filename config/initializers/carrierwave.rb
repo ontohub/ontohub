30ca5f571602ca00f4321399eaedd497f63082b7
Julian Kornberger <juliank@tzi.de>
2012-03-17 19:48:46 +0100

    [#26561443] add .owl to remote filename if extension is missing or unknown

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,19 @@
 require 'open-uri'
 
 # HACK HACK HACK
-# monkey patching carrier-wave to add an additional header
 class CarrierWave::Uploader::Download::RemoteFile
+  
+  def original_filename
+    name  = File.basename(file.base_uri.path)
+    # add .owl if file extension is missing or unknown
+    name += ".owl" unless name =~ /(owl|clf|clif|xml)$/
+    name
+  end
   
   private
   
   def file
+    # monkey patching carrier-wave to add an additional header
     if @file.blank?
       @file = Kernel.open(@uri.to_s, 'Accept' => 'application/rdf+xml,*/*;q=0.9')
       @file = @file.is_a?(String) ? StringIO.new(@file) : @file
