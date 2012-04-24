860ccc55684a5cbfed050ec17c6d9c145a8a9369
Julian Kornberger <juliank@tzi.de>
2012-04-24 15:37:18 +0200

    Navigation, Tooltips, Pagination and Tables styled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,20 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
+// ----------------------------------------------
+// jQuery
+
 //= require jquery
 //= require jquery.ui.autocomplete
 //= require jquery.ui.progressbar
-//= require jquery.ui.tabs
 //= require jquery.effects.fade
 //= require jquery.effects.highlight
 //= require jquery_ujs
+
+// ----------------------------------------------
+// Bootstrap
+//= require bootstrap-alert
+//= require bootstrap-dropdown
+//= require bootstrap-modal
+//= require bootstrap-tooltip
+
+// ----------------------------------------------
+// Application
 //= require_tree .
