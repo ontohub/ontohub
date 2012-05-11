9812e3e030e88a5b0f69f5b88713bd97f3da34dc
Daniel Vale <danielvale@gmail.com>
2012-05-11 17:36:37 +0200

    Correct indentation

--- db/migrate/20120424155621_create_language_mappings.rb
+++ db/migrate/20120424155621_create_language_mappings.rb
@@ -3,17 +3,17 @@ class CreateLanguageMappings < ActiveRecord::Migration
     create_table :language_mappings do |t|
       t.references :source, :null => false
       t.references :target, :null => false
-	  t.string :iri
+      t.string :iri
       t.string :kind
 
       t.timestamps :null => false
     end
-	
-	change_table :language_mappings do |t|
-	  t.index :source_id
-	  t.index :target_id
-	  t.foreign_key :languages, :column => :source_id, :dependent => :delete
-	  t.foreign_key :languages, :column => :target_id, :dependent => :delete
+    
+    change_table :language_mappings do |t|
+      t.index :source_id
+      t.index :target_id
+      t.foreign_key :languages, :column => :source_id, :dependent => :delete
+      t.foreign_key :languages, :column => :target_id, :dependent => :delete
     end
   end
 end
