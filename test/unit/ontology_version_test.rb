e31e8ef79bbd16ef2f2e11046a4360b48359280d
Daniel Vale <danielvale@gmail.com>
2012-05-29 14:55:32 +0200

    Update migrations to fulfill test expectations Update test expectations

--- test/unit/ontology_version_test.rb
+++ test/unit/ontology_version_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class OntologyVersionTest < ActiveSupport::TestCase
-  should have_db_index(:ontology_id)
+  should have_db_index([:ontology_id, :number])
   should have_db_index(:user_id)
   should have_db_index(:logic_id)
+  should have_db_index(:previous_version_id)
+  should have_db_index(:checksum)
 
   should have_many :entities
   should have_many :sentences
@@ -94,7 +96,7 @@ class OntologyVersionTest < ActiveSupport::TestCase
 
     should 'raise no exception if called with remote uri' do
       assert_nothing_raised do
-        @ontology_version.remote_raw_file_url = 'http://colore.googlecode.com/svn/trunk/ontologies/arithmetic/robinson_arithmetic.clif'
+        @ontology_version.remote_raw_file_url = 'http://colore.googlecode.com/svn-history/r679/trunk/ontologies/arithmetic/robinson_arithmetic.clif'
         @ontology_version.save!
         @ontology_version.parse
       end
@@ -102,7 +104,7 @@ class OntologyVersionTest < ActiveSupport::TestCase
 
     should 'raise exception if called with unsupported remote file' do
       assert_raise Hets::HetsError do
-        @ontology_version.remote_raw_file_url = 'http://colore.googlecode.com/svn/trunk/ontologies/algebra/module.clif'
+        @ontology_version.remote_raw_file_url = 'http://colore.googlecode.com/svn-history/r679/trunk/ontologies/algebra/module.clif'
         @ontology_version.save!
         @ontology_version.parse
       end
