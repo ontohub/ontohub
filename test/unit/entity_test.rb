bbee46f9d9cb1a0bbbf0bf5f8aeb9630b6e528e2
Daniel Couto Vale <danielvale@uni-bremen.de>
2012-08-14 13:09:21 +0200

    Correct migration tests of entities and sentences
    Add migration tests to entities

--- test/unit/entity_test.rb
+++ test/unit/entity_test.rb
@@ -1,24 +1,30 @@
 require 'test_helper'
 
 class EntityTest < ActiveSupport::TestCase
+
+  context 'Migrations' do
+    %w( ontology_id comments_count ).each do |column|
+      should have_db_column(column).of_type(:integer)
+    end
   
-  %w( ontology_version_id comments_count ).each do |column|
-    should have_db_column(column).of_type(:integer)
-  end
-  
-  %w( kind name uri range ).each do |column|
-    should have_db_column(column).of_type(:string)
+    %w( kind name iri range ).each do |column|
+      should have_db_column(column).of_type(:string)
+    end
+
+    should have_db_column('text').of_type(:text)
+
+    should have_db_index([:ontology_id, :text]).unique(true)
+    should have_db_index([:ontology_id, :kind])
   end
 
-  should have_db_index([:ontology_version_id, :text]).unique(true)
-  should have_db_index([:ontology_version_id, :kind])
-  
-  should belong_to :ontology_version
-  should have_and_belong_to_many :sentences
+  context 'Associations' do
+    should belong_to :ontology
+    should have_and_belong_to_many :sentences
+  end
 
-  context 'OntologyVersionInstance' do
+  context 'OntologyInstance' do
   	setup do
-  		@ontology_version = Factory :ontology_version
+  		@ontology = Factory :single_ontology
   	end
 
   	context 'creating Entities' do
@@ -30,12 +36,12 @@ class EntityTest < ActiveSupport::TestCase
 	  			'text' => 'nat'
 	  		}
 
-	      @ontology_version.entities.update_or_create_from_hash @entity_hash
+	      @ontology.entities.update_or_create_from_hash @entity_hash
   		end
 
   		context 'correct attribute' do
   			setup do
-  				@entity = @ontology_version.entities.first
+  				@entity = @ontology.entities.first
   			end
 
   			%w[name range kind text].each do |attr|
