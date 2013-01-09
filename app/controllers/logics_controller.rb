3931010d4e211e93afe24111906bfe7065fc9c6d
Christian Clausen <cclausen@tzi.de>
2013-01-09 11:33:25 +0100

    logics-controller

---
+++ app/controllers/logics_controller.rb
@@ -0,0 +1,26 @@
+# 
+# Controller for ontologies
+# 
+class LogicsController < InheritedResources::Base
+
+  respond_to :json, :xml
+  has_pagination
+  has_scope :search
+
+  load_and_authorize_resource :except => [:index, :show]
+
+  def index
+    super do |format|
+      format.html do
+        @search = params[:search]
+        @search = nil if @search.blank?
+      end
+    end
+  end
+
+  def create
+    @version.user = current_user
+    super
+  end
+
+end
