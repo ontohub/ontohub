bdebc23d24db99c417a1bb228705d8dce93bfe7d
Daniel Couto Vale <danielvale@uni-bremen.de>
2013-01-15 19:17:25 +0100

    Add sample logics to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,6 +32,68 @@ team = Team.create! \
   team.users << user if i < 2
 end
 
+# Logic Section
+#
+# Author: Daniel Couto Vale <danielvale@uni-bremen.de>
+# Source: The Distributed Otology Language (DOL): use cases, syntax and extensibility
+#
+# Logics:
+# * Common Logic
+# * SROIQ
+# * DL-RL
+# * DL-Lite.g
+# * EL++
+# * RDFS
+# * RDF
+logic = Logic.new({
+  :name        => "CL",
+  :iri         => "http://ontohub.org/cl", 
+  :description => "Common logic"
+})
+logic.save!
+
+logic = Logic.new({
+  :name        => "SROIQ",
+  :iri         => "http://ontohub.org/sroiq", 
+  :description => "SROIQ"
+})
+logic.save!
+
+logic = Logic.new({
+  :name        => "DL-RL",
+  :iri         => "http://ontohub.org/dl-rl", 
+  :description => "DL-RL is a sublogic of description logic whose reasoning is done by rule-based techlonogies and inspired by Description Logic Programs DLP and pD*."
+})
+logic.save!
+
+logic = Logic.new({
+  :name        => "DL-Lite.g",
+  :iri         => "http://ontohub.org/dl-lite.g", 
+  :description => "DL-Lite.g"
+})
+logic.save!
+
+logic = Logic.new({
+  :name        => "EL++",
+  :iri         => "http://ontohub.org/el++", 
+  :description => "DL-EL"
+})
+logic.save!
+
+logic = Logic.new({
+  :name        => "RDFS",
+  :iri         => "http://ontohub.org/rdfs", 
+  :description => "RDFS"
+})
+logic.save!
+
+logic = Logic.new({
+  :name        => "RDF",
+  :iri         => "http://ontohub.org/rdf", 
+  :description => "RDF"
+})
+logic.save!
+
 # Import ontologies
 Dir["#{Rails.root}/test/fixtures/ontologies/*/*.{casl,clf,clif,owl}"].each do |file|
   basename = File.basename(file)
