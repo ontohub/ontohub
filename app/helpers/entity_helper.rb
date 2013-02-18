f0135e8fb7e55408056f1d72cb9e42ad13b5e8cc
Timo Kohorst <timo-hb@arcor.de>
2013-02-18 21:29:20 +0100

    refined regex

--- app/helpers/entity_helper.rb
+++ app/helpers/entity_helper.rb
@@ -1,5 +1,5 @@
 module EntityHelper
   def name_highlighter(entity)
-    h(entity.text).gsub(Regexp.new(entity.name), (content_tag(:strong,entity.name))).html_safe
+    h(entity.text).gsub(/#{entity.name}/, (content_tag(:strong,entity.name))).html_safe
   end
 end
