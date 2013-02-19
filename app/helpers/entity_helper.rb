26ec2953523d3e2ae3b657ebdc6cb6b924d4327b
Timo Kohorst <timo-hb@arcor.de>
2013-02-19 14:54:23 +0100

    highlight only standalone occurrences of name in text

--- app/helpers/entity_helper.rb
+++ app/helpers/entity_helper.rb
@@ -1,5 +1,5 @@
 module EntityHelper
   def name_highlighter(entity)
-    h(entity.text).gsub(/#{entity.name}/, (content_tag(:strong,entity.name))).html_safe
+    h(entity.text).gsub(/\b#{entity.name}\b/, (content_tag(:strong,entity.name))).html_safe
   end
 end
