d8b7df2cba9b3621f0464cdd2b07ed255ae590e0
henning mueller <henning@orgizm.net>
2013-03-21 17:30:06 +0100

    Exclude failed from ontologies#index.

--- app/models/ontology.rb
+++ app/models/ontology.rb
@@ -26,7 +26,7 @@ class Ontology < ActiveRecord::Base
   strip_attributes :only => [:name, :iri]
 
   scope :search, ->(query) { where "iri #{connection.ilike_operator} :term OR name #{connection.ilike_operator} :term", :term => "%" << query << "%" }
-  scope :desc, order("ontologies.entities_count DESC")
+  scope :done_sorted_by_entities_count, where(state: 'done').order('ontologies.entities_count desc')
 
   def to_s
     name? ? name : iri
