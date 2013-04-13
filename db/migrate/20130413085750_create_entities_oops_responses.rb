782fa71d979cf4f481ec791093b57a29504b7487
henning mueller <henning@orgizm.net>
2013-04-13 14:20:27 +0200

    It has to be entites_oor_responses as table name!

--- db/migrate/20130413085750_create_entities_oops_responses.rb
+++ db/migrate/20130413085750_create_entities_oops_responses.rb
@@ -1,11 +1,11 @@
-class CreateOopsResponsesEntities < ActiveRecord::Migration
+class CreateEntitiesOopsResponses < ActiveRecord::Migration
   def change
-    create_table :oops_responses_entities do |t|
+    create_table :entities_oops_responses do |t|
       t.references :oops_response, null: false
       t.references :entity, null: false
     end
 
-    change_table :oops_responses_entities do |t|
+    change_table :entities_oops_responses do |t|
       t.index [:oops_response_id, :entity_id]
       t.foreign_key :oops_responses, dependent: :delete
       t.foreign_key :entities
