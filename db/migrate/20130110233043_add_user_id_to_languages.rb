753bf89d56968fe2466826b97cfcb967f3ca4752
henning mueller <henning@orgizm.net>
2013-05-16 18:29:25 +0200

    Fix worked and has to be used for any migration which adds an user_id column.
    Conflicts:
    	db/migrate/20130109104804_add_user_id_to_logics.rb

--- db/migrate/20130110233043_add_user_id_to_languages.rb
+++ db/migrate/20130110233043_add_user_id_to_languages.rb
@@ -1,6 +1,6 @@
 class AddUserIdToLanguages < ActiveRecord::Migration
   def change
-    add_column :languages, :user_id, :integer, :null => false
+    add_column :languages, :user_id, :integer, null: false, default: 1
     add_foreign_key :languages, :users
   end
 end
