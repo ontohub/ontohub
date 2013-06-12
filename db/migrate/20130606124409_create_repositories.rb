bfb6952df365e5b65f90e3c79bf1faee73fcd85b
Julian Kornberger <juliank@tzi.de>
2013-06-12 10:48:34 +0200

    Foreign key for repository_id in combination with parent_id added.

--- db/migrate/20130606124409_create_repositories.rb
+++ db/migrate/20130606124409_create_repositories.rb
@@ -14,5 +14,10 @@ class CreateRepositories < ActiveRecord::Migration
 
     add_column      :ontologies, :repository_id, :integer, null: false
     add_foreign_key :ontologies, :repositories
+
+    add_index :ontologies, [:repository_id, :id], unique: true
+    execute "ALTER TABLE ontologies
+             ADD FOREIGN KEY (parent_id,repository_id)
+             REFERENCES ontologies (id, repository_id)"
   end
 end
