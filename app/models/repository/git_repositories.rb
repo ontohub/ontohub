740c4a0f3566245bc3ee97259d8c7cf0c7ec25c8
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-06-27 07:21:42 -0700

    add save_file method

--- app/models/repository/git_repositories.rb
+++ app/models/repository/git_repositories.rb
@@ -19,26 +19,27 @@ module Repository::GitRepositories
   end
 
   def save_file(tmp_file, filepath, message, user)
-    commit_oid = git.add_file({email: user[:email], name: user[:name]}, tmp_file, filepath, message)
-    if(git.path_exists?(nil, filepath))
-      clazz = filepath.ends_with?('.casl') ? DistributedOntology : SingleOntology
+    commit_oid = git.add_file({email: user[:email], name: user[:name]}, tmp_file, filepath, message) do
+      if(git.path_exists?(nil, filepath))
+        clazz = filepath.ends_with?('.casl') ? DistributedOntology : SingleOntology
 
-      o = clazz.new \
-        iri:         "http://#{Settings.hostname}/#{path}/#{Ontology.filename_without_extension(filepath)}",
-        name:        filepath.split('/')[-1].split(".")[0].capitalize,
-        description: ''
+        o = clazz.new \
+          iri:         "http://#{Settings.hostname}/#{path}/#{Ontology.filename_without_extension(filepath)}",
+          name:        filepath.split('/')[-1].split(".")[0].capitalize,
+          description: ''
 
-      v = o.versions.build({ :commit_oid => commit_oid, :user => user }, { without_protection: true })
+        v = o.versions.build({ :commit_oid => commit_oid, :user => user }, { without_protection: true })
 
-      o.repository = self
-      o.save!
-      o.ontology_version = v;
-      o.save!
-    else
-      o = ontologies.find_by_path(filepath)
-      v = o.versions.build({ :commit_oid => commit_oid, :user => user }, { without_protection: true })
-      o.ontology_version = v
-      o.save!
+        o.repository = self
+        o.save!
+        o.ontology_version = v;
+        o.save!
+      else
+        o = ontologies.find_by_path(filepath)
+        v = o.versions.build({ :commit_oid => commit_oid, :user => user }, { without_protection: true })
+        o.ontology_version = v
+        o.save!
+      end
     end
     touch
   end
