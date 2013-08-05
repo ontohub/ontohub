576cc431afb29947271c5028acbdbcbe206271ed
Sascha Graef <snow-wolf@luna-wolf.de>
2013-08-05 13:24:57 +0200

    prerpare links for entityMappings

--- app/models/entity_mapping.rb
+++ app/models/entity_mapping.rb
@@ -1,6 +1,6 @@
 class EntityMapping < ActiveRecord::Base
-  belongs_to :source
-  belongs_to :target
+  belongs_to :source, class_name: "Entitiy"
+  belongs_to :target, class_name: "Entitiy"
   belongs_to :link
   
   KINDS = %w( subsumes is-subsumed equivalent incompatible has-instance instance-of default-relation )
