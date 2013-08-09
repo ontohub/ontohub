bffd3eeabb50d2fa50e7aa0191c93cfe7e9a898a
Tim Reddehase <robustus@rightsrestricted.com>
2013-08-09 16:25:44 +0200

    adjust graphs controller tests for empty results
    - Test for return of valid response when calling
      graphs on logic/ontology without mappings/links
    - Remove obsolete user portion (factory takes
      care of that).

--- test/functional/graphs_controller_test.rb
+++ test/functional/graphs_controller_test.rb
@@ -5,13 +5,11 @@ class GraphsControllerTest < ActionController::TestCase
   should route(:get, "/logics/id/graphs").to(:controller=> :graphs, :action => :index, :logic_id => 'id')
 
   setup do
-    @user = FactoryGirl.create :user
-    @source = FactoryGirl.create(:logic, user: @user)
-    @target = FactoryGirl.create(:logic, user: @user)
+    @source = FactoryGirl.create(:logic)
+    @target = FactoryGirl.create(:logic)
     @mapping = FactoryGirl.create(:logic_mapping,
                                   source: @source,
-                                  target: @target,
-                                  user: @user)
+                                  target: @target)
   end
 
   context 'on GET to index' do
@@ -25,6 +23,14 @@ class GraphsControllerTest < ActionController::TestCase
     context 'valid on json-request' do
       setup do
         get :index, logic_id: @source, format: :json
+      end
+
+      should respond_with :success
+    end
+    context 'valid response when there are no mappings/links' do
+      setup do
+        @unlinked_source = FactoryGirl.create(:logic)
+        get :index, logic_id: @unlinked_source, format: :json
       end
 
       should respond_with :success
