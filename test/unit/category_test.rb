07c5ddde61bc219436226779bd7a5cd7aa469fb8
Timo Kohorst <timo-hb@arcor.de>
2013-08-13 18:27:13 +0200

    test creation of categories

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -6,4 +6,36 @@ class CategoryTest < ActiveSupport::TestCase
     should have_db_column('name').of_type(:string)
   end
 
+  context 'extracting names from sentences' do
+    setup do 
+      @ontology = FactoryGirl.create :ontology
+      @ontology.entities.push(FactoryGirl.create :entity, :kind => 'Class', :name => "Business_and_administration")
+      @ontology.entities.push(FactoryGirl.create :entity, :kind => 'Class', :name => "Accounting_and_taxation")
+      sentence = FactoryGirl.create :sentence, :text => 'Class: <https://github.com/ontohub/OOR_Ontohub_API/blob/master/Domain_fields.owl#Accounting_and_taxation>\n       SubClassOf: <https://github.com/ontohub/OOR_Ontohub_API/blob/master/Domain_fields.owl#Business_and_administration>'
+      @ontology.logic = FactoryGirl.create :logic, :name => 'OWL2', :user => (FactoryGirl.create :user)
+      sentence.ontology = @ontology
+      sentence.save!
+      @ontology.save!
+      @ontology.create_categories
+    end
+
+    should 'write fragments to database' do
+      assert_not_nil Category.find_by_name("Business_and_administration")
+      assert_not_nil Category.find_by_name("Accounting_and_taxation")
+    end
+
+    should 'have proper ancestry relationship' do
+      assert Category.find_by_name("Accounting_and_taxation").parent == Category.find_by_name("Business_and_administration")
+    end
+
+    context 'a non-OWL2 ontology' do
+      setup do
+        @ontology.logic.name = "CASL"
+      end
+      should 'raise an exception' do
+        assert_raise(Exception) { @ontology.create_categories }
+      end
+    end
+  end
+
 end
--- test/unit/sentence_test.rb
+++ test/unit/sentence_test.rb
@@ -53,12 +53,12 @@ class SentenceTest < ActiveSupport::TestCase
 
   context 'extracted names' do
     setup do
-      @sentence = FactoryGirl.create :sentence, :meta_ontology
-      c1,c2 = @sentence.extract_class_names
+      sentence = FactoryGirl.create :sentence, :meta_ontology
+      @name1,@name2 = sentence.extract_class_names
     end
     should "match iris\' fragments" do
-      assert_equal c1, 'Accounting_and_taxation'
-      assert_equal c2, 'Business_and_administration'
+      assert_equal @name1, 'Accounting_and_taxation'
+      assert_equal @name2, 'Business_and_administration'
     end
   end
 
