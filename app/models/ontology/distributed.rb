dcc3d62e5ee2d3f84edc1b0f8bbaf79e58066799
Tim Reddehase <robustus@rightsrestricted.com>
2013-08-13 18:28:46 +0200

    add code for homogeneous distributed ontology
    - add two methods to determine whether
      an ontology is heterogeneous or homogeneous.

--- app/models/ontology/distributed.rb
+++ app/models/ontology/distributed.rb
@@ -9,4 +9,16 @@ module Ontology::Distributed
     is_a? DistributedOntology
   end
 
+  def homogeneous?
+    return true unless distributed?
+    return true unless self.children.any?
+    base_logic = self.children.first.logic
+    self.children.count == self.children.where(logic_id: base_logic).count
+  end
+
+  def heterogeneous?
+    return false unless distributed?
+    not homogeneous?
+  end
+
 end
