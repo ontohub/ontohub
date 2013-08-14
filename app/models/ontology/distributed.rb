2382da8dccfccb49e8021a60e4685b9ec861874f
Tim Reddehase <robustus@rightsrestricted.com>
2013-08-14 16:59:58 +0200

    replace minimal queries with sql-based queries.

--- app/models/ontology/distributed.rb
+++ app/models/ontology/distributed.rb
@@ -5,19 +5,33 @@ module Ontology::Distributed
     acts_as_tree
 
     def self.homogeneous
-      where("ontologies.type = 'DistributedOntology'").
-        select { |ontology| ontology.homogeneous?}
+      select_with_character_selector('=')
     end
 
     def self.heterogeneous
-      where("ontologies.type = 'DistributedOntology'").
-        select { |ontology| ontology.heterogeneous?}
+      select_with_character_selector('>')
     end
 
     def self.heterogeneous_children
       ontologies = where("ontologies.type = 'DistributedOntology'").
         select { |ontology| ontology.heterogeneous?}
       where(parent_id: ontologies)
+    end
+
+    private
+    def self.select_with_character_selector(selector="=")
+      stmt = <<-STMT
+(
+SELECT distributed.id FROM
+  (SELECT distributed_count.id, COUNT(distributed_count.id) AS occurrences FROM
+    (SELECT (ontologies.parent_id) as id FROM ontologies
+    GROUP BY ontologies.logic_id, ontologies.parent_id)
+  AS distributed_count
+GROUP BY distributed_count.id) AS distributed
+WHERE distributed.occurrences #{selector} 1
+)
+      STMT
+      where(type: DistributedOntology).where("ontologies.id IN #{stmt}")
     end
 
   end
