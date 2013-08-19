0490cc31187616035919a7852319f691534bf5ef
Tim Reddehase <robustus@rightsrestricted.com>
2013-08-19 12:27:53 +0200

    add graph as own view for graphs#index action.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,18 +1,19 @@
 class GraphsController < ApplicationController
-  respond_to :json
+  respond_to :json, :html
   inherit_resources
   belongs_to :logic, :ontology, polymorphic: true
 
   def index
     respond_to do |format|
-      depth = params[:depth] ? params[:depth].to_i : 3
-      fetcher = GraphDataFetcher.new(center: parent, depth: depth)
+      @depth = params[:depth] ? params[:depth].to_i : 3
+      fetcher = GraphDataFetcher.new(center: parent, depth: @depth)
       nodes, edges = fetcher.fetch
       data = {nodes: nodes,
               edges: edges,
               center: parent,
               node_url: nodes.any? ? url_for(nodes.first.class) : nil,
               edge_url: edges.any? ? url_for(edges.first.class) : nil}
+      format.html
       format.json do
         respond_with data
       end
