c8e6256890e8de1bf26247d87934ef2676691507
Tim Reddehase <robustus@rightsrestricted.com>
2013-08-25 23:26:44 +0200

    add method to fetch logic-specific het. DOs
    - add a method to fetch the logic-specific
      heterogeneous Distributed Ontologies
    - Refactor duplicate code into own method (DRY)
    - Use "speaking" constants instead of
      seemingly arbitrary strings.

--- app/models/ontology/distributed.rb
+++ app/models/ontology/distributed.rb
@@ -5,28 +5,45 @@ module Ontology::Distributed
     acts_as_tree
 
     def self.homogeneous
-      select_with_character_selector('=')
+      select_with_character_selector(HOMOGENEOUS_SELECTOR)
     end
 
     def self.heterogeneous
-      select_with_character_selector('>')
+      select_with_character_selector(HETEROGENEOUS_SELECTOR)
     end
 
     def self.heterogeneous_children
       where(parent_id: self.heterogeneous)
     end
 
+    # Fetches homogeneous Ontologies which are distributed
+    # and have children in the logic
     def self.distributed_in(logic)
+      distributed_with(logic, HOMOGENEOUS_SELECTOR)
+    end
+
+    # Fetches heterogeneous Ontologies which are distributed
+    # and have children in the logic
+    def self.also_distributed_in(logic)
+      distributed_with(logic, HETEROGENEOUS_SELECTOR)
+    end
+
+    private
+
+    HOMOGENEOUS_SELECTOR = '='
+    HETEROGENEOUS_SELECTOR = '>'
+
+    def self.distributed_with(logic, selector='=')
       query = <<-STMT
 WITH parents AS (
   SELECT (ontologies.parent_id) AS id FROM ontologies WHERE ontologies.logic_id = #{logic.id}
 )
 SELECT * FROM ontologies JOIN parents ON ontologies.parent_id = parents.id
       STMT
-      select_with_character_selector('=', query)
+      select_with_character_selector(selector, query)
     end
 
-    private
+
     def self.select_with_character_selector(selector="=", ontologies_query=nil)
       query = ontologies_query ? "(#{ontologies_query}) AS ontologies" : "ontologies"
       stmt = <<-STMT
