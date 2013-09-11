4cc72a48a07dfc1d25564bf9dc1af4c50136dfe4
Daniel Couto Vale <danielvale@uni-bremen.de>
2013-09-11 06:38:24 -0700

    Readd consistency check table

---
+++ db/migrate/20130911000001_create_consistency_check.rb
@@ -0,0 +1,21 @@
+class CreateConsistencyCheckMethod < ActiveRecord::Migration
+  def change
+    create_table :consistency_checks do |t|
+      t.references :checker
+      t.references :logic
+      t.integer :priority_order
+      t.string :result_status
+      t.string :result_proof
+      t.string :process_status
+
+      t.timestamps
+    end
+
+    change_table :consistency_check_methods do |t|
+      t.index :checker_id
+      t.index :object_id
+      t.foreign_key :consistency_checkers, :column => :checker_id, :dependent => :delete
+      t.foreign_key :ontology_versions, :column => :object_id, :dependent => :delete
+    end
+  end
+end
