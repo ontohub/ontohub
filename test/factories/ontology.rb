babbed04e5e280d3a1288cecf55673bc37dd9ee0
Tim Reddehase <robustus@rightsrestricted.com>
2013-09-14 15:27:07 +0200

    add factory for DO with defined links.

--- test/factories/ontology.rb
+++ test/factories/ontology.rb
@@ -13,6 +13,25 @@ FactoryGirl.define do
     end
     
     factory :distributed_ontology, class: DistributedOntology do
+
+      # Should always be fully linked, so every child should
+      # have a linked (defined by the DO) pointing or sourcing
+      # to/from it.
+      factory :linked_distributed_ontology do |ontology|
+        ontology.after(:build) do |ontology|
+          logic = FactoryGirl.create(:logic)
+          child_one = FactoryGirl.create(:ontology, logic: logic)
+          child_two = FactoryGirl.create(:ontology, logic: logic)
+
+          FactoryGirl.create(:link,
+                            source: child_one,
+                            target: child_two,
+                            ontology: ontology)
+
+          ontology.children.push(child_one, child_two)
+        end
+      end
+
     end
   end
 end
