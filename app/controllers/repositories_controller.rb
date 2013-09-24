a941dc3d367d182ca2f59c0ee7a90bf935f3c260
= <=>
2013-09-24 07:46:40 -0700

    refactoring

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -17,14 +17,15 @@ class RepositoriesController < ApplicationController
     @info = @repository.path_info(params[:path], @oid)
 
     raise Repository::FileNotFoundError, @path if @info.nil?
-    if @info[:type] == :raw
+    
+    case @info[:type]
+    when :raw
       render text: @repository.read_file(@path, params[:oid]),
              content_type: Mime::Type.lookup('application/force-download')
-    end
-
-    if @info[:type] == :file_base
+    when :file_base
       @content = @repository.read_file(@info[:entry][:path], params[:oid])
+    when :file_base_ambiguous
+      @info[:entries]
     end
   end
-
 end
