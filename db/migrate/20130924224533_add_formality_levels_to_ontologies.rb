9222e2a426938210bd9cc7f98ddb881e346d8cb2
Timo Kohorst <timo-hb@arcor.de>
2013-09-25 08:22:44 +0200

    migration not committed

---
+++ db/migrate/20130924224533_add_formality_levels_to_ontologies.rb
@@ -0,0 +1,21 @@
+class AddFormalityLevelsToOntologies < ActiveRecord::Migration
+  def change
+    create_table :formality_levels do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+    
+    change_table :formality_levels do |t|
+      t.index :name, :unique => true
+    end
+
+    change_table :ontologies do |t|
+      t.integer :formality_level_id
+      t.index :formality_level_id
+      t.foreign_key :formality_levels
+    end
+
+  end
+end
