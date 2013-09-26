19301a8c4dc6c28933193aefee6fa75abd48213c
Timo Kohorst <timo-hb@arcor.de>
2013-09-26 17:07:05 +0200

    refactor regex, cover different cases

--- app/helpers/sentence_helper.rb
+++ app/helpers/sentence_helper.rb
@@ -1,9 +1,11 @@
 module SentenceHelper
 
-  ### Some sentences have their very own name appended to the text. This method removes it for readability purposes
-
-  def text_stripper(sentence)
-    "#{sentence.text}".sub(/\s%\(#{sentence.name}\)%/, '')
+  def format_for_view(sentence)
+    if sentence.display_text? then
+      sentence.display_text.gsub(/<(\S*)>/, "<strong>\\1</strong>" ).html_safe
+    else
+      "#{sentence.text}".sub(/\s%\(#{sentence.name}\)%/, '')
+    end
   end
 
 end
