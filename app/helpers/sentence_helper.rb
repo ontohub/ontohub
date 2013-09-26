3981aea09c5616bf6de0ce9eeeb0ed6bbaff17f2
Timo Kohorst <timo-hb@arcor.de>
2013-09-26 15:29:41 +0200

    add comment; actually use helper in view

--- app/helpers/sentence_helper.rb
+++ app/helpers/sentence_helper.rb
@@ -1,5 +1,9 @@
 module SentenceHelper
+
+  ### Some sentences have their very own name appended to the text. This method removes it for readability purposes
+
   def text_stripper(sentence)
     "#{sentence.text}".sub(/\s%\(#{sentence.name}\)%/, '')
   end
+
 end
