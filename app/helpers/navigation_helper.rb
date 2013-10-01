e94524ace06d27a6968a4929c30999439584c547
Sascha Graef <sgraef@informatik.uni-bremen.de>
2013-10-01 22:16:07 +0200

    new Metadata Tab

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -11,10 +11,8 @@ module NavigationHelper
     
     @metadatas = [
           ['Projects', :projects],
-          ['Acronyms', :acronyms],
+          ['Categories', :categories],
           ['Tasks', :tasks],
-          ['Formality Level', :formality_levels],
-          ['Ontology Type', :ontology_type],
           ['Licens Model', :licens_model]
         ]
 
