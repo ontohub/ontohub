320bd500c01d06da4cb9d8c90924401006567950
Timo Kohorst <timo-hb@arcor.de>
2013-10-02 09:08:21 +0200

    quick fix for test

--- test/unit/helpers/sentence_helper_test.rb
+++ test/unit/helpers/sentence_helper_test.rb
@@ -2,15 +2,23 @@ require 'test_helper'
 
 class SentenceHelperTest < ActionView::TestCase
 
-  context 'Sentence display' do
+  context 'Redundant names' do
     setup do
       @sentence = FactoryGirl.create(:sentence)
       @sentence.name = 'aaaa'
       @sentence.text = 'fasdfiasdf %(aaaa)%'
     end
-    should 'remove redundant name from text' do
-      assert_equal 'fasdfiasdf', text_stripper(@sentence)
+    should 'should be removed from text' do
+      assert_equal 'fasdfiasdf', format_for_view(@sentence)
     end
   end
-  
+
+# context 'Full entity iris'
+#   setup do
+#     @sentence = FactoryGirl.create(:sentence, :owl2)
+#   end
+#   should 'should be replaced by boldened display_name' do
+#     assert_equal 2, format_for_view(@sentence).scan(/'strong'/)
+#   end
+
 end
