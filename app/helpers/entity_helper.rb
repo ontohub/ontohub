ae90558500ed7d609e1259011273402f373722a4
henning mueller <henning@orgizm.net>
2013-10-04 12:43:07 +0200

    Integrated issues from discussion on last wednesday.

--- app/helpers/entity_helper.rb
+++ app/helpers/entity_helper.rb
@@ -1,17 +1,13 @@
 module EntityHelper
+
   def name_highlighter(entity)
     if entity.name == entity.text
       string = entity.text
     else
-      string = content_tag(:span, entity.name, :class => 'entity_highlight')
+      string = content_tag(:strong, entity.name, class: 'entity_highlight')
     end
-
-    string = iri_tooltip(entity, string) if entity.iri
 
     h(entity.text).gsub(/\b#{entity.name}\b/, string).html_safe
   end
 
-  def iri_tooltip(entity, string)
-    content_tag :span, string, :'data-original-title' => entity.iri, :class => 'entity_tooltip'
-  end
 end
