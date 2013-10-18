fe0d0519f1b7b9ef8d213556d2df116404b24b8a
Julian Kornberger <juliank@tzi.de>
2013-10-18 23:32:05 +0200

    async_method added and retrying failed jobs disabled

--- config/initializers/async_helper.rb
+++ config/initializers/async_helper.rb
@@ -1,4 +1,14 @@
 class ActiveRecord::Base
+
+  # Makes a method asynchronous
+  def self.async_method(*methods)
+    methods.each do |method|
+      alias_method "#{method}_sync", method
+      define_method method do |*args|
+        async "#{method}_sync", *args
+      end
+    end
+  end
   
   # Enqueues a method to run in the background
   def async(method, *args)
@@ -11,6 +21,7 @@ class ActiveRecord::Base
     Sidekiq::Client.push \
       'queue'     => self.class.instance_variable_get('@queue') || 'default',
       'class'     => Worker,
+      'retry'     => false,
       'args'      => [self.class.to_s, id.to_s, method.to_s, *args],
       'at'        => (at < 1_000_000_000 ? Time.now + at : at).to_f
   end
