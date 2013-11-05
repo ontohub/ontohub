2208d4ff39dab811ec08328f33d56dfa1e510a7e
Tim Reddehase <robustus@rightsrestricted.com>
2013-11-05 21:30:20 +0100

    add method to create code-reference from a range.

--- app/models/code_reference.rb
+++ app/models/code_reference.rb
@@ -5,4 +5,23 @@ class CodeReference < ActiveRecord::Base
   attr_accessible :end_column, :end_line
   attr_accessible :referencee, :referencee_id
 
+
+  def self.from_range(range, referencee, persist=false)
+    return if range.nil?
+    match = range.match( %r{
+      (?<begin_line>\d+)\.
+      (?<begin_column>\d+)
+      -
+      (?<end_line>\d+)\.
+      (?<end_column>\d+)}x)
+    if match
+      reference = self.new(begin_line: match[:begin_line].to_i,
+                           begin_column: match[:begin_column].to_i,
+                           end_line: match[:end_line].to_i,
+                           end_column: match[:end_column].to_i,
+                           referencee_id: referencee)
+      reference.save if persist
+      reference
+    end
+  end
 end
