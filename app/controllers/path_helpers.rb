843b1e0f620247429ef66831a201aabf0fb6db9d
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-11-07 11:00:18 +0100

    fix #368

--- app/controllers/path_helpers.rb
+++ app/controllers/path_helpers.rb
@@ -5,13 +5,13 @@ module PathHelpers
     helper_method :fancy_repository_path
   end
 
-  def fancy_repository_path(repository, params)
-    params ||= {}
-    action = params[:action] || :files
+  def fancy_repository_path(repository, args)
+    args ||= {}
+    action = args[:action] || :files
     if params[:ref].nil? && action == :files
-      repository_tree_path repository, path: params[:path]
+      repository_tree_path repository, path: args[:path]
     else
-      repository_ref_path repository_id: repository, ref: params[:ref], action: action, path: params[:path]
+      repository_ref_path repository_id: repository, ref: args[:ref], action: action, path: args[:path]
     end
   end
 
