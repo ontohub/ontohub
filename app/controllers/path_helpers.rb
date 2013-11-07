9ef865993490bbd4e8d95ebb2b19a78c24981b38
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-11-07 11:13:37 +0100

    small refactoring and fix #348

--- app/controllers/path_helpers.rb
+++ app/controllers/path_helpers.rb
@@ -2,13 +2,17 @@ module PathHelpers
   extend ActiveSupport::Concern
 
   included do
-    helper_method :fancy_repository_path
+    helper_method :fancy_repository_path, :in_ref_path?
+  end
+
+  def in_ref_path?
+    !params[:ref].nil?
   end
 
   def fancy_repository_path(repository, args)
     args ||= {}
     action = args[:action] || :files
-    if params[:ref].nil? && action == :files
+    if !in_ref_path? && action == :files
       repository_tree_path repository, path: args[:path]
     else
       repository_ref_path repository_id: repository, ref: args[:ref], action: action, path: args[:path]
