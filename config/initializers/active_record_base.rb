960b69d6fd2d71796f8fb5374e580d957abe7b40
Julian Kornberger <juliank@tzi.de>
2013-11-09 14:44:13 +0100

    AutocompleteTest fixed

--- config/initializers/active_record_base.rb
+++ config/initializers/active_record_base.rb
@@ -1,5 +1,16 @@
 class ActiveRecord::Base
-  include ScopeBuilder
+  
+  # Rails does not like self.inherited to be overwritten.
+  # Because `include ScopeBuilder` does not work as expetected
+  # we need to do this dirty hack.
+  def self.inherited_with_foo(base)
+    base.send :include, ScopeBuilder
+    inherited_without_foo(base)
+  end
+  class << self
+    alias_method_chain :inherited, :foo
+  end
+
   
   def self.execute_sql(*args)     
     sql = self.send :sanitize_sql_array, args
