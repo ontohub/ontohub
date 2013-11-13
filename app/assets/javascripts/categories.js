1d096c303b678b7eb190f71954909d699ab97f7b
Sascha Graef <sgraef@informatik.uni-bremen.de>
2013-11-13 14:06:36 +0100

    try to fix production mode categories

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,14 +1,15 @@
 $(function () {
-        $(".selector").jstree({
-          "plugins" : [ "search", "themes", "html_data", "checkbox", "sort", "ui" ],
-          "checkbox": {
-            real_checkboxes: true,
-            real_checkboxes_names: function (n) { return [("category_ids[" + n[0].id  + "]"), 1]; },
-            two_state: true
-           },
-          "themes": {
-            theme: "classic",
-            icons: false
-          }
-    });
+  $.jstree._themes = "/assets/jstree-themes/";
+  $(".selector").jstree({
+    "plugins" : [ "search", "themes", "html_data", "checkbox", "sort", "ui" ],
+    "checkbox": {
+      real_checkboxes: true,
+      real_checkboxes_names: function (n) { return [("category_ids[" + n[0].id  + "]"), 1]; },
+      two_state: true
+    },
+    "themes": {
+      theme: "classic",
+      icons: false
+    }
+  });
 }); 
