a85a5acd1c462f025cccba61b6fe7f7c41fe92a1
Tim Reddehase <robustus@rightsrestricted.com>
2013-11-25 17:56:36 +0100

    utilize pre_axiom callback to parse descriptions.

--- lib/parsing_callback/owl.rb
+++ lib/parsing_callback/owl.rb
@@ -8,6 +8,29 @@ module ParsingCallback::OWL
 
   class Callback < ParsingCallback::GenericCallback
 
+    def pre_axiom(hash)
+      if is_annotation_sentence?(hash)
+        m = hash['text'].match(%r{
+          Class:\s+(?<entity_name><[^;]+>) # Entity/Symbol Identifier
+          \s+
+          Annotations:\s+(?<annotation_type>label|comment) # the type of annotation
+          \s+
+          "(?<annotation>.*)" # The actual annotation
+          \s*
+          (?<additionals>[^\s].*) # optional, e.g. a language tag like @pt}xm)
+        if m
+          entity = Entity.where(name: m['entity_name']).first
+          if entity
+            entity.description = m['annotation']
+            entity.save
+          end
+        end
+        false
+      else
+        true
+      end
+    end
+
     def axiom(hash, axiom)
     end
 
