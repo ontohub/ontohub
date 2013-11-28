8c83bfbf186b793d1d5f800ec366b21bf3daa0bd
henning mueller <henning@orgizm.net>
2013-11-28 12:29:47 +0100

    Removed omitable migrations before merging into staging.

--- db/migrate/20131107212414_add_description_to_entities.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToEntities < ActiveRecord::Migration
-  def change
-    add_column :entities, :description, :text
-  end
-end
--- db/migrate/20131126125834_remove_description_from_entities.rb
+++
@@ -1,9 +0,0 @@
-class RemoveDescriptionFromEntities < ActiveRecord::Migration
-  def up
-    remove_column :entities, :description
-  end
-
-  def down
-    add_column :entities, :description, :string
-  end
-end
