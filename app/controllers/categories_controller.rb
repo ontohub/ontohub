8bca887722e8f680c9ede8b7dc8bdabd7bd13a95
henning mueller <henning@orgizm.net>
2013-12-06 17:34:54 +0100

    Removed unused files generated by scaffolding.
    Conflicts:
    	app/assets/stylesheets/categories.css.scss
    	app/controllers/categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,10 @@
 class CategoriesController < InheritedResources::Base
+
   belongs_to :ontology
-    def index
-      @ontology = Ontology.find(params[:ontology_id])
-      @categories = Kaminari.paginate_array(@ontology.categories).page(params[:page])
-      
-    end
+
+  def index
+    @ontology = Ontology.find(params[:ontology_id])
+    @categories = Kaminari.paginate_array(@ontology.categories).page(params[:page])
+  end
+
 end
