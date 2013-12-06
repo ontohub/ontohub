a60927fe2101305fea1eeb5312bd2f292d29c2a9
henning mueller <henning@orgizm.net>
2013-12-06 17:34:54 +0100

    Removed unused files generated by scaffolding.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,23 @@
 class CategoriesController < InheritedResources::Base
+
   belongs_to :ontology, :optional => true
-    def index
-      if params[:ontology_id]
-        @ontology = Ontology.find(params[:ontology_id])
-        @categories = Kaminari.paginate_array(@ontology.categories).page(params[:page])
-      else
-        @c_vertices = []
-        vert = Category.first
-        if vert
-          @c_vertices = vert.roots.first.children
-        end
+
+  def index
+    if params[:ontology_id]
+      @ontology = Ontology.find(params[:ontology_id])
+      @categories = Kaminari.paginate_array(@ontology.categories).page(params[:page])
+    else
+      @c_vertices = []
+      vert = Category.first
+      if vert
+        @c_vertices = vert.roots.first.children
       end
     end
-    
-    def show
-      @category = Category.find(params[:id])
-      @ontologies = @category.related_ontologies
-    end
+  end
+  
+  def show
+    @category = Category.find(params[:id])
+    @ontologies = @category.related_ontologies
+  end
     
 end
