694631fb23b62e1c1ba4fca24f1b16f5b1f3a1d2
Tim Reddehase <robustus@rightsrestricted.com>
2013-12-08 16:15:27 +0100

    use id to fetch repository in update hook worker.

--- lib/repository_update_worker.rb
+++ lib/repository_update_worker.rb
@@ -2,7 +2,9 @@
 class RepositoryUpdateWorker < Worker
 
   def perform(repo_path, oldrev, newrev, refname, key_id)
-    Repository.where(path: repo_path).first!
+    repo_path =~ /(\d+)\/?\z/
+    repo_id = $1.to_i
+    Repository.where(id: repo_id).first!
       .suspended_save_ontologies \
         start_oid: newrev,
         stop_oid:  oldrev
