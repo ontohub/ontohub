51151d658dd3b3ccbef1e5aa45aa98af14b5c1a9
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-12-08 14:29:49 +0100

    authorize files controller

--- app/controllers/ontologies_controller.rb
+++ app/controllers/ontologies_controller.rb
@@ -12,6 +12,7 @@ class OntologiesController < InheritedResources::Base
   actions :index, :show, :edit, :update
 
   before_filter :check_write_permission, :except => [:index, :show, :oops_state]
+  before_filter :check_read_permissions
 
   def index
     if in_repository?
@@ -95,6 +96,10 @@ class OntologiesController < InheritedResources::Base
 
   protected
   
+  def check_read_permissions
+    authorize! :show, resource.repository
+  end
+
   def build_resource
     @ontology ||= begin
       type  = (params[:ontology] || {}).delete(:type)
