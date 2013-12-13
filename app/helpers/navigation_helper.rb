d28ec039bdaa0052214dd16acde111a247513088
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-12-13 16:18:03 +0100

    ontology header: show only defining files

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -66,8 +66,7 @@ module NavigationHelper
       resource:           ontology,
       current_page:       current_page,
       pages:              pages,
-      additional_actions: [],
-      files:              ontology.repository.paths_starting_with(ontology.basepath)
+      additional_actions: []
     }
   end
      
