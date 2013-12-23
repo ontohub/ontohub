f3a113d3e0e5769225794ccae76d67b2ab7ce86c
Tim Reddehase <robustus@rightsrestricted.com>
2013-12-23 15:45:07 +0100

    do not use ALTER TABLE IF EXISTS
    Since we also need to support PostgreSQL 9.1,
    we can't use the ALTER TABLE IF EXISTS, since
    IF EXISTS on ALTER TABLE was introduced in 9.2.

--- db/migrate/20131219232039_add_logic_constraint_to_ontologies.rb
+++ db/migrate/20131219232039_add_logic_constraint_to_ontologies.rb
@@ -1,14 +1,14 @@
 class AddLogicConstraintToOntologies < ActiveRecord::Migration
   def up
     execute <<-SQL
-ALTER TABLE IF EXISTS ontologies
+ALTER TABLE ontologies
 ADD CONSTRAINT logic_id_check CHECK (state != 'done' OR logic_id IS NOT NULL OR type = 'DistributedOntology')
     SQL
   end
 
   def down
     execute <<-SQL
-ALTER TABLE IF EXISTS ontologies
+ALTER TABLE ontologies
 DROP CONSTRAINT IF EXISTS logic_id_check
     SQL
   end
