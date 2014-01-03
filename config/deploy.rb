1e5f872a93f86ef89c44bfd31ba402a3f3c29434
henning mueller <henning@orgizm.net>
2014-01-03 21:03:26 +0100

    In capistrano config, increment Ruby version for rvm.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :deploy_via, :remote_cache
 # RVM
 require "rvm/capistrano"
 set :rvm_type, :system
-set :rvm_ruby_string, "ruby-2.0.0@#{application}"
+set :rvm_ruby_string, "ruby-2.1.0@#{application}"
 
 role :app, hostname
 role :web, hostname
