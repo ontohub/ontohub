813228091db7abf114e821e3db4883f7be01b934
Daniel Couto Vale <danielvale@uni-bremen.de>
2014-01-10 17:53:44 +0100

    Fix filtered search

--- app/models/ontology/searching.rb
+++ app/models/ontology/searching.rb
@@ -36,11 +36,11 @@ module Ontology::Searching
     def search_by_keywords(keywords, page, repository, project, ontology_type, license_model, formality_level, task)
       Ontology.search do
         keywords.each { |keyword| fulltext keyword }
-        with(:ontology_type_id, ontology_type.id) if ontology_type
-	with(:repository_id, repository.id) if repository
-        with(:license_model_id, license_model.id) if license_model
-        with(:formality_level_id, formality_level.id) if formality_level
-        with(:task_id, task.id) if task
+        with(:ontology_type_id, ontology_type.id) unless ontology_type.nil?
+        with(:repository_id, repository.id) unless repository.nil?
+        with(:license_model_id, license_model.id) unless license_model.nil?
+        with(:formality_level_id, formality_level.id) unless formality_level.nil?
+        with(:task_id, task.id) unless task.nil?
         paginate page: page, per_page: 20
       end
     end
