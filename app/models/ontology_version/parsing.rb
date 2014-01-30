519002bc41b20a069be1c669660e702f9bd4a593
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-01-30 15:48:13 +0100

    first steps toward fast hets run: adding parameter fast_parse

--- app/models/ontology_version/parsing.rb
+++ app/models/ontology_version/parsing.rb
@@ -5,6 +5,7 @@ module OntologyVersion::Parsing
     @queue = 'hets'
     
     after_create :async_parse, :if => :commit_oid?
+    attr_accessor :fast_parse
   end
 
   def do_not_parse!
@@ -14,11 +15,16 @@ module OntologyVersion::Parsing
   def async_parse
     if !@deactivate_parsing
       update_state! :pending
-      async :parse
+
+      if @fast_parse
+        async :parse_fast
+      else
+        async :parse_full
+      end
     end
   end
 
-  def parse
+  def parse(structure_only: false)
 #    do_or_set_failed do
 #      condition = ['checksum = ? and id != ?', self.checksum, self.id]
 #      if OntologyVersion.where(condition).any?
@@ -31,7 +37,7 @@ module OntologyVersion::Parsing
     do_or_set_failed do
       refresh_checksum! unless checksum?
       
-      @path = Hets.parse(self.raw_path!, self.ontology.repository.url_maps, File.dirname(self.xml_path))
+      @path = Hets.parse(self.raw_path!, self.ontology.repository.url_maps, File.dirname(self.xml_path), structure_only: false)
       
       # move generated file to destination
       File.rename @path, self.xml_path
@@ -43,4 +49,11 @@ module OntologyVersion::Parsing
     end
   end
   
+  def parse_full
+    parse(structure_only: false)
+  end
+
+  def parse_fast
+    parse(structure_only: true)
+  end
 end
