4d86671bbfab6c3b5636ce8d3a2cb3c2e5992fb8
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-02-06 23:37:23 +0100

    make user_info a function

--- app/models/repository/git_repositories.rb
+++ app/models/repository/git_repositories.rb
@@ -42,7 +42,7 @@ module Repository::GitRepositories
   def save_file(tmp_file, filepath, message, user, iri=nil)
     version = nil
 
-    git.add_file({email: user.email, name: user.name}, tmp_file, filepath, message) do |commit_oid|
+    git.add_file(user_info(user), tmp_file, filepath, message) do |commit_oid|
       version = save_ontology(commit_oid, filepath, user, iri: iri)
     end
     touch
@@ -235,4 +235,7 @@ module Repository::GitRepositories
     end
   end
 
+  def user_info(user)
+    {email: user.email, name: user.name}
+  end
 end
