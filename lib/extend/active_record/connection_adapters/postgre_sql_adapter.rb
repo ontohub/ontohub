d52460ad1cd3b6afe8a78337cb11771bcd0c5ee6
Tim Reddehase <robustus@rightsrestricted.com>
2014-02-17 15:14:59 +0100

    add drop-type support for rake db:migrate:clean
    This is needed since we now create Types
    in some migrations.

---
+++ lib/extend/active_record/connection_adapters/postgre_sql_adapter.rb
@@ -0,0 +1,11 @@
+class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter < ActiveRecord::ConnectionAdapters::AbstractAdapter
+  def types
+    query("SELECT t.typname as typename
+FROM pg_type t
+LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
+WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
+AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
+AND n.nspname NOT IN ('pg_catalog', 'information_schema')
+", 'SCHEMA').flatten
+  end
+end
