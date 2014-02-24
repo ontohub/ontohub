042359c7522ab44105362b5a0f2f568c8afe00a7
Tim Reddehase <robustus@rightsrestricted.com>
2014-02-24 14:57:05 +0100

    improve concurreny balancers error messages.

--- lib/concurrency_balancer.rb
+++ lib/concurrency_balancer.rb
@@ -6,13 +6,13 @@ class ConcurrencyBalancer
 
   def mark_as_processing_or_complain(iri, unlock_this_iri: nil)
     successful = redis.sadd REDIS_KEY, iri
-    raise AlreadyProcessingError, iri unless successful
+    raise AlreadyProcessingError, "This iri <#{iri}> is already being processed" unless successful
     mark_as_finished_processing(unlock_this_iri) if unlock_this_iri
   end
 
   def mark_as_finished_processing(iri)
     successful = redis.srem REDIS_KEY, iri
-    raise UnmarkedProcessingError, iri unless successful
+    raise UnmarkedProcessingError, "This iri <#{iri}> should've being marked as done, but wasn't marked as processing beforehand" unless successful
   end
 
   protected
