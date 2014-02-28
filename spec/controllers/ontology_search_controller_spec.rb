84618b62a0e216410c5aab505c63e09f019da930
henning mueller <henning@orgizm.net>
2014-02-28 16:49:50 +0100

    Minimal code style change in ontology search controller spec.

--- spec/controllers/ontology_search_controller_spec.rb
+++ spec/controllers/ontology_search_controller_spec.rb
@@ -2,22 +2,24 @@ require 'spec_helper'
 
 describe OntologySearchController do
 
-  describe "GET search" do
-    it "searches for ontologies with the given keywords" do
-      @request.env["HTTP_ACCEPT"] = "application/json"
-      @request.env["CONTENT_TYPE"] = "application/json"
-      get :search, { keywords: [] }
+  describe 'GET search' do
+    it 'searches for ontologies with the given keywords' do
+      @request.env['HTTP_ACCEPT'] = 'application/json'
+      @request.env['CONTENT_TYPE'] = 'application/json'
 
+      get :search, { keywords: [] }
 
       expect(response.status).to eq(200)
     end
   end
 
-  describe "GET filter_map" do
-    it "renders a map of filters in json" do
-      @request.env["HTTP_ACCEPT"] = "application/json"
-      @request.env["CONTENT_TYPE"] = "application/json"
+  describe 'GET filter_map' do
+    it 'renders a map of filters in json' do
+      @request.env['HTTP_ACCEPT'] = 'application/json'
+      @request.env['CONTENT_TYPE'] = 'application/json'
+
       get :filters_map
+
       expect(response.status).to eq(200)
     end
   end
