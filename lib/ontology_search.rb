3dc4e9663ede590ca9b2d7bfafd81a13ea21cdb2
henning mueller <henning@orgizm.net>
2014-02-28 16:28:15 +0100

    Some refactoring (especially of the OntologySearch class).

--- lib/ontology_search.rb
+++ lib/ontology_search.rb
@@ -1,11 +1,8 @@
 require 'json'
 
-#
 # Beware! This is not tested well.
-#
 class OntologySearch
-
-  class Response < Struct.new(:page,:ontologiesPerPage,:ontologiesInSet,:ontologies)
+  class Response < Struct.new(:page, :ontologiesPerPage, :ontologiesInSet, :ontologies)
   end
 
   def initialize
@@ -17,87 +14,70 @@ class OntologySearch
   end
 
   def make_repository_keyword_list_json(repository, prefix)
-    JSON.generate(make_repository_keyword_list(repository, prefix))
+    JSON.generate make_repository_keyword_list(repository, prefix)
   end
 
   def make_global_keyword_list_json(prefix)
-    JSON.generate(make_global_keyword_list(prefix))
+    JSON.generate({text: prefix})
   end
 
-  def make_filters_map()
-    types = OntologyType.select([:name, :id]).order(:name).all.map {|type| {"name" => type.name.sub(/Ontology/, "ontologies"), "value" => type.id.to_s} }
-    repositories = Repository.select([:name, :id]).order(:name).all.map {|repository| {"name" => "in " + repository.name, "value" => repository.id.to_s} }
-    projects = Project.select([:name, :id]).order(:name).all.map {|project| {"name" => "from " + project.name, "value" => project.id.to_s} }
-    formalities = FormalityLevel.select([:name, :id]).order(:name).all.map {|formality| {"name" => "in " + formality.name, "value" => formality.id.to_s} }
-    licenses = LicenseModel.select([:name, :id]).order(:name).all.map {|license| {"name" => "under " + license.name, "value" => license.id.to_s} }
-    tasks = Task.select([:name, :id]).order(:name).all.map {|task| {"name" => "for " + task.name[0..-5].from_titlecase_to_spacedlowercase, "value" => task.id.to_s} }
+  def make_filters_map
     filters_map = {
       'OntologyType' => [
-        { "name" => 'Ontologies', "value" => nil },
+        {'name' => 'Ontologies', 'value' => nil},
         *types
       ],
       'Repository' => [
-        { "name" => 'in all repositories', "value" => nil },
+        {'name' => 'in all repositories', 'value' => nil},
         *repositories
       ],
       'Project' => [
-        { "name" => 'from all projects', "value" => nil },
+        {'name' => 'from all projects', 'value' => nil},
         *projects
       ],
       'FormalityLevel' => [
-        { "name" => 'in any formality', "value" => nil },
+        {'name' => 'in any formality', 'value' => nil},
         *formalities
       ],
       'LicenseModel' => [
-        { "name" => 'under any license', "value" => nil },
+        {'name' => 'under any license', 'value' => nil},
         *licenses
       ],
       'Task' => [
-        { "name" => 'for any purpose', "value" => nil },
+        {'name' => 'for any purpose', 'value' => nil},
         *tasks
       ]
     }
   end
 
   def make_repository_keyword_list(repository, prefix)
-    text_list = Set.new
+    text_list = []
     
-    #unless repository.ontologies.where("name = :prefix", prefix: prefix, repository_id: repository).empty?
-    #  text_list.add(prefix)
-    #end
-    
-    repository.ontologies.select(:name).where("name ILIKE :prefix", prefix: "#{prefix}%").limit(25).group("name").limit(5).each do |ontology|
-      text_list.add(ontology.name)
-    end
+    ontology_names = repository.ontologies
+      .select(:name)
+      .where('name ilike ?', "#{prefix}%")
+      .group(:name)
+      .limit(5)
+      .pluck(:name)
+
+    ontology_names.map { |name| text_list << name }
 
     Entity.collect_keywords(prefix, repository).each do |symbol|
-      text_list.add(symbol.display_name) if symbol.display_name
-      text_list.add(symbol.name) if symbol.name
-      text_list.add(symbol.text) if symbol.text
+      %i[display_name name text].each do |method|
+        value = symbol.call method
+        text_list << value if value
+      end
     end
 
-    ontology_ids = Set.new
-    repository.ontologies.select(:id).each do |ontology|
-      ontology_ids.add(ontology.id)
+    ontology_ids = repository.ontologies.pluck(:id)
+
+    logics = Logic.where('name ILIKE ?', "#{prefix}%").limit(5)
+    logics.each do |logic|
+      ids = logic.ontologies.pluck(:id)
+      text_list << logic.name unless (ontology_ids & ids).empty?
     end
 
-    Logic.where("name ILIKE :prefix", prefix: "#{prefix}%").limit(5).each do |logic|
-       logic_ontology_ids = Set.new
-       logic.ontologies.each do |ontology|
-         logic_ontology_ids.add(ontology.id)
-       end
-       if (ontology_ids & logic_ontology_ids).size != 0
-         text_list.add(logic.name)
-       end
-    end
-
-    text_list.to_a.sort.map { |x| {text: x} }
-  end
-
-  def make_global_keyword_list(prefix)
-    text_list = Set.new
-    text_list.add(prefix)
-    text_list.to_a.sort.map { |x| {text: x} }
+    text_list.sort.map { |x| {text: x} }
   end
 
   def make_bean_list_json(repository, keyword_list, page)
@@ -106,11 +86,13 @@ class OntologySearch
 
   def select_item_list(keyword_list, type_name)
     item_list = Array.new
+
     keyword_list.each do |keyword|
       if keyword["type"] == type_name
         item_list.push keyword["item"]
       end
     end
+
     item_list
   end
 
@@ -124,11 +106,13 @@ class OntologySearch
         end
       end
     end
+
     nil
   end
 
   def make_bean_list_response(repository, keyword_list, page)
     mixed_list = select_item_list(keyword_list, 'Mixed')
+
     qualifiers = Hash.new
     qualifiers[:repository] = repository
     qualifiers[:ontology_type] = select_item(keyword_list, 'OntologyType', OntologyType)
@@ -138,6 +122,7 @@ class OntologySearch
     qualifiers[:task] = select_item(keyword_list, 'Task', Task)
 
     bean_list_factory = OntologyBeanListFactory.new
+
     search = Ontology.search_by_keywords(mixed_list, page, qualifiers)
     search.results.each do |ontology|
       bean_list_factory.add_small_bean(ontology)
@@ -146,4 +131,48 @@ class OntologySearch
     Response.new(page, @limit, search.total, bean_list_factory.bean_list)
   end
 
+  private
+
+  def model_to_filters_map(model, name_proc)
+    model
+      .select([:name, :id])
+      .order(:name)
+      .all
+      .map do |item|
+        {
+          'name'  => name_proc.call(item),
+          'value' => item.id.to_s
+        }
+      end
+  end
+
+  def types
+    model_to_filters_map OntologyType,
+      ->(x) {x.name.sub(/Ontology/, 'ontologies')}
+  end
+
+  def repositories
+    model_to_filters_map Repository,
+      ->(x) {'in ' + x.name}
+  end
+
+  def projects
+    model_to_filters_map Project,
+      ->(x) {'from ' + x.name }
+  end
+
+  def formalities
+    model_to_filters_map FormalityLevel,
+      ->(x) {'in ' + x.name}
+  end
+
+  def licenses
+    model_to_filters_map LicenseModel,
+      ->(x) {'under ' + x.name}
+  end
+
+  def tasks
+    model_to_filters_map Task,
+      ->(x) {'for ' + x.name[0..-5].from_titlecase_to_spacedlowercase}
+  end
 end
