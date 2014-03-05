d9f8c4120bb7409b56e1ca212c23ff1428516348
Tim Reddehase <robustus@rightsrestricted.com>
2014-03-05 14:22:16 +0100

    fix namespacing and method-name for uri-fetcher.

--- lib/uri_fetcher.rb
+++ lib/uri_fetcher.rb
@@ -2,7 +2,7 @@ module UriFetcher
 
   def fetch_uri_content(uri, limit: 10, write_file: nil)
     raise ArgumentError, 'too many HTTP redirects' if limit == 0
-    Net::HTTP.get_request(URI(uri)) do |response|
+    Net::HTTP.get_response(URI(uri)) do |response|
       if has_actual_content?(response)
         produce_response_body(response, write_file)
       else
@@ -29,7 +29,7 @@ module UriFetcher
   end
 
   def has_actual_content?(response)
-    response.is_a?(HTTPSuccess) && response.content_type != 'text/html'
+    response.is_a?(Net::HTTPSuccess) && response.content_type != 'text/html'
   end
 
 end
