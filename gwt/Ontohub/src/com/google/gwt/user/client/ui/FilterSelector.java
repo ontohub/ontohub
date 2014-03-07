a7639c883d152b07b2562e881bada24ce9c42339
Daniel Couto Vale <danielvale@uni-bremen.de>
2014-03-07 08:44:58 +0100

    Fix double click issue of filter selection

--- gwt/Ontohub/src/com/google/gwt/user/client/ui/FilterSelector.java
+++ gwt/Ontohub/src/com/google/gwt/user/client/ui/FilterSelector.java
@@ -78,6 +78,8 @@ public class FilterSelector extends Composite implements HasWidgets, HasFilterSe
 	@UiHandler("button")
 	public final void onButtonClick(ClickEvent event) {
 		toggle();
+		event.stopPropagation();
+		event.preventDefault();
 	}
 
 	/**
