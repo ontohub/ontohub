b1771f28608b86404c2decef5afeded1d906dc57
Tim Reddehase <robustus@rightsrestricted.com>
2014-03-09 20:17:03 +0100

    save current state of sentence translations.

--- app/models/ontology/sentences.rb
+++ app/models/ontology/sentences.rb
@@ -5,9 +5,86 @@ module Ontology::Sentences
     has_many :sentences,
       autosave: false,
       extend:   Methods
+    include GraphStructures::SqlHelper
+
+    def translated_sentences(audience)
+      translated = self.sentences.translated_for(audience)
+      imported = self.direct_imported_ontologies.map do |ontology|
+        ontology.translated_sentences(self)
+      end
+      translated + imported
+    end
+
+    def incoming_imports_with_mappings
+      # INNER JOINS always return an empty result set if the
+      # ON clause does not match.
+      Link.joins(:entity_mappings).where(target_id: self, kind: 'import')
+    end
+
+    def create_translated_sentences
+      query, args = links_by_kind_query(self, 'import')
+      imported_ontology_ids = pluck_select([query, *args], :id).reverse
+      imported_ontology_ids.each { |o_id| Ontology.find(o_id).create_translated_sentences }
+      import_links = incoming_imports_with_mappings
+      if import_links.any?
+        sentences.each do |sentence|
+          import_links.each do |import_link|
+            import_link.entity_mappings.each do |mapping|
+              translate_sentence(sentence, mapping)
+            end
+          end
+        end
+      end
+    end
+
+    def translate_sentence(sentence, mapping)
+      entity_ids = sentence.entities.pluck(:id)
+      entity_intersection = entity_ids.include?(mapping.source_id)
+      if entity_intersection
+        translated_text = mapping.apply(sentence)
+        audience = mapping.link.source
+        TranslatedSentence.create(audience: audience,
+                                  sentence: sentence,
+                                  translated_text: translated_text,
+                                  ontology: self)
+      end
+    end
+
+    protected
+    def translate_sentences_for(audience_ontology)
+      link = Link.where(source_id: audience_ontology, target_id: self, kind: 'import').first
+      mappings = link.entity_mappings
+      if mappings.any?
+        create_or_fetch_translations(audience_ontology, link, mappings)
+      else
+        default_translated_sentences
+      end
+    end
+
+    # If there are no translations applicable, what do you do?
+    def default_translated_sentences
+      self.sentences
+    end
+
+    def create_or_fetch_translations(audience_ontology, link, mappings, overwrite: false)
+      translations = TranslatedSentence.for(audience_ontology, sentences_from: self)
+      if translations.any? && !overwrite
+        translations
+      else
+        translations.delete_all
+        create_translations(audience_ontology, link, mappings)
+      end
+    end
+
+    def create_translations(audience_ontology, link, mappings)
+      query, args = links_by_kind_query(self, 'import')
+      imported_ontology_ids = pluck_select([query, *args], :id).reverse
+      imported_ontology_ids.each { |o_id| Ontology.find(o_id).create_translated_sentences }
+    end
   end
 
   module Methods
+
     def update_or_create_from_hash(hash, timestamp = Time.now)
       e = find_or_initialize_by_name(hash['name'])
 
@@ -27,5 +104,10 @@ module Ontology::Sentences
 
       e
     end
+
+    def translated_for(audience)
+
+    end
+
   end
 end
