b714134027bc44a229866fcb93b493d3c4b7254a
Tim Reddehase <robustus@rightsrestricted.com>
2014-03-29 13:15:12 +0100

    create readable indentation for vanilla sql
    squish will remove it on the object level.

--- lib/graph_structures/specific_fetchers/links.rb
+++ lib/graph_structures/specific_fetchers/links.rb
@@ -10,12 +10,12 @@ module GraphStructures
       # of one represents that there are no logic translations.
       def contains_logic_translations_query(ontology)
         query, args = links_by_kind_query(ontology, 'import')
-        full_query = <<-SQL
-SELECT ? as logically_translated UNION
-SELECT ontologies.logic_id as logically_translated
-FROM ontologies
-JOIN (#{query}) as imported
-ON ontologies.id = imported.ontology_id
+        full_query = <<-SQL.squish
+          SELECT ? as logically_translated UNION
+          SELECT ontologies.logic_id as logically_translated
+          FROM ontologies
+          JOIN (#{query}) as imported
+          ON ontologies.id = imported.ontology_id
         SQL
         [full_query, [ontology.logic_id] + args]
       end
@@ -26,15 +26,15 @@ ON ontologies.id = imported.ontology_id
       end
 
       def links_by_kind_query(center, kind)
-        query = <<-SQL
-WITH RECURSIVE imported_ontologies(ontology_id) AS (
-    SELECT source_id FROM links WHERE links.kind = ? AND links.target_id = ?
-  UNION
-    SELECT source_id FROM links
-      JOIN imported_ontologies ON links.target_id = imported_ontologies.ontology_id
-      WHERE links.kind = ?
-)
-SELECT ontology_id FROM imported_ontologies
+        query = <<-SQL.squish
+          WITH RECURSIVE imported_ontologies(ontology_id) AS (
+              SELECT source_id FROM links WHERE links.kind = ? AND links.target_id = ?
+            UNION
+              SELECT source_id FROM links
+                JOIN imported_ontologies ON links.target_id = imported_ontologies.ontology_id
+                WHERE links.kind = ?
+          )
+          SELECT ontology_id FROM imported_ontologies
         SQL
         [query, [kind, center, kind]]
       end
