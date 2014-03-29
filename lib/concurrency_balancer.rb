9148dbb94fa04554378a15f0718246e0266add5d
Tim Reddehase <robustus@rightsrestricted.com>
2014-03-29 12:07:31 +0100

    document the concurrency balancer.

--- lib/concurrency_balancer.rb
+++ lib/concurrency_balancer.rb
@@ -1,3 +1,9 @@
+# This is a helper used in the import of ontologies.
+# It uses redis to create an atomic global store
+# for the currently processing ontologies (by their iris).
+# This allows to lock on concurrency problems in the
+# way that a job can only parse an ontology (and write content)
+# if no other job currently parses this ontology.
 class ConcurrencyBalancer
   include WrappingRedis
 
@@ -10,17 +16,26 @@ class ConcurrencyBalancer
   class UnmarkedProcessingError < Error; end
   class AlreadyLockedError < Error; end
 
+  # Marks an ontology (by its iri) as processing.
+  # As one ontology parsing job can contain multiple
+  # ontologies this is needed to circumvent
+  # concurrency problems
   def mark_as_processing_or_complain(iri, unlock_this_iri: nil)
     successful = redis.sadd REDIS_KEY, iri
     raise AlreadyProcessingError, "This iri <#{iri}> is already being processed" unless successful
     mark_as_finished_processing(unlock_this_iri) if unlock_this_iri
   end
 
+  # This marks an ontology (by its iri) as
+  # done (in the scope of processing ontology-content
   def mark_as_finished_processing(iri)
     successful = redis.srem REDIS_KEY, iri
     raise UnmarkedProcessingError, "This iri <#{iri}> should've being marked as done, but wasn't marked as processing beforehand" unless successful
   end
 
+  # Basic sequential lock which ensures that only one
+  # (concurrently executed job) can be really executed
+  # at the same time (used as part of the sequential queue).
   def self.sequential_lock
     if RedisWrapper.new.sadd(SEQUENTIAL_LOCK_KEY, true)
       begin
