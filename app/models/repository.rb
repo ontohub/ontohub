214b17ce5309c42f31fa6df96778cc9c85ae927d
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-03-29 15:08:34 +0100

    move destroying-code into a separate module

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -5,6 +5,7 @@ class Repository < ActiveRecord::Base
   include Permissionable
 
   include Repository::Access
+  include Repository::Destroying
   include Repository::FilesList
   include Repository::GitRepositories
   include Repository::Importing
@@ -24,7 +25,6 @@ class Repository < ActiveRecord::Base
   attr_accessor :user
 
   after_save :clear_readers
-  before_destroy :mark_as_destroying, prepend: true
 
   scope :latest, order('updated_at DESC')
 
@@ -34,27 +34,6 @@ class Repository < ActiveRecord::Base
 
   def to_param
     path
-  end
-
-  def is_destroying?
-    self.class.instance_variable_get(:@destroying).include?(self.id)
-  end
-
-  def destroy
-    super
-  rescue StandardError => e
-    unmark_as_destroying
-    raise e.class, "Can't delete repository: It contains an ontology that is imported by another repository."
-  end
-
-  protected
-
-  def mark_as_destroying
-    self.class.instance_variable_get(:@destroying) << self.id if self.id
-  end
-
-  def unmark_as_destroying
-    self.class.instance_variable_get(:@destroying).delete(self.id) if self.id
   end
 
 end
