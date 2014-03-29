1115fd7e3171f7aaeb98472789edf016996b7278
henning mueller <henning@orgizm.net>
2014-03-29 11:27:27 +0100

    Just a minor change, which lowers redundancy.

--- app/controllers/ontology_search_controller.rb
+++ app/controllers/ontology_search_controller.rb
@@ -1,14 +1,11 @@
-# 
 # Ontology search endpoints for the GWT search code.
-# 
 class OntologySearchController < ApplicationController
-
   respond_to :json
 
   def keywords
     prefix = params[:prefix] || ''
+
     if in_repository?
-      repository = Repository.find_by_path params[:repository_id]
       respond_with OntologySearch.new.make_repository_keyword_list_json(repository, prefix)
     else
       respond_with OntologySearch.new.make_global_keyword_list_json(prefix)
@@ -17,15 +14,16 @@ class OntologySearchController < ApplicationController
 
   def search
     keywords = params[:keywords] || []
+
     begin
       keywords = keywords.map { |keyword| JSON.parse(keyword) }
     rescue
       raise ActionController::RoutingError.new('Not Found')
-      return
     end
+
     page = params[:page].to_i
+
     if in_repository?
-      repository = Repository.find_by_path params[:repository_id]
       respond_with OntologySearch.new.make_bean_list_json(repository, keywords, page)
     else
       respond_with OntologySearch.new.make_bean_list_json(nil, keywords, page)
@@ -36,4 +34,9 @@ class OntologySearchController < ApplicationController
     respond_with OntologySearch.new.make_filters_map_json
   end
 
+  private
+
+  def repository
+    Repository.find_by_path params[:repository_id]
+  end
 end
