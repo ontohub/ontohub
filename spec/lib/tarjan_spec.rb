9e5b4e4ef9b6bf7306b37638c04d0073dcaa8519
Sascha Graef <sgraef@informatik.uni-bremen.de>
2014-03-29 19:17:36 +0100

    tests are green

---
+++ spec/lib/tarjan_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe TarjanTree do
+
+  context 'owl classhierachy' do
+      let(:ontology) { create :ontology }
+      let(:user) { create :user }
+      let(:xml_path) { Rails.root + 'test/fixtures/ontologies/xml/' + 'cycle.xml' }
+
+      before do
+        ontology.import_xml_from_file xml_path, nil, user
+        ontology.create_entity_tree
+      end
+      
+      it 'should have an entity-tree' do
+        ontology.entity_groups.size.should == 3
+        ontology.entities.where(name: ["B1","B2","B3"])
+      end
+      
+      it 'should have an congruence node' do
+        ontology.entity_groups.where("name LIKE '%☰%'")
+      end
+      
+    end
+
+end
