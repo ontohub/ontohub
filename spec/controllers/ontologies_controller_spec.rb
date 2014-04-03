4006c3197773443c7393545ff125792107c3e20d
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-04-03 12:23:26 +0200

    fix erroneous ontology spec

--- spec/controllers/ontologies_controller_spec.rb
+++ spec/controllers/ontologies_controller_spec.rb
@@ -47,7 +47,7 @@ describe OntologiesController do
         end
 
         it{ should respond_with :found }
-        expect(response).to redirect_to([repository, :ontologies])
+        it{ response.should redirect_to([repository, :ontologies]) }
       end
 
       context 'unsuccessful deletion' do
