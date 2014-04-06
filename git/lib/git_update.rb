b5f0070e605a64759eb97a876f1718d448cd1dc5
Tim Reddehase <robustus@rightsrestricted.com>
2014-04-06 12:16:45 +0200

    provide useful error message to git-push user
    When ssh-access querying for permissions fails
    for HTTP related reasons the git-push user should be
    informed of the situation instead of being attacked
    by a ruby stacktrace.

--- git/lib/git_update.rb
+++ git/lib/git_update.rb
@@ -33,6 +33,14 @@ class GitUpdate
       update_redis
       exit 0
     end
+  rescue OntohubNet::UnexpectedStatusCodeError => error
+    puts <<-ERROR
+We couldn't determine your permissions successfully because
+we encountered a status code of #{error.status_code} when
+querying for permissions. Please try again in a few minutes.
+If this issue persists please inform an Administrator.
+    ERROR
+    exit 1
   end
 
   protected
