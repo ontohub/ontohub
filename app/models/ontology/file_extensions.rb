edb0262a62074951f0d4997a783ce03eebc7c63e
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-04-09 15:14:27 +0200

    add functions for retrieving extensions from db

--- app/models/ontology/file_extensions.rb
+++ app/models/ontology/file_extensions.rb
@@ -2,13 +2,20 @@ module Ontology::FileExtensions
   extend ActiveSupport::Concern
 
   included do
-    FILE_EXTENSIONS_DISTRIBUTED = ActiveRecord::Base.connection.execute(
-        "SELECT extension FROM ontology_file_extensions WHERE distributed = 'true'"
-      ).map{ |r| r['extension']}
-    FILE_EXTENSIONS_SINGLE = ActiveRecord::Base.connection.execute(
+    def self.file_extensions
+      @file_extensions ||= file_extensions_distributed + file_extensions_single
+    end
+
+    def self.file_extensions_distributed
+      @file_extensions_distributed ||= ActiveRecord::Base.connection.execute(
+        "SELECT extension FROM ontology_file_extensions WHERE distributed = 'true'").map{ |r| r['extension']}
+    end
+
+    def self.file_extensions_single
+     @file_extensions_single ||= ActiveRecord::Base.connection.execute(
       "SELECT extension FROM ontology_file_extensions WHERE distributed = 'false'"
       ).map{ |r| r['extension']}
-    FILE_EXTENSIONS = FILE_EXTENSIONS_DISTRIBUTED + FILE_EXTENSIONS_SINGLE
+    end
   end
 
 end
