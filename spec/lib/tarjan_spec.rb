853da9c8c05e1fdb779529a59aafcc117700fa6a
Sascha Graef <sgraef@informatik.uni-bremen.de>
2014-04-09 14:45:57 +0200

    tests are green again

--- spec/lib/tarjan_spec.rb
+++ spec/lib/tarjan_spec.rb
@@ -15,7 +15,7 @@ describe TarjanTree do
       it 'should have an entity-tree' do
         ontology.entity_groups.size.should == 3
         entities = ontology.entities.where(name: ["B1","B2","B3"])
-        expect(ontology.entity_groups.where(entities: entities).first).not_to be_nil
+        expect(ontology.entity_groups.includes(:entities).where("entities.id" => entities).first).not_to be_nil
       end
       
       it 'should have an congruence node' do
