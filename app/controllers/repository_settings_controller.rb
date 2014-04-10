2734828790288bd09303cf59cbe52097fdec6e24
Sascha Graef <sgraef@informatik.uni-bremen.de>
2014-04-10 15:47:45 +0200

    removing unused variable

--- app/controllers/repository_settings_controller.rb
+++ app/controllers/repository_settings_controller.rb
@@ -2,7 +2,6 @@ class RepositorySettingsController < InheritedResources::Base
   belongs_to :repository, finder: :find_by_path!
   
   def index
-    repo = Repository.find_by_path(params[:repository_id])
     redirect_to repository_url_maps_path
   end
 end
