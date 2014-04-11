db68b8e9ff1d6052cc49037887311667e239b3b8
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-04-11 17:09:52 +0200

    remove id from file extensions db table

--- db/migrate/20140409123407_create_ontology_file_extensions.rb
+++ db/migrate/20140409123407_create_ontology_file_extensions.rb
@@ -1,6 +1,6 @@
 class CreateOntologyFileExtensions < ActiveRecord::Migration
   def change
-    create_table :ontology_file_extensions do |t|
+    create_table :ontology_file_extensions, id: false do |t|
       t.string :extension, :null => false, :unique => true
       t.boolean :distributed, :null => false
     end
