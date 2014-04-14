0a120bb57fc4781dd81a23cbc1c9576397f76e6d
Daniel Couto Vale <danielvale@uni-bremen.de>
2014-04-14 10:03:16 +0200

    Add display fields to the small widget ontology

--- gwt/Ontohub/src/org/ontohub/client/OntologyWidget.java
+++ gwt/Ontohub/src/org/ontohub/client/OntologyWidget.java
@@ -10,24 +10,27 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 public class OntologyWidget extends Composite {
 
-	private static OntologyWidgetUiBinder uiBinder = GWT
-			.create(OntologyWidgetUiBinder.class);
+	private static OntologyWidgetUiBinder uiBinder = GWT.create(OntologyWidgetUiBinder.class);
 
 	interface OntologyWidgetUiBinder extends UiBinder<Widget, OntologyWidget> {
 	}
 
 	@UiField
+	Image iconImage;
+
+	@UiField
 	Anchor titleAnchor;
 //
-//	@UiField
-//	InlineLabel acronymLabel;
-//
+	@UiField
+	InlineLabel acronymLabel;
+
 //	@UiField
 //	InlineLabel languageLabel;
 
@@ -39,6 +42,27 @@ public class OntologyWidget extends Composite {
 
 	@UiField
 	Label descriptionLabel;
+
+	@UiField
+	Anchor typeAnchor;
+
+	@UiField
+	Anchor topic0Anchor;
+
+	@UiField
+	Anchor topic1Anchor;
+
+	@UiField
+	Anchor topic2Anchor;
+
+	@UiField
+	Anchor project0Anchor;
+
+	@UiField
+	Anchor project1Anchor;
+
+	@UiField
+	Anchor project2Anchor;
 	
 	public OntologyWidget() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -50,14 +74,46 @@ public class OntologyWidget extends Composite {
 	}
 
 	private final void setOntology(Ontology ontology) {
+		iconImage.setUrl(ontology.getIconUrl());
+		iconImage.setAltText(ontology.getIconAltText());
 		titleAnchor.setText(ontology.getName());
 		titleAnchor.setHref(ontology.getHref());
 		titleAnchor.setTabIndex(-1);
-//		acronymLabel.setText(ontology.getAcronym());
+		if (null != ontology.getAcronym() && ontology.getAcronym().length() != 0) {
+			acronymLabel.setVisible(true);
+			acronymLabel.setText("(" + ontology.getAcronym() + ")");
+		} else {
+			acronymLabel.setVisible(false);
+		}
 //		languageLabel.setText(ontology.getLanguage());
 		logicLabel.setText(ontology.getLogic());
 		iriLabel.setText(ontology.getIri());
 		descriptionLabel.setText(ontology.getDescription());
+		updateWidgetAnchor(typeAnchor, ontology.getType(), ontology.getTypeUrl());
+		updateWidgetAnchor(topic0Anchor, ontology.getTopic0(), ontology.getTopic0Url());
+		updateWidgetAnchor(topic1Anchor, ontology.getTopic1(), ontology.getTopic1Url());
+		updateWidgetAnchor(topic2Anchor, ontology.getTopic2(), ontology.getTopic2Url());
+		updateWidgetAnchor(project0Anchor, ontology.getProject0(), ontology.getProject0Url());
+		updateWidgetAnchor(project1Anchor, ontology.getProject1(), ontology.getProject1Url());
+		updateWidgetAnchor(project2Anchor, ontology.getProject2(), ontology.getProject2Url());
+	}
+
+	/**
+	 * Updates an anchor by setting its text and href and by displaying it only when there is a text
+	 * and an href.
+	 * 
+	 * @param anchor the anchor to update
+	 * @param anchorText the text of the anchor
+	 * @param anchorHref the href of the anchor
+	 */
+	private final static void updateWidgetAnchor(Anchor anchor, String anchorText, String anchorHref) {
+		if (anchorText != null && anchorText.length() != 0 && anchorHref != null && anchorHref.length() != 0) {
+			anchor.setVisible(true);
+			anchor.setText(anchorText);
+			anchor.setHref(anchorHref);
+		} else {
+			anchor.setVisible(false);
+		}
 	}
 
 	@Override
--- gwt/Ontohub/src/org/ontohub/shared/Ontology.java
+++ gwt/Ontohub/src/org/ontohub/shared/Ontology.java
@@ -16,4 +16,20 @@ public class Ontology extends JavaScriptObject {
 	public final native String getIri() /*-{ return this.iri; }-*/;
 	public final native String getHref() /*-{ return this.url; }-*/; 
 	public final native String getDescription() /*-{ return this.description; }-*/;
+	public final native String getType() /*-{ return this.type; }-*/;
+	public final native String getTypeUrl() /*-{ return this.typeUrl; }-*/;
+	public final native String getTopic0() /*-{ return this.topic0; }-*/;
+	public final native String getTopic1() /*-{ return this.topic1; }-*/;
+	public final native String getTopic2() /*-{ return this.topic2; }-*/;
+	public final native String getTopic0Url() /*-{ return this.topic0Url; }-*/;
+	public final native String getTopic1Url() /*-{ return this.topic1Url; }-*/;
+	public final native String getTopic2Url() /*-{ return this.topic2Url; }-*/;
+	public final native String getProject0() /*-{ return this.project0; }-*/;
+	public final native String getProject1() /*-{ return this.project1; }-*/;
+	public final native String getProject2() /*-{ return this.project2; }-*/;
+	public final native String getProject0Url() /*-{ return this.project0Url; }-*/;
+	public final native String getProject1Url() /*-{ return this.project1Url; }-*/;
+	public final native String getProject2Url() /*-{ return this.project2Url; }-*/;
+	public final native String getIconUrl() /*-{ return this.iconUrl; }-*/;
+	public final native String getIconAltText() /*-{ return this.iconAltText; }-*/;
 }
