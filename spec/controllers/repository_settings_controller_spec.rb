0bf5a71512582be0c44bcd21acaafed87c4c6c48
henning mueller <henning@orgizm.net>
2014-05-02 12:02:05 +0200

    Cosmetics.

--- spec/controllers/repository_settings_controller_spec.rb
+++ spec/controllers/repository_settings_controller_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe RepositorySettingsController do
   context '#index' do
-    #redirecting in SettingsControler to url Maps because SettingsController
-    #is only a meta controller
+    # Redirecting in SettingsControler to url Maps because SettingsController
+    # is only a meta controller
     let(:repository){ create :repository }
     before do
       get :index, repository_id: repository.to_param
