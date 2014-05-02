52f48599c05f6b3385a2ac8e04ec96cbe7bb4f39
henning mueller <henning@orgizm.net>
2014-05-02 10:48:52 +0200

    Comment for CSP unsafe-inline cause.

--- config/initializers/security_headers.rb
+++ config/initializers/security_headers.rb
@@ -3,6 +3,8 @@
   config.x_frame_options        = 'DENY'
   config.x_content_type_options = "nosniff"
   config.x_xss_protection       = {:value => 1, :mode => false}
+  # By default, load resources only from own origin.
+  # For CSS, allow styles from style elements and attributes for GWT.
   config.csp                    = {
     default_src: "self",
     style_src:   "'self' 'unsafe-inline'",
