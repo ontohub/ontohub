3df59f8f57e90e1667f4c31d77accbe2b4910c1c
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-06-11 12:00:29 +0200

    put permission delete confirmation into modal

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -5,6 +5,24 @@ class PermissionsController < PrivilegeList::Base
 
   belongs_to :repository, finder: :find_by_path!
 
+  def destroy
+    destroy! do
+      redirect_to(:back)
+    end
+  rescue Permission::PowerVaccuumError => e
+    flash[:alert] = e.message
+    redirect_to :back
+  end
+
+  def update
+    update!  do
+      redirect_to(:back)
+    end
+  rescue Permission::PowerVaccuumError => e
+    flash[:alert] = e.message
+    redirect_to :back
+  end
+
   protected
 
   def relation_list
