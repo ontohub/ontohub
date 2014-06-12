935454c98226156eac882ac6dff3ccaab9275339
Tim Reddehase <robustus@rightsrestricted.com>
2014-06-12 17:42:38 +0200

    adjust timeout worker for rescheduling
    Utilize Sidekiqs scheduling capability.
    Also allow rescheduling if timeout was not
    yet reached.

--- lib/timeout_worker.rb
+++ lib/timeout_worker.rb
@@ -2,11 +2,14 @@ class TimeoutWorker < BaseWorker
   class Error < ::StandardError; end
   class TimeOutNotSetError < Error; end
 
+  RESCHEDULE_TIME = 30 # minutes
+
   include Sidekiq::Worker
 
-  def self.start_timeout_clock(ontology_version_id, start_time=nil)
-    start_time ||= self.time_since_epoch
-    self.perform_async(start_time, ontology_version_id)
+  def self.start_timeout_clock(ontology_version_id, hours_offset=nil)
+    hours_offset ||= Settings.ontology_parse_timeout
+    self.perform_in(hours_offset.hours,
+                    self.time_since_epoch, ontology_version_id)
   end
 
   def self.time_since_epoch(time=Time.now)
@@ -16,8 +19,16 @@ class TimeoutWorker < BaseWorker
   def perform(start_time, ontology_version_id)
     if timeout_reached?(Time.at(start_time))
       version = OntologyVersion.find(ontology_version_id)
-      version.update_state!('failed', "The job reached the timeout limit of #{timeout_limit} hours.")
+      if state_not_terminal?(version)
+        version.update_state!('failed', "The job reached the timeout limit of #{timeout_limit} hours.")
+      end
+    else
+      self.class.perform_in(RESCHEDULE_TIME.minutes, start_time, ontology_version_id)
     end
+  end
+
+  def state_not_terminal?(ontology_version)
+    !(ontology_version.state == 'failed' || ontology_version.state == 'done')
   end
 
   def timeout_limit
