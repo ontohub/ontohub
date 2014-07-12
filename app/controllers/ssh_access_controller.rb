ef11e6500a9ff5569cd9f90465cbe56286aa7b2b
Tim Reddehase <robustus@rightsrestricted.com>
2014-07-12 18:15:44 +0200

    handle push-to-mirror error in controller
    Also add a new flag 'provide_to_user' which
    determine whether the reason message could be
    provided to the user via STDOUT.

--- app/controllers/ssh_access_controller.rb
+++ app/controllers/ssh_access_controller.rb
@@ -1,13 +1,21 @@
 class SshAccessController < InheritedResources::Base
 
+  MIRROR_DENY_MSG = "you cannot write to a mirror repository, they are readonly"
+
   belongs_to :repository, finder: :find_by_path!
 
   def index
     allowed = SshAccess.determine_permission(
       *SshAccess.extract_permission_params(params, parent), parent)
     render json: {allowed: allowed}
+  rescue SshAccess::InvalidAccessOnMirrorError
+    render json: {allowed: false,
+                  reason: MIRROR_DENY_MSG,
+                  provide_to_user: true}
   rescue => e# ensure that we always return a valid response
-    render json: {allowed: false, reason: "internal server problem: #{e.message}"}
+    render json: {allowed: false,
+                  reason: "internal server problem: #{e.message}",
+                  provide_to_user: false}
   end
 
 end
