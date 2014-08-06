baa1df9159a311a367ffc80b3fb543482e88a7db
Tim Reddehase <robustus@rightsrestricted.com>
2014-08-06 15:31:42 +0200

    add processing iris into the status mix
    Combine the data of processing ontologies, if possible, with the data of
    processing iris.
    
    However distributed ontologies never have processing iris (it seems) and
    single in distributed ontologies are never processing (as per
    version/own state). This should probably be investigated.

--- app/viewhelpers/status_ontology_viewhelper.rb
+++ app/viewhelpers/status_ontology_viewhelper.rb
@@ -1,4 +1,5 @@
 class StatusOntologyViewhelper < ViewhelperBase
+  include WrappingRedis
 
   STATE = 'processing'
 
@@ -7,17 +8,37 @@ class StatusOntologyViewhelper < ViewhelperBase
   end
 
   def processing_ontology_data
-    @data ||= processing_ontology_query.map do |ov|
-      OpenStruct.new({
+    @data ||= procure_processing_ontology_data
+  end
+
+  def procure_processing_ontology_data
+    ov_data = processing_ontology_query.reduce({}) do |data, ov|
+      # usually iri, second part is just for uniqueness
+      key = ov.ontology.iri || "#{ov.ontology.name}#{ov.ontology.path}"
+      data[key] = OpenStruct.new({
         ontology: ov.ontology,
         ontology_version: ov,
         running_for: view.time_ago_in_words(ov.state_updated_at),
+        processing_iri: nil
       })
+      data
     end
+    processing_iris.reduce(ov_data) do |data, iri|
+      if data[iri]
+        data[iri].processing_iri = iri
+      else
+        data[iri] = OpenStruct.new({processing_iri: iri})
+      end
+      data
+    end.values
   end
 
   def any_processing_ontologies?
     processing_ontology_query.any?
+  end
+
+  def processing_iris
+    redis.smembers ConcurrencyBalancer::REDIS_KEY
   end
 
   private
