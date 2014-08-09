19e4db305edc343b4020ea192bdd612c90ab7144
Tim Reddehase <robustus@rightsrestricted.com>
2014-08-09 16:27:59 +0200

    add basic display of errors tabs in status.

--- app/viewhelpers/status_ontology_error_viewhelper.rb
+++ app/viewhelpers/status_ontology_error_viewhelper.rb
@@ -14,4 +14,20 @@ class StatusOntologyErrorViewhelper < ViewhelperBase
     Ontology.where(state: STATE)
   end
 
+  def errored_version_query
+    OntologyVersion.includes(:ontology).
+      where(state: STATE).
+      where(ontology_id: errored_ontology_query)
+  end
+
+  def errored_ontology_data
+    @data ||= procure_errored_ontology_data
+  end
+
+  # Relies on the 'old' state_message method
+  # for. Should be improved later on.
+  def procure_errored_ontology_data
+    errored_version_query.group_by(&:state_message)
+  end
+
 end
