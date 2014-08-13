fdb4faa74390ccf97fa41ddd8517217cbe1e8259
Tim Reddehase <robustus@rightsrestricted.com>
2014-08-13 15:00:13 +0200

    improve integration-test for processing ontologies.

--- features/ProcessingOntologiesStatus.feature
+++ features/ProcessingOntologiesStatus.feature
@@ -9,3 +9,9 @@ Scenario: Watching the list of processing ontologies
     And i navigate to the status page
     When i click on the processing ontologies tab selector
     Then i should be redirected to the corresponding tab
+
+Scenario: Seeing the right information for processing ontologies
+    Given i have 5 processing ontologies
+    And i am logged in as an admin
+    When i am on the processing ontologies tab
+    Then i should see the processing ontologies
--- features/step_definitions/processing_ontologies_status_steps.rb
+++ features/step_definitions/processing_ontologies_status_steps.rb
@@ -19,3 +19,30 @@ Then(/^i should be redirected to the corresponding tab$/) do
   expect("#{url.path}?#{url.query}").
     to eq(admin_status_path(tab: 'ontologies'))
 end
+
+Transform /^(-?\d+)$/ do |number|
+  number.to_i
+end
+
+Given(/^i have (\d+) (#{Ontology::States::STATES.join('|')}) ontologies$/) do |count, state|
+  @ontology_versions = FactoryGirl.create_list(:ontology_version, count, state: state)
+  @ontologies = @ontology_versions.map { |ov| ov.ontology }
+  @ontologies.each do |ontology|
+    expect(ontology.state).to eq(state)
+  end
+end
+
+When(/^i am on the processing ontologies tab$/) do
+  steps %Q{
+    And i navigate to the status page
+    When i click on the processing ontologies tab selector
+  }
+end
+
+Then(/^i should see the processing ontologies$/) do
+  encountered_names = Set.new
+  all('li.status_box .first-line span a').each do |anchor|
+    encountered_names << anchor.text
+  end
+  expect(encountered_names).to eq(@ontologies.map(&:name).to_set)
+end
