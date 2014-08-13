ceacc893576e7572731e3ad2f1ba2e5311e2de1c
Tim Reddehase <robustus@rightsrestricted.com>
2014-08-13 15:23:46 +0200

    fix unsucessful parsing by using post-receive
    Introduce new post-receive hook for scheduling
    sidekiq jobs on push.

--- git/lib/git_update.rb
+++ git/lib/git_update.rb
@@ -4,6 +4,25 @@ require 'ontohub_net'
 
 class GitUpdate
 
+  ENV_KEY_REFNAME = 'GIT_UPDATE_REFNAME'
+  ENV_KEY_OLDREV = 'GIT_UPDATE_OLDREV'
+  ENV_KEY_NEWREV = 'GIT_UPDATE_NEWREV'
+
+  def self.update_redis(repo_path, oldrev, newrev, refname, key_id)
+    Subprocess.run 'redis-cli', 'rpush', "#{Settings.redis_namespace}:queue:default", {
+      class: 'RepositoryUpdateWorker',
+      args: [repo_path, oldrev, newrev, refname, key_id]
+    }.to_json
+  end
+
+  def self.retrieve_update_environment
+    [
+      ENV[ENV_KEY_REFNAME],
+      ENV[ENV_KEY_OLDREV],
+      ENV[ENV_KEY_NEWREV],
+    ]
+  end
+
   def initialize(repo_path, key_id, refs)
     @repo_path = repo_path.strip
     @repo_name = repo_path.sub(Settings.git_root.to_s, '').
@@ -14,8 +33,16 @@ class GitUpdate
     @oldrev  = refs[1]
     @newrev  = refs[2]
 
+    set_update_environment
+
     @key_id = key_id
     @branch_name = /refs\/heads\/([\w\.-]+)/.match(@refname).to_a.last
+  end
+
+  def set_update_environment
+    ENV[ENV_KEY_REFNAME] = @refname
+    ENV[ENV_KEY_OLDREV] = @oldrev
+    ENV[ENV_KEY_NEWREV] = @newrev
   end
 
   def exec
@@ -23,7 +50,6 @@ class GitUpdate
     # we need to check user persmission per branch first
     if ssh?
       if api.allowed?('git-receive-pack', @repo_name, @key_id, @branch_name)
-        update_redis
         exit 0
       else
         STDERR.puts <<-MSG
@@ -35,7 +61,6 @@ for more information about permissions.
         exit 1
       end
     else
-      update_redis
       exit 0
     end
   rescue OntohubNet::UnexpectedStatusCodeError => error
@@ -58,10 +83,4 @@ If this issue persists please inform an Administrator.
     @key_id =~ /\Akey\-\d+\Z/
   end
 
-  def update_redis
-    Subprocess.run 'redis-cli', 'rpush', "#{Settings.redis_namespace}:queue:default", {
-      class: 'RepositoryUpdateWorker',
-      args: [@repo_path, @oldrev, @newrev, @refname, @key_id]
-    }.to_json
-  end
 end
