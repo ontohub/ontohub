a5970def208bdace216c80f5af1b16c2577ff37b
Sascha Graef <sgraef@informatik.uni-bremen.de>
2014-08-16 15:35:44 +0200

    make configuration vor develop again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,20 +5,23 @@ Ontohub::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
+
+  # Activate debug mode (and make browser fetch assets separately)
+  config.assets.debug = true
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
