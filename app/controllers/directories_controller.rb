65f3c2b94d123490783bb6e0a0302b5c8857ecd0
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-08-18 12:14:28 +0200

    add DirectoriesController

---
+++ app/controllers/directories_controller.rb
@@ -0,0 +1,40 @@
+class DirectoriesController < InheritedResources::Base
+  defaults resource_class: RepositoryDirectory
+  actions :new, :create
+
+  helper_method :repository
+  before_filter :check_write_permissions, only: [:new, :create]
+  before_filter :check_read_permissions
+
+  def new
+    resource
+  end
+
+  def create
+    if resource.valid?
+      resource.save
+      flash[:success] = "Successfully created the subdirectory."
+      redirect_to fancy_repository_path(repository, path: resource.target_path)
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def resource
+    @repository_directory ||= RepositoryDirectory.new(params.merge(user: current_user))
+  end
+
+  def repository
+    @repository ||= resource.repository
+  end
+
+  def check_read_permissions
+    authorize! :show, repository
+  end
+
+  def check_write_permissions
+    authorize! :write, repository
+  end
+end
