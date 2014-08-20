2328afd56bdca88e3275639b16f5a743fd0ee487
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-08-20 16:23:06 +0200

    add specs for access tokens

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -1,5 +1,54 @@
 require 'spec_helper'
 
 describe AccessToken do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:access_token) { create :access_token }
+  let(:repository) { create :repository, access_token: access_token }
+
+  context 'expired?' do
+    it 'should not be expired when the expiration date is in the future' do
+      expect(access_token.expired?).to be_falsy
+    end
+
+    it 'should be expired when the expiration date is in the past' do
+      access_token.expiration = (-1).minutes.from_now
+      expect(access_token.expired?).to be_truthy
+    end
+  end
+
+  context 'refresh' do
+    let!(:old_access_token) { access_token.dup }
+    before do
+      access_token.expiration = (-1).minutes.from_now
+    end
+
+    it 'should be expired before refresh' do
+      expect(access_token.expired?).to be_truthy
+    end
+
+    context 'after refresh' do
+      before do
+        access_token.refresh!
+      end
+
+      it 'should not be expired' do
+        expect(access_token.expired?).to be_falsy
+      end
+
+      it 'should have the same token' do
+        expect(access_token.token).to eq(old_access_token.token)
+      end
+    end
+  end
+
+  context 'replacement' do
+    let!(:replacement) { access_token.replace }
+
+    it 'should delete the old token' do
+      expect(access_token.persisted?).to be_falsy
+    end
+
+    it 'should generate another token' do
+      expect(replacement.token).not_to eq(access_token.token)
+    end
+  end
 end
