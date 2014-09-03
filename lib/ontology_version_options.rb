a7a5df95f6c7e21fdba17313f177b26a7004c28f
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-09-03 11:46:47 +0200

    fix code style according to hound comments

--- lib/ontology_version_options.rb
+++ lib/ontology_version_options.rb
@@ -1,8 +1,8 @@
 class OntologyVersionOptions
-
   attr_reader :filepath, :user, :fast_parse, :do_not_parse, :previous_filepath
 
-  def initialize(filepath, user, fast_parse: false, do_not_parse: false, previous_filepath: nil)
+  def initialize(filepath, user, fast_parse: false, do_not_parse: false,
+    previous_filepath: nil)
     @filepath = filepath
     @user = user
     @fast_parse = fast_parse
@@ -13,5 +13,4 @@ class OntologyVersionOptions
   def pre_saving_filepath
     previous_filepath || filepath
   end
-
 end
