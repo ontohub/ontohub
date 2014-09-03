36ec4872a8ea62879182f57759a24eb1bc708365
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-09-03 09:37:29 +0200

    delete repository asynchronously

--- app/models/repository/destroying.rb
+++ app/models/repository/destroying.rb
@@ -14,8 +14,13 @@ module Repository::Destroying
     super
   rescue StandardError => e
     unmark_as_destroying
-    raise e.class, "Can't delete repository: It contains
-    #{Settings.OMS.with_indefinite_article} that is imported by another repository."
+    raise e.class, I18n.t('repository.delete_error', oms: Settings.OMS.with_indefinite_article)
+  end
+
+  def destroy_asynchonously
+    self.destroying = true
+    save!
+    async(:destroy)
   end
 
   def can_be_deleted?
