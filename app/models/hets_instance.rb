301eaa407a5ed36d76b835614d49d25113c2154a
Tim Reddehase <robustus@rightsrestricted.com>
2014-09-04 12:15:21 +0200

    add functionality to determine up-state for hets
    Before save-actions occur we should perform
    a reachability test. This will allow us to
    check whether a hets-instance is 'up' and
    to retrieve the version which it is using.

--- app/models/hets_instance.rb
+++ app/models/hets_instance.rb
@@ -1,6 +1,8 @@
 class HetsInstance < ActiveRecord::Base
   attr_accessible :name, :uri
 
+  before_save :set_up_state
+
   # will result in 0.99 for <v0.99, something or other>
   def general_version
     version.split(', ').first[1..-1] if version
@@ -13,5 +15,21 @@ class HetsInstance < ActiveRecord::Base
 
   def up?
     up
+  end
+
+  protected
+  def check_up_state
+    Hets::VersionCaller.new(self).call
+  end
+
+  def set_up_state
+    version = check_up_state
+    self.up = !! version
+    self.version = version if up
+  end
+
+  def set_up_state!
+    set_up_state
+    save!
   end
 end
