17e57f34169ca7ed023832806a77f0e0613074f1
Tim Reddehase <robustus@rightsrestricted.com>
2014-09-04 12:14:29 +0200

    add missing webmock to rspec
    In order to mock web-requests we could/should
    use webmock instead of regular method-stubbing with
    rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require Rails.root.join('config', 'database_cleaner.rb')
+require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
