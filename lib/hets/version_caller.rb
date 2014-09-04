ddcf1a7ed9d595e97418e9bc607184006e57d606
Tim Reddehase <robustus@rightsrestricted.com>
2014-09-04 11:35:52 +0200

    add hets-version-caller into the mix
    The VersionCaller can retrieve the version of
    a running hets-instance by calling the restful
    API. It also serves as a sort of heartbeat in order
    to determine whether the hets-instance is
    reachable (internally called 'up').

---
+++ lib/hets/version_caller.rb
@@ -0,0 +1,18 @@
+module Hets
+  class VersionCaller < Caller
+
+    CMD = 'version'
+    METHOD = :get
+
+    def call
+      api_uri = build_api_uri(CMD)
+      perform(api_uri, METHOD).try(:body)
+    rescue UriFetcher::Error
+      Rails.logger.warn <<-MSG
+==HETS== Hets Instance <#{hets_instance}> is currently not reachable.
+      MSG
+      nil
+    end
+
+  end
+end
