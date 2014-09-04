c3475fcc513639b0a63f39a0d210fd1d2aa687ac
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-09-04 12:29:07 +0200

    fiy typo

--- app/models/repository/destroying.rb
+++ app/models/repository/destroying.rb
@@ -13,7 +13,7 @@ module Repository::Destroying
     raise e.class, I18n.t('repository.delete_error', oms: Settings.OMS)
   end
 
-  def destroy_asynchonously
+  def destroy_asynchronously
     Rails.logger.info "Mark #{self.class} #{self} (id: #{id}) as destroying"
     self.destroying = true
     save!
