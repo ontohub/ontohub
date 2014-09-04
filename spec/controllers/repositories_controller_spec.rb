adc9f41b460ce5975d4f22ccb7709d4718b25c36
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-09-04 15:21:06 +0200

    add specs for repository fork

--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -36,4 +36,45 @@ describe RepositoriesController do
     end
   end
 
+  context 'creating a clone', :process_jobs_synchronously do
+    context 'signed in' do
+      let(:user) { create :user }
+      let(:git_repository) { create :git_repository }
+      before do
+        sign_in user
+      end
+
+      context 'mirror' do
+        before do
+          post :create, repository: { name: 'repo',
+            source_address: "file://#{git_repository.path}" },
+            remote_type: 'mirror'
+        end
+
+        after do
+          FileUtils.rm_rf(git_repository.path)
+        end
+
+        it { should respond_with :found }
+        it { response.should redirect_to Repository.find_by_path('repo') }
+        it 'should have created a mirrored repository' do
+          expect(Repository.find_by_path('repo').mirror?).to be_truthy
+        end
+      end
+
+      context 'fork' do
+        before do
+          post :create, repository: { name: 'repo',
+            source_address: "file://#{git_repository.path}" },
+            remote_type: 'fork'
+        end
+
+        it { should respond_with :found }
+        it { response.should redirect_to Repository.find_by_path('repo') }
+        it 'should have created a non-mirrored repository' do
+          expect(Repository.find_by_path('repo').mirror?).to be_falsy
+        end
+      end
+    end
+  end
 end
--- spec/models/repository/remote_spec.rb
+++ spec/models/repository/remote_spec.rb
@@ -1,18 +1,33 @@
 require 'spec_helper'
 
 describe 'Repository Remote' do
-  let(:repository) { create :repository_with_remote }
+  context 'converting' do
+    let(:repository) { create :repository_with_remote }
 
-  it 'should be a mirrored reppository' do
-    expect(repository.mirror?).to be_truthy
-  end
-
-  context 'convert to local' do
-    before do
-      repository.convert_to_local!
+    it 'should be a mirrored reppository' do
+      expect(repository.mirror?).to be_truthy
     end
 
-    it 'should become a non-mirrored repository' do
+    context 'convert to local' do
+      before do
+        repository.convert_to_local!
+      end
+
+      it 'should become a non-mirrored repository' do
+        expect(repository.mirror?).to be_falsy
+      end
+    end
+  end
+
+  context 'forking', :process_jobs_synchronously do
+    let!(:repository) do
+      r = build :repository_with_remote
+      r.remote_type = 'fork'
+      r.save!
+      r.reload
+    end
+
+    it 'should be a non-mirrored repository' do
       expect(repository.mirror?).to be_falsy
     end
   end
