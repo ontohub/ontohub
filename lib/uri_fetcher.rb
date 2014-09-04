14446ef4d8181bc484358d51cd08c4ad6353bb2e
Tim Reddehase <robustus@rightsrestricted.com>
2014-09-04 10:57:17 +0200

    add mechanism to handle unfollowableness
    Add a custom errorclass to uri-fetcher in order to detect and handle
    unfollowable responses.  Unfollowable Response in our case actually
    means two things
    
    1. the response does not have a location header set, which would be
    needed in order to follow the response to a different location.
    2. the response itself is not a response which yields a body suitable
    for us. suitableness is determined through the overrideable
      has_actual_content? method.

--- lib/uri_fetcher.rb
+++ lib/uri_fetcher.rb
@@ -6,13 +6,16 @@ module UriFetcher
   def fetch_uri_content(uri, limit: 10, write_file: nil, prev_resp: nil)
     raise TooManyRedirectionsError.new(last_response: prev_resp) if limit == 0
     Net::HTTP.get_response(URI(uri)) do |response|
+      response.read_body
       if has_actual_content?(response)
         produce_response_body(response, write_file)
-      else
+      elsif response['location'] && !response['location'].empty?
         fetch_uri_content(response['location'],
                                     limit: limit-1,
                                     write_file: write_file,
                                     prev_resp: response)
+      else
+        raise UnfollowableResponseError.new(last_response: response)
       end
     end
   end
