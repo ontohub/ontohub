8ac6c70e5175910433f608ead8b42d47c1ed1363
Tim Reddehase <robustus@rightsrestricted.com>
2014-09-05 16:39:56 +0200

    add method to choose a hets-instance for actions.

--- app/models/hets_instance.rb
+++ app/models/hets_instance.rb
@@ -3,6 +3,10 @@ class HetsInstance < ActiveRecord::Base
 
   before_save :set_up_state
 
+  def self.choose
+    where(up: true).where('version >= ?', Hets.minimal_version_string).first
+  end
+
   # will result in 0.99 for <v0.99, something or other>
   def general_version
     version.split(', ').first[1..-1] if version
