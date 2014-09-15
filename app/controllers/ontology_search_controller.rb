25e680b4f0bbd68678787e4d349bc99e23ee0c73
Eileen Bolloff <ebolloff@fb3.uni-bremen.de>
2014-09-15 17:17:20 +0200

    There is a scope for each search filter in the
    view, the search results are filtered only if
    one filter is selected and shows all ontologies
    which has the selected attributes.

--- app/controllers/ontology_search_controller.rb
+++ app/controllers/ontology_search_controller.rb
@@ -7,12 +7,28 @@ class OntologySearchController < ApplicationController
       @search_response = Ontology.scoped
     end
 
+    unless params[:ontology_type].empty?
+      @search_response = @search_response.filter_by_ontology_type(params[:ontology_type])
     end
 
-
+    unless params[:project].empty?
+      @search_response = @search_response.filter_by_project(params[:project])
     end
 
+    unless params[:formality_level].empty?
+      @search_response = @search_response.filter_by_formality(params[:formality_level])
+    end
 
+    unless params[:license].empty?
+      @search_response = @search_response.filter_by_license(params[:license])
+    end
 
+    unless params[:task].empty?
+      @search_response = @search_response.filter_by_task(params[:task])
+    end
+
+    @search_response = paginate_for(@search_response)
+    render "shared/_ontology_search"
   end
+
 end
--- app/models/ontology/searching.rb
+++ app/models/ontology/searching.rb
@@ -10,6 +10,24 @@ module Ontology::Searching
       indexes :description, :analyzer => 'snowball'
     end
   end
+
+    scope :filter_by_ontology_type, ->(type_id) { where(ontology_type_id: type_id) }
+
+    scope :filter_by_project, ->(project_id) do
+      joins(:projects).where("projects.id = #{project_id}")
+    end
+
+    scope :filter_by_formality, ->(formality_id) do
+      where(formality_level_id: formality_id)
+    end
+
+    scope :filter_by_license, ->(license_id) do
+      joins(:license_models).where("license_models.id = #{license_id}")
+    end
+
+    scope :filter_by_task, ->(task_id) do
+      joins(:tasks).where("tasks.id = #{task_id}")
+    end
   end
 
 end
