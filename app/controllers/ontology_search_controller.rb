a72767dfae41be032792284ef42c3a12a45aa4fd
Eileen Bolloff <ebolloff@fb3.uni-bremen.de>
2014-09-15 17:13:46 +0200

    Made the ontology model searchable with
    elasticsearch, implemented basic search in the
    controller and build a view with filter options
    for it.

--- app/controllers/ontology_search_controller.rb
+++ app/controllers/ontology_search_controller.rb
@@ -1,42 +1,18 @@
-# Ontology search endpoints for the GWT search code.
 class OntologySearchController < ApplicationController
-  respond_to :json
-
-  def keywords
-    prefix = params[:prefix] || ''
-
-    if in_repository?
-      respond_with OntologySearch.new.make_repository_keyword_list_json(repository, prefix)
-    else
-      respond_with OntologySearch.new.make_global_keyword_list_json(prefix)
-    end
-  end
 
   def search
-    keywords = params[:keywords] || []
-
-    begin
-      keywords = keywords.map { |keyword| JSON.parse(keyword) }
-    rescue
-      raise ActionController::RoutingError.new('Not Found')
-    end
-
-    page = params[:page].to_i
-
-    if in_repository?
-      respond_with OntologySearch.new.make_bean_list_json(repository, keywords, page)
+    if params[:query].present?
+      @search_response = Ontology.search(params[:query]).records
     else
-      respond_with OntologySearch.new.make_bean_list_json(nil, keywords, page)
+      @search_response = Ontology.scoped
     end
-  end
 
-  def filters_map
-    respond_with OntologySearch.new.make_filters_map_json
-  end
+    end
 
-  private
 
-  def repository
-    Repository.find_by_path params[:repository_id]
+    end
+
+
+
   end
 end
--- app/models/ontology/searching.rb
+++ app/models/ontology/searching.rb
@@ -2,44 +2,14 @@ module Ontology::Searching
   extend ActiveSupport::Concern
 
   included do
-    searchable do
-      text :name
-      text :entities do
-        names = Array.new
-        entities.each do |symbol|
-          names.push symbol.display_name if symbol.display_name
-          names.push symbol.name if symbol.name
-          names.push symbol.text if symbol.text
-        end
-        names
-      end
-      text :logic do
-        logic.name if logic
-      end
-      integer :ontology_type_id do
-        ontology_type.id if ontology_type
-      end
-      integer :formality_level_id do
-        formality_level.id if formality_level
-      end
-      integer :project_ids, multiple: true
-      integer :repository_id
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
+
+    mapping do
+      indexes :name, type: 'string', :analyzer => 'snowball'
+      indexes :description, :analyzer => 'snowball'
     end
   end
-
-  module ClassMethods
-    def search_by_keywords(keywords, page, qualifiers)
-      Ontology.search do
-        keywords.each { |keyword| fulltext keyword }
-        with(:ontology_type_id, qualifiers[:ontology_type].id) if qualifiers[:ontology_type]
-        with(:repository_id, qualifiers[:repository].id) if qualifiers[:repository]
-        with(:license_model_id, qualifiers[:license_model].id) if qualifiers[:license_model]
-        with(:formality_level_id, qualifiers[:formality_level].id) if qualifiers[:formality_level]
-        with(:project_ids, [qualifiers[:project].id]) if qualifiers[:project]
-        with(:task_id, qualifiers[:task].id) if qualifiers[:task]
-        paginate page: page, per_page: 20
-      end
-    end
   end
 
 end
