a45d658683438fa58c32e5ce092af3b7c0277d45
Tim Reddehase <robustus@rightsrestricted.com>
2014-09-18 19:47:25 +0200

    fix hets-spec.

--- spec/lib/hets_spec.rb
+++ spec/lib/hets_spec.rb
@@ -8,34 +8,32 @@ describe Hets, :needs_hets do
 
   context 'Output directory parameter' do
     before do
-      xml_paths = Hets.parse Rails.root.join('test/fixtures/ontologies/owl/pizza.owl'), [], '/tmp'
+      xml_paths = Hets.parse(ontology_file('owl/pizza'), [], '/tmp')
       @xml_path = xml_paths.last
     end
 
     it 'correctly be used' do
-      assert @xml_path.starts_with? '/tmp'
+      expect(@xml_path.starts_with?('/tmp')).to be(true)
     end
   end
 
   %w(owl/pizza.owl owl/generations.owl clif/cat.clif).each do |path|
     context path do
+      let(:ontology) { create :ontology }
+      let(:user) { create :user }
+
       before do
-        xml_paths = Hets.parse Rails.root.join("test/fixtures/ontologies/#{path}"), [], '/tmp'
+        xml_paths = Hets.parse(ontology_file(path), [], '/tmp')
         @xml_path = xml_paths.last
         @pp_path = xml_paths.first
       end
 
       it 'have created output file' do
-        assert File.exists? @xml_path
+        expect(File.exists?(@xml_path)).to be(true)
       end
 
       it 'have generated importable output' do
-        assert_nothing_raised do
-          ontology = FactoryGirl.create :ontology
-          user = FactoryGirl.create :user
-          parse_this(user, ontology, @xml_path, @pp_path)
-          `git checkout #{@xml_path} 2>/dev/null`
-        end
+        expect { parse_this(user, ontology, @xml_path) }.not_to raise_error
       end
     end
   end
@@ -64,28 +62,10 @@ describe Hets, :needs_hets do
       Hets.parse(input_file, url_catalog, '/tmp')
       expect(Subprocess).to(have_received(:run).with(*catalog_args))
     end
-
-    # This test is disabled, because for this ontology, the XML output of hets
-    # contains _two_ DGNode tags, which is interpreted as distributed ontology
-    # by the ontology import mechanism (see app/models/ontology/import.rb).
-    # Funny though, the test did not fail always. Testing on the existence of
-    # the XML output file should suffice as hets currently only has to recognize
-    # the command line option for URL cataloges and does no URL substitutions.
-=begin
-    it 'have generated importable output' do
-      assert_nothing_raised do
-        ontology = FactoryGirl.create :ontology
-        user = FactoryGirl.create :user
-        ontology.import_xml_from_file @xml_path, @pp_path, user
-        `git checkout #{@xml_path} 2>/dev/null`
-      end
-    end
-=end
   end
 
   it 'raise exception if provided with wrong file-format' do
-    assert_raise Hets::ExecutionError do
-      Hets.parse Rails.root.join('test/fixtures/ontologies/xml/valid.xml')
-    end
+    expect { Hets.parse(ontology_file('valid.xml')) }.
+      to raise_error(Hets::ExecutionError)
   end
 end
