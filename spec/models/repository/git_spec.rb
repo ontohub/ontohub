93ab4d920761ff699aea28e1ea3852bdddb577d4
Tim Reddehase <robustus@rightsrestricted.com>
2014-09-23 13:12:01 +0200

    fix failing test with versioned-hets stubbing
    Also fix problems regarding the mass-commiting (for each it-block) into
    the factory-repository at least in this spec. We need to find a more
    general solution.

--- spec/models/repository/git_spec.rb
+++ spec/models/repository/git_spec.rb
@@ -6,13 +6,23 @@ describe Repository do
   let(:repository) { create :repository_with_remote }
 
   context 'when ontohub clones a remote repository' do
+
+    before do
+      stub_hets_for(hets_out_file('cat1'), with: 'cat', with_version: 1)
+      stub_hets_for(hets_out_file('cat2'), with: 'cat', with_version: 2)
+      stub_hets_for(hets_out_file('Px'), with: 'Px')
+    end
+
+    after do
+      FileUtils.rm_rf(repository.source_address)
+    end
+
     it 'should create a bulk job on a queue' do
       expect { repository.remote_send('clone') }.
         to change(OntologyBatchParseWorker.jobs, :size)
     end
 
     it 'should run the ontology import on the cloned repository', process_jobs_synchronously: true do
-      skip 'Fails cause it needs time-specific stubbing!'
       expect { repository.remote_send('clone') }.
         not_to raise_error
     end
