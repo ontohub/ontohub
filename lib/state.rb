6bc7a45b6fe9b01ddcbaaeb0d6a968df72456eb3
Sascha Graef <sgraef@informatik.uni-bremen.de>
2014-09-24 21:44:53 +0200

    make changes according to hound

--- lib/state.rb
+++ lib/state.rb
@@ -1,13 +1,11 @@
 module State
-
   STATES = %w(pending fetching processing done failed)
   TERMINAL_STATES = %w(failed done)
 
-  STATE_LABEL={pending: 'label-warning',
+  STATE_LABEL = {pending: 'label-warning',
      fetching: 'label-primary',
      processing: 'label-primary',
      done: 'label-success',
      failed: 'label-danger',
      }
-
 end
