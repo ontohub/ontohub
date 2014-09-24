fa927e42c081f94af30260ee79feb3255d6e5569
Sascha Graef <sgraef@informatik.uni-bremen.de>
2014-09-24 21:31:26 +0200

    introduce state module

---
+++ lib/state.rb
@@ -0,0 +1,13 @@
+module State
+
+  STATES = %w(pending fetching processing done failed)
+  TERMINAL_STATES = %w(failed done)
+
+  STATE_LABEL={pending: 'label-warning',
+     fetching: 'label-primary',
+     processing: 'label-primary',
+     done: 'label-success',
+     failed: 'label-danger',
+     }
+
+end
