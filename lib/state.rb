73987a07eb7a7b5985e9bd479e42b33650d2913f
Sascha Graef <sgraef@informatik.uni-bremen.de>
2014-09-29 10:25:03 +0200

    change code style according to style guide

--- lib/state.rb
+++ lib/state.rb
@@ -2,7 +2,8 @@ module State
   STATES = %w(pending fetching processing done failed)
   TERMINAL_STATES = %w(done failed)
 
-  STATE_LABEL = {pending: 'label-warning',
+  STATE_LABEL = {
+     pending: 'label-warning',
      fetching: 'label-primary',
      processing: 'label-primary',
      done: 'label-success',
