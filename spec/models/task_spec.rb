a8b44c144d607c18209804f032dfc213cb2aba0f
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-10-11 12:01:58 +0200

    Move task_test to rspec.

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Task do
+  context 'Migrations' do
+    it { should have_db_column(:name).of_type(:text) }
+    it { should have_db_column(:description).of_type(:text) }
+    it { should have_db_index(:name).unique(true) }
+  end
+
+  context 'Validations' do
+    context 'when no name is taken' do
+      it { should allow_value('foo').for :name }
+    end
+
+    context 'when name is already taken' do
+      before do
+        Task.create!(name: 'foo')
+      end
+      it { should_not allow_value('foo').for :name }
+    end
+
+    context 'when Task without name is to be saved' do
+      it 'raise error' do
+        expect { Task.create! }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+end
