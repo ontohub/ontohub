e4f36cc6b6d3d4c1bcc5de4a9d0fbfbfcb9ee922
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-10-11 10:13:13 +0200

    Move formality_level_test to rspec.

---
+++ spec/models/formality_level_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe FormalityLevel do
+  context 'Migrations' do
+    %w( name description ).each do |column|
+      it { should have_db_column(column).of_type(:text) }
+    end
+
+    it { should have_db_index(:name).unique(true) }
+  end
+
+  context 'Validations' do
+    context 'when no name is taken' do
+      it { should allow_value('foo').for(:name) }
+    end
+
+    context "when 'foo' is already taken" do
+      before do
+        FormalityLevel.create!(name: 'foo')
+      end
+
+      it { should_not allow_value('foo').for(:name) }
+    end
+
+    context 'when FormalityLevel without name is to be saved' do
+      it 'raise error' do
+        expect { FormalityLevel.create! }.
+          to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+end
