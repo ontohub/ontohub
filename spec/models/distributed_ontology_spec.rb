07a9c502793886e97a49b1c8180c590852f3cb65
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-10-12 10:16:09 +0200

    Move distributed_ontology_test to rspec.

---
+++ spec/models/distributed_ontology_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe DistributedOntology do
+  context 'associations' do
+    it { should have_many(:children) }
+  end
+
+  context 'distributed ontology instance' do
+    let(:ontology) do
+      FactoryGirl.create :distributed_ontology, iri: 'http://example.com/foo'
+    end
+
+    it 'generate sub iri' do
+      expect(ontology.iri_for_child('bar')).to eq('http://example.com/foo?bar')
+    end
+
+    it 'generate sub iri that already is an iri' do
+      expect(ontology.iri_for_child('http://example.com/dummy')).
+        to eq('http://example.com/dummy')
+    end
+  end
+end
