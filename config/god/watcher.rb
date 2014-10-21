a3120a9a5fea37cdc151a9194a4fd892ed3007c1
Tim Reddehase <robustus@rightsrestricted.com>
2014-10-21 16:17:21 +0200

    provide a default watcher-system
    It should be extensible for other jobs.

---
+++ config/god/watcher.rb
@@ -0,0 +1,74 @@
+class Watcher
+  RAILS_ROOT = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + "/../.."
+
+  attr_accessor :count
+
+  def self.configure(&block)
+    new.instance_eval(&block)
+  end
+
+  def initialize
+    self.count = 0
+  end
+
+  def group
+  end
+
+  def start_cmd(*args)
+  end
+
+  def pid_file
+  end
+
+  def watch(*args)
+    self.count += 1
+
+    God.watch do |w|
+      w.dir           = RAILS_ROOT
+      w.group         = group
+      w.name          = "#{group}-#{@count}"
+      w.pid_file      = pid_file
+
+      w.interval      = 30.seconds
+      w.start         = start_cmd(*args)
+      w.start_grace   = 10.seconds
+
+      # Restart if memory gets too high
+      w.transition(:up, :restart) do |on|
+        on.condition(:memory_usage) do |c|
+          c.above = 350.megabytes
+          c.times = 2
+        end
+      end
+
+      # Determine the state on startup
+      w.transition(:init, { true => :up, false => :start }) do |on|
+        on.condition(:process_running) do |c|
+          c.running = true
+        end
+      end
+
+      # Determine when process has finished starting
+      w.transition([:start, :restart], :up) do |on|
+        on.condition(:process_running) do |c|
+          c.running = true
+          c.interval = 5.seconds
+        end
+
+        # Failsafe
+        on.condition(:tries) do |c|
+          c.times = 5
+          c.transition = :start
+          c.interval = 5.seconds
+        end
+      end
+
+      # Start if process is not running
+      w.transition(:up, :start) do |on|
+        on.condition(:process_running) do |c|
+          c.running = false
+        end
+      end
+    end
+  end
+end
