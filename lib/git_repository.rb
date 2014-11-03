389ef6f0a2c389ad624dcf0b0e7336e39c3fbec5
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-11-03 08:41:56 +0100

    Do what the hound says.

--- lib/git_repository.rb
+++ lib/git_repository.rb
@@ -32,7 +32,7 @@ class GitRepository
     FileUtils.rmtree(self.path)
   end
 
-  def dir?(path, commit_oid=nil)
+  def dir?(path, commit_oid = nil)
     path ||= '/'
     if empty?
       return false
@@ -44,7 +44,7 @@ class GitRepository
     !object.nil? && object.type == :tree
   end
 
-  def path_exists?(path, commit_oid=nil)
+  def path_exists?(path, commit_oid = nil)
     rugged_commit = get_commit(commit_oid)
     if !rugged_commit && path.empty?
       true
@@ -53,7 +53,7 @@ class GitRepository
     end
   end
 
-  def paths_starting_with(path, commit_oid=nil)
+  def paths_starting_with(path, commit_oid = nil)
     dir = dir?(path, commit_oid) ? path : path.split('/')[0..-2].join('/')
     contents = folder_contents(commit_oid, dir)
 
@@ -61,7 +61,7 @@ class GitRepository
       select { |p| p.starts_with?(path) }
   end
 
-  def get_file(path, commit_oid=nil)
+  def get_file(path, commit_oid = nil)
     begin
       get_file!(path, commit_oid)
     rescue GitRepository::PathNotFoundError
@@ -69,7 +69,7 @@ class GitRepository
     end
   end
 
-  def get_file!(path, commit_oid=nil)
+  def get_file!(path, commit_oid = nil)
     path ||= '/'
     rugged_commit = get_commit(commit_oid)
     raise GitRepository::PathNotFoundError if !rugged_commit && path.empty?
@@ -77,7 +77,7 @@ class GitRepository
     GitFile.new(self, rugged_commit, path)
   end
 
-  def get_path_of_dir(oid=nil, path=nil)
+  def get_path_of_dir(oid = nil, path = nil)
     path ||= ''
     path = path[0..-2] if(path[-1] == '/')
     raise PathNotFoundError.new unless path_exists?(path, oid)
@@ -89,23 +89,32 @@ class GitRepository
     path.split("/")[0..-2].join("/")
   end
 
-  # given a commit oid or a branch name, commit_id returns a hash of oid and branch name if existent
-  def commit_id(oid)
-    return { oid: head_oid, branch_name: 'master' } if oid.nil?
-    if oid.match(/[0-9a-fA-F]{40}/)
-      branch_names = branches.select { |b| b[:oid] == oid }
-      if branch_names.empty?
-        { oid: oid, branch_name: nil }
-      else
-        { oid: oid, branch_name: branch_names[0][:name] }
-      end
+  # Given a commit oid or a branch name, commit_id returns a hash of oid and
+  # branch name if existent.
+  def commit_id(ref)
+    return {oid: head_oid, branch_name: 'master'} if ref.nil?
+    if ref.match(/[0-9a-fA-F]{40}/)
+      commit_id_by_oid(ref)
     else
-      if branch_oid(oid).nil?
-        nil
-      else
-        { oid: branch_oid(oid), branch_name: oid }
-      end
+      commit_id_by_branch_name(ref)
     end
+  end
+
+  def commit_id_by_oid(oid)
+    branch_names = branches_by_oid(oid)
+    {oid: oid, branch_name: branch_names.empty? ? nil : branch_names[0][:name]}
+  end
+
+  def commit_id_by_branch_name(name)
+    if branch_oid(name).nil?
+      nil
+    else
+      {oid: branch_oid(name), branch_name: name}
+    end
+  end
+
+  def branches_by_oid(oid)
+    branches.select { |b| b[:oid] == oid }
   end
 
   def branches
@@ -114,7 +123,7 @@ class GitRepository
         refname: ref.name,
         name: ref.name.split('/')[-1],
         commit: ref.target,
-        oid: ref.target.oid
+        oid: ref.target.oid,
       }
     end
   end
@@ -186,7 +195,7 @@ class GitRepository
 
   protected
 
-  def path_exists_rugged?(rugged_commit, path='/')
+  def path_exists_rugged?(rugged_commit, path = '/')
     path.empty? || !get_object(rugged_commit, path).nil?
   rescue Rugged::OdbError
     false
