01caefdd3281d4c9b1a601193ecf0edb27c35f54
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-11-06 17:04:56 +0100

    Remove new action on repository_directories.

--- spec/routing/repository_directories_routing_spec.rb
+++ spec/routing/repository_directories_routing_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe 'RepositoryDirectories Routing' do
-  it { should route(:get, 'repositories/repopath/repository_directories/new').
-    to(controller: 'repository_directories', action: :new, repository_id: 'repopath' ) }
   it { should route(:post, 'repositories/repopath/repository_directories').
     to(controller: 'repository_directories', action: :create, repository_id: 'repopath' ) }
 end
