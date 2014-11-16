e792c3a2a96506472befc8c6fe607cd64817b7ff
Tim Reddehase <robustus@rightsrestricted.com>
2014-11-16 13:23:20 +0100

    set displaytext in sentence only if non-nil.

--- app/models/sentence/readability.rb
+++ app/models/sentence/readability.rb
@@ -2,17 +2,19 @@ module Sentence::Readability
   extend ActiveSupport::Concern
 
   def set_display_text!
-    new_text = self.text.dup
+    if text
+      new_text = self.text.dup
 
-    self.entities.each do |entity|
-      unless entity.display_name.nil?
-        new_text.gsub!(entity.iri, entity.display_name)
+      self.entities.each do |entity|
+        unless entity.display_name.nil?
+          new_text.gsub!(entity.iri, entity.display_name)
+        end
       end
+
+      new_text.gsub!(/\s+/, ' ')
+
+      self.display_text = new_text
+      save!
     end
-
-    new_text.gsub!(/\s+/, ' ')
-
-    self.display_text = new_text
-    save!
   end
 end
