44cc5effed3fe07515ab5d873d73fe9ef91f5a28
Tim Reddehase <robustus@rightsrestricted.com>
2014-11-18 15:13:44 +0100

    save the logic after adding the slug.

--- db/migrate/20141023133500_create_slug_for_existing_logics.rb
+++ db/migrate/20141023133500_create_slug_for_existing_logics.rb
@@ -2,12 +2,18 @@ class CreateSlugForExistingLogics < ActiveRecord::Migration
   def up
     methods_needed = [:set_slug, :slug, :slug=]
     performable = methods_needed.all? { |m| Logic.new.respond_to?(m, true) }
-    Logic.find_each { |l| l.send(:set_slug) } if performable
+    Logic.find_each do |logic|
+      logic.send(:set_slug)
+      logic.save!
+    end if performable
   end
 
   def down
     methods_needed = [:set_slug, :slug, :slug=]
     performable = methods_needed.all? { |m| Logic.new.respond_to?(m, true) }
-    Logic.find_each { |l| l.send(:slug=, nil) } if performable
+    Logic.find_each do |logic|
+      logic.send(:slug=, nil)
+      logic.save!
+    end if performable
   end
 end
