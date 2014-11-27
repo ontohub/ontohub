6d004cf3c75a44595b9a62796b6f56785b8af774
Tim Reddehase <robustus@rightsrestricted.com>
2014-11-27 10:31:26 +0100

    refactor graphscontroller for maintainability
    It should also increase the readability.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -10,32 +10,7 @@ class GraphsController < InheritedResources::Base
       @ontology = parent if parent.is_a?(Ontology)
       @depth = params[:depth] ? params[:depth].to_i : 3
       format.html
-      format.json do
-        fetcher = GraphDataFetcher.new(center: parent, depth: @depth)
-        nodes, edges = fetcher.fetch
-        nodes_aggregate = nodes.reduce({}) do |mem, node|
-          mem[node.id] = node.aggregate
-          mem
-        end
-        # edges_aggregate = edges.reduce({}) do |mem, edge|
-        #   mem[edge.id] = edge.aggregate
-        #   mem
-        # end
-        chain = resource_chain
-        if chain.empty?
-          chain << nodes.first.class
-        else
-          chain[-1] = nodes.first.class
-        end
-        data = {nodes: nodes,
-                edges: edges,
-                center: parent,
-                node_url: nodes.any? ? url_for(chain) : nil,
-                edge_url: edges.any? ? url_for(edges.first.class) : nil,
-                edge_type: GraphDataFetcher.link_for(parent.class),
-                nodes_aggregate: nodes_aggregate }
-        respond_with data
-      end
+      format.json { respond_with graph_data }
     end
   end
 
@@ -43,5 +18,40 @@ class GraphsController < InheritedResources::Base
 
   def check_read_permissions
     authorize! :show, parent.repository if parent.is_a? Ontology
+  end
+
+  def graph_data
+    nodes_aggregate = nodes.reduce({}) do |mem, node|
+      mem[node.id] = node.aggregate
+      mem
+    end
+    data = {
+      nodes: nodes,
+      edges: edges,
+      center: parent,
+      node_url: nodes.any? ? url_for(graph_resource_chain) : nil,
+      edge_url: edges.any? ? url_for(edges.first.class) : nil,
+      edge_type: GraphDataFetcher.link_for(parent.class),
+      nodes_aggregate: nodes_aggregate
+    }
+  end
+
+  def graph_resource_chain
+    chain = resource_chain
+    if chain.empty?
+      chain << nodes.first.class
+    else
+      chain[-1] = nodes.first.class
+    end
+  end
+
+  def nodes
+    @fetch_result ||= GraphDataFetcher.new(center: parent, depth: @depth).fetch
+    @fetch_result.first
+  end
+
+  def edges
+    @fetch_result ||= GraphDataFetcher.new(center: parent, depth: @depth).fetch
+    @fetch_result.last
   end
 end
