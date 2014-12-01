7aa257096578ae891833367f3e2cc75ded9dbd36
Eileen Bolloff <ebolloff@fb3.uni-bremen.de>
2014-12-01 16:14:26 +0100

    Allow localhost http connections for rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ require Rails.root.join('config', 'database_cleaner.rb')
 require 'addressable/template'
 require 'webmock/rspec'
 
+WebMock.disable_net_connect!(allow_localhost: true)
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
