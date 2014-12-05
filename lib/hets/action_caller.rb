35381478e3322d10eff9bcaed64479fdfe6569fb
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-12-05 11:02:46 +0100

    Add ProveCaller and ActionCaller
    The new ProveCaller and the ParseCaller share some code which is
    extracted to the new ActionCaller.

---
+++ lib/hets/action_caller.rb
@@ -0,0 +1,19 @@
+module Hets
+  class ActionCaller < Caller
+
+    attr_accessor :url_catalog
+
+    def initialize(hets_instance, url_catalog = [])
+      self.url_catalog = url_catalog
+      msg = "<#{hets_instance}> not up."
+      raise Hets::InactiveInstanceError, msg unless hets_instance.up?
+      super(hets_instance)
+    end
+
+    def build_query_string
+      query_hash = {}
+      query_hash[:"url-catalog"] = url_catalog.join(',') if url_catalog.present?
+      query_hash
+    end
+  end
+end
