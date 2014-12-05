c179ee56a7e82755d4c3018d5adb42465609be98
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-12-05 11:03:11 +0100

    Add prove_via_api method.

--- lib/hets.rb
+++ lib/hets.rb
@@ -110,6 +110,11 @@ we expected it to be matchable by this regular expression:
     parse_caller.call(qualified_loc_id_for(resource), with_mode: mode)
   end
 
+  def self.prove_via_api(resource, url_catalog = [])
+    prove_caller = Hets::ProveCaller.new(HetsInstance.choose, url_catalog)
+    prove_caller.call(resource.versioned_iri)
+  end
+
   def self.filetype(resource)
     iri = qualified_loc_id_for(resource)
     filetype_caller = Hets::FiletypeCaller.new(HetsInstance.choose)
