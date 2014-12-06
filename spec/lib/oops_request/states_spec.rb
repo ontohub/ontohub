e3692177e816b386d44d153c412816e0eb7ffebf
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-12-06 21:14:06 +0100

    Completely rewrite oops_request/states_spec.

--- spec/lib/oops_request/states_spec.rb
+++ spec/lib/oops_request/states_spec.rb
@@ -1,58 +1,42 @@
 require 'spec_helper'
 
 describe 'OopsRequest::States' do
-  context 'new oops request' do
-    let(:request) { create :oops_request }
-    before { allow_any_instance_of(OopsRequest).to(receive(:async_run)) }
+  let(:request) { create :oops_request }
+  before { allow(request).to receive(:execute_and_save) }
 
-    it 'be pending' do
-      expect(request.state).to eq('pending')
-    end
+  it 'have state pending' do
+    expect(request.state).to eq('pending')
+  end
 
-    context 'aftert processed with error' do
-      let(:message) { 'some error message' }
-      before do
-        allow_any_instance_of(OopsRequest).to(receive(:execute_and_save).
-          and_raise(Oops::Error, message))
-      end
-
-      it 'raises error' do
-        allow_any_instance_of(OopsRequest).to(receive(:execute_and_save).
-          and_raise(Oops::Error, message))
-        expect { request.run }.to raise_error(Oops::Error)
-      end
-
-      context 'after run' do
-        before do
-          begin
-            request.run
-          rescue Oops::Error
-          end
-        end
-
-        it 'set state to failed' do
-          expect(request.state).to eq('failed')
-        end
-
-        it 'state the error message' do
-          expect(request.last_error).to eq(message)
-        end
-      end
-    end
-
-    context 'aftert processed without error' do
-      before do
-        allow_any_instance_of(OopsRequest).to receive(:execute_and_save)
+  context 'with an error' do
+    let(:message) { 'error from stub' }
+    before do
+      allow(request).to receive(:execute_and_save).
+        and_raise(Oops::Error, message)
+      begin
         request.run
+      rescue Oops::Error
       end
+    end
 
-      it 'set state to done' do
-        expect(request.state).to eq('done')
-      end
+    it 'have state failed' do
+      expect(request.state).to eq('failed')
+    end
 
-      it 'not have a errors message' do
-        expect(request.last_error).to be_nil
-      end
+    it 'have the error message' do
+      expect(request.last_error).to eq(message)
+    end
+  end
+
+  context 'without an error' do
+    before { request.run }
+
+    it 'have state done' do
+      expect(request.state).to eq('done')
+    end
+
+    it 'have no error' do
+      expect(request.last_error).to be_nil
     end
   end
 end
