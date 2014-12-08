e7208592e0a2caaa53ab413a90c2f1f3980643e6
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-12-08 11:32:14 +0100

    Rename request to oops_request.

--- spec/lib/oops_request/states_spec.rb
+++ spec/lib/oops_request/states_spec.rb
@@ -1,42 +1,42 @@
 require 'spec_helper'
 
 describe 'OopsRequest::States' do
-  let(:request) { create :oops_request }
-  before { allow(request).to receive(:execute_and_save) }
+  let(:oops_request) { create :oops_request }
+  before { allow(oops_request).to receive(:execute_and_save) }
 
   it 'have state pending' do
-    expect(request.state).to eq('pending')
+    expect(oops_request.state).to eq('pending')
   end
 
   context 'with an error' do
     let(:message) { 'error from stub' }
     before do
-      allow(request).to receive(:execute_and_save).
+      allow(oops_request).to receive(:execute_and_save).
         and_raise(Oops::Error, message)
       begin
-        request.run
+        oops_request.run
       rescue Oops::Error
       end
     end
 
     it 'have state failed' do
-      expect(request.state).to eq('failed')
+      expect(oops_request.state).to eq('failed')
     end
 
     it 'have the error message' do
-      expect(request.last_error).to eq(message)
+      expect(oops_request.last_error).to eq(message)
     end
   end
 
   context 'without an error' do
-    before { request.run }
+    before { oops_request.run }
 
     it 'have state done' do
-      expect(request.state).to eq('done')
+      expect(oops_request.state).to eq('done')
     end
 
     it 'have no error' do
-      expect(request.last_error).to be_nil
+      expect(oops_request.last_error).to be_nil
     end
   end
 end
