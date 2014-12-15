5d9c7cb90e4a0b5417683cbf35ec59e7e7284e78
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-12-15 20:23:51 +0100

    Adjust class name.

--- spec/routing/symbols_search_routing_spec.rb
+++ spec/routing/symbols_search_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe EntitiesSearchController do
+describe SymbolsSearchController do
   it do
     should route(:get, 'symbols_search').to(
       controller: :symbols_search, action: :index)
