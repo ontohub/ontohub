8ad47f4edef58b0df66752810e7fa49cb03e8736
Model Renamer <bot-rename@ontohub.org>
2014-12-15 19:02:52 +0100

    Revert renaming in and of link_helper and related.

--- lib/external_mapping.rb
+++ lib/external_mapping.rb
@@ -1,6 +1,6 @@
 module ExternalMapping
 
-  def generate_external_mapping(controller, action, second_level, external_target)
+  def generate_external_link(controller, action, second_level, external_target)
     mappings = Ontohub::Application.config.external_url_mapping[external_target]
     root = mappings['root']
     target = get_mapping_for mappings, second_level, controller, action
