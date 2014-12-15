6839dc73f207fd407cb9444f3a2ffb81bad79422
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2014-12-15 20:18:57 +0100

    Revert renaming in spec/lib/external_mapping_spec.rb.

--- spec/lib/external_mapping_spec.rb
+++ spec/lib/external_mapping_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 include ExternalMapping
 describe ExternalMapping do
 
-  context 'external_wiki_mappings' do
+  context 'external_wiki_links' do
 
     before do
       @mappings = Ontohub::Application.config.external_url_mapping["wiki"]
       @root = @mappings["root"]
     end
 
-    it 'should mapping to wiki root for non existing actions' do
-      mapping = @root+(get_mapping_for @mappings, "controller", "graphs", "show")
-      mapping.should == @root
+    it 'should link to wiki root for non existing actions' do
+      link = @root+(get_mapping_for @mappings, "controller", "graphs", "show")
+      link.should == @root
     end
 
-    it 'should mapping to specific wiki page' do
-      mapping = @root+(get_mapping_for @mappings, "controller", "graphs", "index")
-      mapping.should == @root+"show_graph"
+    it 'should link to specific wiki page' do
+      link = @root+(get_mapping_for @mappings, "controller", "graphs", "index")
+      link.should == @root+"show_graph"
     end
   end
 end
