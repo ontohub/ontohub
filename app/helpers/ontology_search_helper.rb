e2f4b0dc2761e746cbe9344edd9c8751d22edb68
Eileen Bolloff <ebolloff@fb3.uni-bremen.de>
2015-01-22 20:37:51 +0100

    Fix code duplication in view.

---
+++ app/helpers/ontology_search_helper.rb
@@ -0,0 +1,20 @@
+module OntologySearchHelper
+
+  def render_filter(klass, include_blank='', selected_key: nil)
+    name = klass.name.underscore.to_sym
+    render 'shared/ontology_search_filter', {
+      name: name,
+      filter: filter_list_for(klass),
+      include_blank: include_blank,
+      selected: params[selected_key]
+    }
+  end
+
+  def filter_list_for(klass)
+    if @repository_id
+      klass.not_empty.where(ontologies: {repository_id: @repository_id})
+    else
+      klass.not_empty
+    end
+  end
+end
