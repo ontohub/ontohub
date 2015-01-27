ea3b1dcb33fa22494493d7c540f0acedd5714631
Tim Reddehase <robustus@rightsrestricted.com>
2015-01-27 14:45:27 +0100

    add commit and branch references as well
    Additionally to version-numbers the /ref syntax now also supports
    commits and branches.

--- lib/ontology_version_finder.rb
+++ lib/ontology_version_finder.rb
@@ -1,4 +1,6 @@
 class OntologyVersionFinder
+  SUPPORTED_BRANCHES = %w(master)
+
   attr_accessor :reference, :ontology
 
   def self.applicable_reference?(reference)
@@ -11,17 +13,49 @@ class OntologyVersionFinder
   end
 
   def find
+    with_version_reference ||
+      with_commit_reference ||
+      with_branch_reference
+  end
+
+  def with_version_reference
     OntologyVersion.where(ontology_id: ontology,
                           number: version_reference).first
   end
 
+  def with_commit_reference
+    OntologyVersion.where(ontology_id: ontology,
+                          commit_oid: commit_reference).first
+  end
+
+  def with_branch_reference
+    branch_commit = ontology.repository.commit_id(branch_reference)[:oid]
+    OntologyVersion.where(ontology_id: ontology,
+                          commit_oid: branch_commit).first
+  end
+
   def applicable?
-    !! version_reference
+    result = version_reference ||
+      commit_reference ||
+      branch_reference
+    !! result
   end
 
   def version_reference
     if reference =~ /\A\d+\Z/
       reference.to_i
+    end
+  end
+
+  def commit_reference
+    if reference =~ /\A[a-fA-F0-9]+\Z/
+      reference
+    end
+  end
+
+  def branch_reference
+    if SUPPORTED_BRANCHES.include?(reference)
+      reference
     end
   end
 end
