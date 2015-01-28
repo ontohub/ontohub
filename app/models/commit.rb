a64b89f298e3973ed3c46b5035a644a38d53649c
Tim Reddehase <robustus@rightsrestricted.com>
2015-01-28 16:43:29 +0100

    add convencience relations to commit
    We should easily get all ontology_versions that
    were created due to a commit and all ontologies
    that were influenced by a commit.

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,5 +3,8 @@ class Commit < ActiveRecord::Base
   attr_accessible :author, :author_date
   attr_accessible :commit_date, :committer
 
+  has_many :ontology_versions
+  has_many :ontologies, through: :ontology_versions
+
   belongs_to :repository
 end
