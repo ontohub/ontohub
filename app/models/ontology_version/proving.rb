351107dffecdb0b4534a4ee4c84485180571bbcc
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-01-31 10:48:17 +0100

    Add OntologyVersion::Proving module.

---
+++ app/models/ontology_version/proving.rb
@@ -0,0 +1,34 @@
+module OntologyVersion::Proving
+
+  extend ActiveSupport::Concern
+  include Hets::ErrorHandling
+
+  included do
+    @queue = 'hets'
+  end
+
+  def async_prove(*args)
+    async :prove
+  end
+
+  def prove
+    update_state! :processing
+
+    do_or_set_failed do
+      cmd, input_io = execute_proof
+      return if cmd == :abort
+
+      ontology.import_proof(self, self.user, input_io)
+      update_state! :done
+    end
+  end
+
+  # generate XML by passing the raw ontology to Hets
+  def execute_proof
+    input_io = Hets.prove_via_api(ontology, ontology.repository.url_maps)
+    [:all_is_well, input_io]
+  rescue Hets::ExecutionError => e
+    handle_hets_execution_error(e, self)
+    [:abort, nil]
+  end
+end
