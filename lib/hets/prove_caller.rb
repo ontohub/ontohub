d4b758e035c041ce6c511848f01ceab3c104d395
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-02-06 16:47:36 +0100

    Only prove theorems of current node.

--- lib/hets/prove_caller.rb
+++ lib/hets/prove_caller.rb
@@ -5,11 +5,11 @@ module Hets
 
     PROVE_OPTIONS = {format: 'json', include: 'true'}
 
-    def call(iri)
+    def call(iri, options = {})
       escaped_iri = Rack::Utils.escape_path(iri)
       arguments = [escaped_iri]
       api_uri = build_api_uri(CMD, arguments, build_query_string)
-      perform(api_uri, PROVE_OPTIONS, METHOD)
+      perform(api_uri, PROVE_OPTIONS.merge(options), METHOD)
     end
   end
 end
