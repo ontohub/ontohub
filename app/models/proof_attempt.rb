06520b65b66a843c9761e7ac0b0d149f3c3a8ac3
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-02-07 12:07:09 +0100

    Add used_theorems to ProofAttempt
    Because Theorem is a subclass of Sentence and we have a default scope
    of Sentence that excludes theorems, we need to have a second relation
    to get our hands on all the used axioms (sentences and theorems) of a
    proof.

--- app/models/proof_attempt.rb
+++ app/models/proof_attempt.rb
@@ -4,12 +4,20 @@ class ProofAttempt < ActiveRecord::Base
   has_and_belongs_to_many :used_axioms,
     class_name: 'Sentence',
     join_table: 'used_axioms_proof_attempts'
+  has_and_belongs_to_many :used_theorems,
+    class_name: 'Theorem',
+    association_foreign_key: 'sentence_id',
+    join_table: 'used_axioms_proof_attempts'
 
   attr_accessible :prover, :prover_output, :tactic_script, :time_taken
 
   validates_presence_of :theorem
 
   after_save :update_theorem_status
+
+  def used_sentences
+    @used_sentences ||= used_axioms + used_theorems
+  end
 
   def update_theorem_status
     theorem.update_proof_status(proof_status)
