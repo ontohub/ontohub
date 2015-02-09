b45327792cf88ddfc35e62ad3a7179aeadb79f51
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-02-09 11:38:07 +0100

    Add migration to evaluate .omn files.

---
+++ db/migrate/20150209103247_add_omn_to_ontology_file_extensions.rb
@@ -0,0 +1,11 @@
+class AddOmnToOntologyFileExtensions < ActiveRecord::Migration
+  def up
+      ActiveRecord::Base.connection.
+        execute("INSERT INTO ontology_file_extensions (extension, distributed) VALUES ('.omn', 'false')")
+  end
+
+  def down
+      ActiveRecord::Base.connection.
+        execute("DELETE FROM ontology_file_extensions WHERE extension='.omn'")
+  end
+end
