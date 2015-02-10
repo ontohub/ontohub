5984251c837b89b3f5aacaaae23463ce80cf8092
Tim Reddehase <robustus@rightsrestricted.com>
2015-02-10 13:12:00 +0100

    fix oops-response query for entities->symbols.

--- app/models/oops_response.rb
+++ app/models/oops_response.rb
@@ -2,9 +2,9 @@ class OopsResponse < ActiveRecord::Base
   belongs_to :request, class_name: 'OopsRequest'
   has_and_belongs_to_many :symbols, class_name: 'OntologyMember::Symbol'
 
-  scope :global, joins('LEFT JOIN symbols_oops_responses ON '\
-    'oops_responses.id = symbols_oops_responses.oops_response_id').
-    where('symbols_oops_responses.symbol_id' => nil)
+  scope :global, joins('LEFT JOIN oops_responses_symbols ON '\
+    'oops_responses.id = oops_responses_symbols.oops_response_id').
+    where('oops_responses_symbols.symbol_id' => nil)
 
   attr_accessor :affects
 
