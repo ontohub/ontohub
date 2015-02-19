f0c4b737bfd7753a27b1cc4ee12daec5edda4382
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-02-19 14:14:17 +0100

    Add Numbering to ProofAttempt.

--- app/models/proof_attempt.rb
+++ app/models/proof_attempt.rb
@@ -1,4 +1,8 @@
 class ProofAttempt < ActiveRecord::Base
+  include Numbering
+
+  numbering_parent_column 'sentence_id'
+
   belongs_to :theorem, foreign_key: 'sentence_id'
   belongs_to :proof_status
   has_many :generated_axioms, dependent: :destroy
@@ -10,11 +14,17 @@ class ProofAttempt < ActiveRecord::Base
                           association_foreign_key: 'sentence_id',
                           join_table: 'used_axioms_proof_attempts'
 
-  attr_accessible :prover, :prover_output, :tactic_script, :time_taken
+  attr_accessible :prover, :prover_output, :tactic_script, :time_taken, :number
 
   validates :theorem, presence: true
 
   after_save :update_theorem_status
+
+  scope :latest, order('number DESC')
+
+  def to_param
+    self.number
+  end
 
   def used_sentences
     @used_sentences ||= used_axioms + used_theorems
