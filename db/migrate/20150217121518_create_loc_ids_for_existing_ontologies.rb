ddbacb678590ff696466c4447bf49184c3b188ba
Tim Reddehase <robustus@rightsrestricted.com>
2015-02-19 12:44:52 +0100

    fix loc/id creation migration
    Apparently updating the locid column does not work if the migration is
    executed on the same run as the creation of the locid column (that at
    least applies for my machine). So we'll use update_column.

--- db/migrate/20150217121518_create_loc_ids_for_existing_ontologies.rb
+++ db/migrate/20150217121518_create_loc_ids_for_existing_ontologies.rb
@@ -1,13 +1,13 @@
 class CreateLocIdsForExistingOntologies < ActiveRecord::Migration
   def up
-    Ontology.find_each do |ontology|
+    Ontology.all.each do |ontology|
       ontology.locid = "/#{ontology.repository.path}/"
       if ontology.parent
-        ontology.locid << "#{ontology.parent.basepath}/#{ontology.name}"
+        ontology.locid << "#{ontology.parent.basepath}//#{ontology.name}"
       else
         ontology.locid << "#{ontology.basepath}"
       end
-      ontology.save!
+      ontology.update_column(:locid, ontology.locid)
     end
   end
 
