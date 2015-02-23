10df2c61cd5ff56311a848f153133b5430d733f4
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-02-23 12:02:49 +0100

    Drop column only if it exists.

--- db/migrate/20140915123504_move_to_has_and_belongs_to_many_for_ontologies_tasks_end.rb
+++ db/migrate/20140915123504_move_to_has_and_belongs_to_many_for_ontologies_tasks_end.rb
@@ -1,7 +1,7 @@
 class MoveToHasAndBelongsToManyForOntologiesTasksEnd < ActiveRecord::Migration
   def up
-    remove_column(:ontologies, :task_id)
-    remove_column(:tasks, :ontology_id)
+    execute('ALTER TABLE "ontologies" DROP COLUMN IF EXISTS "task_id";')
+    execute('ALTER TABLE "tasks" DROP COLUMN IF EXISTS "ontology_id";')
   end
 
   def down
