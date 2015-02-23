b7e41aca5060c6ae8622fd488b95849c7bb8940a
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-02-23 11:07:15 +0100

    Remove nonexistent method calls from data migration.

--- db/data/20140821151926_fix_ontology_version_data.rb
+++ db/data/20140821151926_fix_ontology_version_data.rb
@@ -8,8 +8,6 @@ class FixOntologyVersionData < ActiveRecord::Migration
             where(number: version_count - child_version.number + 1).first
           child_version.commit_oid = parent_version.try(:commit_oid)
           child_version.basepath = parent_version.try(:basepath)
-          child_version.pp_xml_name = parent_version.try(:pp_xml_name)
-          child_version.xml_name = parent_version.try(:xml_name)
           child_version.file_extension = parent_version.try(:file_extension)
           child_version.parent = parent_version
           child_version.state = 'done'
@@ -29,8 +27,6 @@ class FixOntologyVersionData < ActiveRecord::Migration
             where(number: version_count - child_version.number + 1).first
           child_version.commit_oid = nil
           child_version.basepath = nil
-          child_version.pp_xml_name = nil
-          child_version.xml_name = nil
           child_version.file_extension = nil
           child_version.parent = nil
           child_version.state = 'pending'
