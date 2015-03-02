d20fde5f84d6003cafb67ec6db830977092143b5
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-02 19:55:50 +0100

    Obey style, load same config chain as in Rails.

--- lib/environment_light.rb
+++ lib/environment_light.rb
@@ -3,7 +3,7 @@ require 'pathname'
 # Set up Rails configuration
 Rails = Struct.new(:env,:root,:logger).new
 Rails.env  = ENV['RAILS_ENV'] || 'production'
-Rails.root = Pathname.new(File.dirname(__FILE__) << "/..")
+Rails.root = Pathname.new(File.dirname(__FILE__) << '/..')
 
 # Load Bundler
 ENV['BUNDLE_GEMFILE'] = Rails.root.join('Gemfile').to_s
@@ -11,5 +11,13 @@ require 'rubygems'
 require 'bundler/setup'
 
 # Load application settings
-require File.expand_path("../../config/initializers/rails_config", __FILE__)
-Settings = RailsConfig.load_files ["settings.yml","settings.local.yml", "settings/#{Rails.env}.yml"].map{|f| "#{Rails.root}/config/#{f}" }
+require Rails.root.join('config/initializers/rails_config.rb')
+Settings = RailsConfig.load_files([
+    'settings.yml',
+    "settings/#{Rails.env}.yml",
+    "environments/#{Rails.env}.yml",
+    'settings.local.yml',
+    "settings.#{Rails.env}.local.yml",
+    "environments/#{Rails.env}.local.yml",
+    'hets.yml'
+  ].map { |f| Rails.root.join('config', f) })
