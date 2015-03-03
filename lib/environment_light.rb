6bb8b5130588e23c2e7f5dba47d3a69b93e24fca
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-03 09:58:46 +0100

    Read from rails_config which files to load.

--- lib/environment_light.rb
+++ lib/environment_light.rb
@@ -12,12 +12,7 @@ require 'bundler/setup'
 
 # Load application settings
 require Rails.root.join('config/initializers/rails_config.rb')
-Settings = RailsConfig.load_files([
-    'settings.yml',
-    "settings/#{Rails.env}.yml",
-    "environments/#{Rails.env}.yml",
-    'settings.local.yml',
-    "settings.#{Rails.env}.local.yml",
-    "environments/#{Rails.env}.local.yml",
-    'hets.yml'
-  ].map { |f| Rails.root.join('config', f) })
+# only load basic files, NOT the auxiliaries like hets.yml
+settings_files = RailsConfig.setting_files(Rails.root.join('config'), Rails.env)
+abs_settings_files = settings_files.map { |f| Rails.root.join('config', f) }
+Settings = RailsConfig.load_files(abs_settings_files)
