c438a78969cee42951bb42899855cdb1a965dd55
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-04 20:25:08 +0100

    setup activemodel serializers w/ custom baseclass.

---
+++ app/serializers/application_serializer.rb
@@ -0,0 +1,12 @@
+class ApplicationSerializer < ActiveModel::Serializer
+  def urls
+    Rails.application.routes.url_helpers
+  end
+
+  def qualified_locid_for(resource, *commands, **query_components)
+    iri = "#{Settings.hostname}#{resource.locid}"
+    iri << "///#{commands.join('///')}" if commands.any?
+    iri << "?#{query_components.to_query}" if query_components.any?
+    iri
+  end
+end
