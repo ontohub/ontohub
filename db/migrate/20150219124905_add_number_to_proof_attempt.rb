13fda56313f6a99d734895ff44740c3ea7ebd359
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-05 00:01:39 +0100

    Define default proof status number.

--- db/migrate/20150219124905_add_number_to_proof_attempt.rb
+++ db/migrate/20150219124905_add_number_to_proof_attempt.rb
@@ -1,5 +1,5 @@
 class AddNumberToProofAttempt < ActiveRecord::Migration
   def change
-    add_column :proof_attempts, :number, :integer, null: false
+    add_column :proof_attempts, :number, :integer, null: false, default: 0
   end
 end
