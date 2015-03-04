b32e0f96fd7683cdc4c685ecc60f52c8234aca7b
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-05 00:17:18 +0100

    Fix hets options - broken by merge.

--- lib/hets.rb
+++ lib/hets.rb
@@ -150,10 +150,10 @@ we expected it to be matchable by this regular expression:
     parse_caller.call(qualified_loc_id_for(resource), with_mode: mode)
   end
 
-  def self.prove_via_api(resource, url_catalog = [])
+  def self.prove_via_api(resource, hets_options = nil)
     options = {}
     options[:node] = resource.name if resource.in_distributed?
-    prove_caller = Hets::ProveCaller.new(HetsInstance.choose!, url_catalog)
+    prove_caller = Hets::ProveCaller.new(HetsInstance.choose!, hets_options)
     prove_caller.call(resource.versioned_iri, options)
   end
 
