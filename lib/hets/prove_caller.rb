1118e43ba564bb499452bfc498deba6edc380cd3
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-04 07:47:47 +0100

    Add hets error handling to ProveCaller.

--- lib/hets/prove_caller.rb
+++ lib/hets/prove_caller.rb
@@ -11,6 +11,8 @@ module Hets
       arguments = [escaped_iri, *COMMAND_LIST]
       api_uri = build_api_uri(CMD, arguments, build_query_string)
       perform(api_uri, PROVE_OPTIONS.merge(options), METHOD)
+    rescue UnfollowableResponseError => error
+      handle_possible_hets_error(error)
     end
   end
 end
