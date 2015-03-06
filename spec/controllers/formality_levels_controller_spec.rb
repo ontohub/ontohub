f2a9e5e1fe169258657e3d2da870f4cfc38efc50
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-06 14:21:34 +0100

    add json validation spec to formality level.

--- spec/controllers/formality_levels_controller_spec.rb
+++ spec/controllers/formality_levels_controller_spec.rb
@@ -14,8 +14,34 @@ describe FormalityLevelsController do
     end
 
     context "show" do
-      before { get :show, id: formality_level.id }
-      it { should respond_with :success }
+      context 'requesting standard representation' do
+        before { get :show, id: formality_level.to_param }
+
+        it { should respond_with :success }
+        it { should render_template :show }
+      end
+
+      context 'requesting json representation', api_specification: true do
+        let(:formality_level_schema) { schema_for('formality_level') }
+
+        before do
+          get :show,
+            id: formality_level.to_param,
+            format: :json
+        end
+
+        it { should respond_with :success }
+
+        it 'respond with json content type' do
+          expect(response.content_type.to_s).to eq('application/json')
+        end
+
+        it 'should return a representation that validates against the schema' do
+          VCR.use_cassette 'api/json-schemata/formality_level' do
+            expect(response.body).to match_json_schema(formality_level_schema)
+          end
+        end
+      end
     end
 
     context "new" do
