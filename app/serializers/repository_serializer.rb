509083a007c5c690da87b9240db8c23d24616c64
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-06 10:19:25 +0100

    add a repository serializer.

---
+++ app/serializers/repository_serializer.rb
@@ -0,0 +1,32 @@
+class RepositorySerializer < ApplicationSerializer
+  class Reference < ApplicationSerializer
+    attributes :iri
+    attributes :name
+
+    def iri
+      urls.repository_url(object, host: Settings.hostname)
+    end
+  end
+
+  attributes :iri, :remote_origin, :source
+  attributes :name, :path, :description
+
+  def iri
+    Reference.new(object).iri
+  end
+
+  def remote_origin
+    object.mirror? || object.fork?
+  end
+
+  def source
+    if remote_origin
+      {
+        type: object.remote_type,
+        source_type: object.source_type,
+        state: object.state,
+        address: object.source_address
+      }
+    end
+  end
+end
