290970b6edfd669bc5dfe7870c351a77a731aebb
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-06 12:35:47 +0100

    add json validation spec to repository.

--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -45,10 +45,34 @@ describe RepositoriesController do
     end
 
     context 'on GET to show' do
-      before { get :show, id: repository.path }
+      context 'requesting standard representation' do
+        before { get :show, id: repository.path }
 
-      it { should respond_with :success }
-      it { should render_template :show }
+        it { should respond_with :success }
+        it { should render_template :show }
+      end
+
+      context 'requesting json representation', api_specification: true do
+        let(:repository_schema) { schema_for('repository') }
+
+        before do
+          get :show,
+            id: repository.to_param,
+            format: :json
+        end
+
+        it { should respond_with :success }
+
+        it 'respond with json content type' do
+          expect(response.content_type.to_s).to eq('application/json')
+        end
+
+        it 'should return a representation that validates against the schema' do
+          VCR.use_cassette 'api/json-schemata/repository' do
+            expect(response.body).to match_json_schema(repository_schema)
+          end
+        end
+      end
     end
   end
 
