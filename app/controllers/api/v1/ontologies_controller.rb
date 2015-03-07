93266970477c9fdff01b03720e0929e514d6f489
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-07 18:11:36 +0100

    move ontology api handling to api namespace.

--- app/controllers/api/v1/ontologies_controller.rb
+++ app/controllers/api/v1/ontologies_controller.rb
@@ -1,4 +1,25 @@
 class Api::V1::OntologiesController < Api::V1::Base
   inherit_resources
   actions :show
+
+  respond_to :text, only: %i(show)
+
+  def show
+    super do |format|
+      format.json { render json: resource, serializer: OntologySerializer }
+      format.text { send_download }
+    end
+  end
+
+  private
+  def send_download
+    asset = version || resource
+    render text: asset.file_in_repository.content,
+           content_type: Mime::Type.lookup('application/force-download')
+  end
+
+  def version
+    finder = OntologyVersionFinder.new(params[:reference], resource)
+    @version ||= finder.find if params[:reference]
+  end
 end
--- spec/controllers/ontologies_controller_spec.rb
+++ spec/controllers/ontologies_controller_spec.rb
@@ -90,30 +90,6 @@ describe OntologiesController do
     context 'on GET to show' do
       before { OntologyMember::Symbol.delete_all }
 
-      context 'with format json', api_specification: true do
-        let(:ontology_schema) { schema_for('ontology') }
-
-        before do
-          get :show,
-            repository_id: repository.to_param,
-            format:        :json,
-            id:            ontology.to_param
-        end
-
-        it { should respond_with :success }
-
-        it 'respond with json content type' do
-          expect(response.content_type.to_s).to eq('application/json')
-        end
-
-        it 'should return a representation that validates against the schema' do
-          VCR.use_cassette 'api/json-schemata/ontology' do
-            expect(response.body).to match_json_schema(ontology_schema)
-          end
-        end
-
-      end
-
       context 'without symbols' do
         before do
           get :show,
