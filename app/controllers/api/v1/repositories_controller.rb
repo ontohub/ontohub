242a1f2f61fb1204c7e9443b53c1c881c1715c4b
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-07 21:25:12 +0100

    move repository api handling to api namespace.

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::RepositoriesController < Api::V1::Base
   inherit_resources
+  defaults finder: :find_by_path!
+
   actions :show
 end
--- spec/controllers/repositories_controller_spec.rb
+++ spec/controllers/repositories_controller_spec.rb
@@ -52,27 +52,6 @@ describe RepositoriesController do
         it { should render_template :show }
       end
 
-      context 'requesting json representation', api_specification: true do
-        let(:repository_schema) { schema_for('repository') }
-
-        before do
-          get :show,
-            id: repository.to_param,
-            format: :json
-        end
-
-        it { should respond_with :success }
-
-        it 'respond with json content type' do
-          expect(response.content_type.to_s).to eq('application/json')
-        end
-
-        it 'should return a representation that validates against the schema' do
-          VCR.use_cassette 'api/json-schemata/repository' do
-            expect(response.body).to match_json_schema(repository_schema)
-          end
-        end
-      end
     end
   end
 
