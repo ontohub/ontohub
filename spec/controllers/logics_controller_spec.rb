d8a16b0cab9d6dca3f40f2f2f098c8a1c03d3f7b
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-08 09:54:16 +0100

    move logic api handling to api namespace.

--- spec/controllers/logics_controller_spec.rb
+++ spec/controllers/logics_controller_spec.rb
@@ -25,28 +25,6 @@ describe LogicsController do
         it { should_not set_the_flash }
       end
     end
-
-    context 'requesting json representation', api_specification: true do
-      let(:logic_schema) { schema_for('logic') }
-
-      before do
-        get :show,
-          id: logic.to_param,
-          format: :json
-      end
-
-      it { should respond_with :success }
-
-      it 'respond with json content type' do
-        expect(response.content_type.to_s).to eq('application/json')
-      end
-
-      it 'should return a representation that validates against the schema' do
-        VCR.use_cassette 'api/json-schemata/logic' do
-          expect(response.body).to match_json_schema(logic_schema)
-        end
-      end
-    end
   end
 
   context 'in GET to index' do
@@ -55,17 +33,5 @@ describe LogicsController do
     it { should respond_with :success }
     it { should render_template :index }
     it { should_not set_the_flash }
-  end
-
-  context 'when requesting xml' do
-
-    before do
-      @request.env['HTTP_ACCEPT'] = 'text/xml'
-      get :show, id: logic.slug
-    end
-
-    it 'should respond as a application/rdf+xml' do
-      expect(response.content_type).to eq('application/rdf+xml')
-    end
   end
 end
