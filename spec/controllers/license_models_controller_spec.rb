09788f1ef2039fe06ad3a6534d254cdda13e6bcd
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-08 10:00:07 +0100

    move license model api handling to api namespace.

--- spec/controllers/license_models_controller_spec.rb
+++ spec/controllers/license_models_controller_spec.rb
@@ -20,28 +20,6 @@ describe LicenseModelsController do
         it { should respond_with :success }
         it { should render_template :show }
       end
-
-      context 'requesting json representation', api_specification: true do
-        let(:license_model_schema) { schema_for('license_model') }
-
-        before do
-          get :show,
-            id: license_model.to_param,
-            format: :json
-        end
-
-        it { should respond_with :success }
-
-        it 'respond with json content type' do
-          expect(response.content_type.to_s).to eq('application/json')
-        end
-
-        it 'should return a representation that validates against the schema' do
-          VCR.use_cassette 'api/json-schemata/license_model' do
-            expect(response.body).to match_json_schema(license_model_schema)
-          end
-        end
-      end
     end
 
     context "new" do
