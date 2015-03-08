2c00294d0cba0ae6202af276bf19adcfc8ae31da
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-08 13:43:13 +0100

    add api index-action for mapping,sentence,symbol.

--- app/controllers/api/v1/sentences_controller.rb
+++ app/controllers/api/v1/sentences_controller.rb
@@ -1,4 +1,15 @@
 class Api::V1::SentencesController < Api::V1::Base
   inherit_resources
-  actions :show
+  belongs_to :ontology
+
+  actions :index, :show
+
+  def index
+    super do |format|
+      format.json do
+        render json: collection,
+          each_serializer: SentenceSerializer::Reference
+      end
+    end
+  end
 end
--- app/controllers/api/v1/symbols_controller.rb
+++ app/controllers/api/v1/symbols_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::SymbolsController < Api::V1::Base
   inherit_resources
   defaults resource_class: OntologyMember::Symbol
+  belongs_to :ontology
 
-  actions :show
+  actions :index, :show
+
+  def index
+    super do |format|
+      format.json do
+        render json: collection,
+          each_serializer: OntologyMember::SymbolSerializer::Reference
+      end
+    end
+  end
 end
