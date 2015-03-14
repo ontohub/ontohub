0fa5567bba77a383ee437c8675085e9bf75d161e
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-14 13:31:57 +0100

    Add CollectiveProofAttempt spec.

--- spec/factories/prover_factory.rb
+++ spec/factories/prover_factory.rb
@@ -1,5 +1,15 @@
 FactoryGirl.define do
+  sequence :prover_name do |n|
+    "prover-#{n}"
+  end
+
   factory :prover do
     name { 'SPASS' }
+
+    trait :with_sequenced_name do
+      after(:build) do |prover|
+        prover.name = generate :prover_name
+      end
+    end
   end
 end
