997de9f21fe58b036307d860e9be6e49377da65e
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-16 10:37:31 +0100

    define Rails constant after loading settings
    When using environment-light define the Rails constant after loading the
    Settings in order to circumvent the problem that RailsConfig jumps into
    Rails-Mode if the constant is defined.

--- lib/environment_light.rb
+++ lib/environment_light.rb
@@ -1,18 +1,21 @@
 require 'pathname'
 
 # Set up Rails configuration
-Rails = Struct.new(:env,:root,:logger).new
-Rails.env  = ENV['RAILS_ENV'] || 'production'
-Rails.root = Pathname.new(File.dirname(__FILE__) << '/..')
+rails_env = ENV['RAILS_ENV'] || 'production'
+rails_root = Pathname.new(File.dirname(__FILE__) << "/..")
 
 # Load Bundler
-ENV['BUNDLE_GEMFILE'] = Rails.root.join('Gemfile').to_s
+ENV['BUNDLE_GEMFILE'] = rails_root.join('Gemfile').to_s
 require 'rubygems'
 require 'bundler/setup'
 
 # Load application settings
-require Rails.root.join('config/initializers/rails_config.rb')
+require rails_root.join('config/initializers/rails_config.rb')
 # only load basic files, NOT the auxiliaries like hets.yml
-settings_files = RailsConfig.setting_files(Rails.root.join('config'), Rails.env)
-abs_settings_files = settings_files.map { |f| Rails.root.join('config', f) }
+settings_files = RailsConfig.setting_files(rails_root.join('config'), rails_env)
+abs_settings_files = settings_files.map { |f| rails_root.join('config', f) }
 Settings = RailsConfig.load_files(abs_settings_files)
+
+Rails = Struct.new(:env, :root, :logger).new
+Rails.env  = rails_env
+Rails.root = rails_root
