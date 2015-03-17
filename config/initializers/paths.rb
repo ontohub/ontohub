9e0a56b393dcbed891a972d2b757d7d0de64373f
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-17 16:24:08 +0100

    Remove unneeded git-user/group/home configuration
    The task, the user and group configuration had done, is now part of
    cp_keys. We don't need that configuration anymore.
    Also, git_home is now set explicitly in the settings.

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -19,17 +19,6 @@ module PathsInitializer
 
       config.commits_path =
         cleanup_release(Rails.root.join(Settings.paths.commits))
-
-      settings = Settings.git
-      if settings && settings.user
-        config.git_user  = settings.user
-        config.git_group = settings.group
-        config.git_home  = File.expand_path("~#{config.git_user}")
-      else
-        config.git_user  = nil
-        config.git_group = nil
-        config.git_home  = Rails.root.join('tmp','git')
-      end
     end
   end
 end
