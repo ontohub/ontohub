4133c867b8e39aa942d8823aa5235f6d59304a65
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-21 15:05:09 +0100

    Handle states of ProofAttempt, Theorem, OntologyVersion.

--- lib/collective_proof_attempt.rb
+++ lib/collective_proof_attempt.rb
@@ -8,18 +8,32 @@ class CollectiveProofAttempt
   end
 
   def run
-    ontology_version.update_state! :processing
-    options_to_attempts_hash.each do |prove_options, proof_attempts|
-      prove_options.merge!(resource.prove_options)
-      prove(prove_options, proof_attempts)
+    ontology_version.update_state!(:processing)
+    ontology_version.do_or_set_failed do
+      if theorem?
+        resource.update_state!(:processing)
+        resource.do_or_set_failed do
+          run_body
+          resource.update_state!(:done)
+        end
+      else
+        resource.theorems.each { |theorem| theorem.update_state!(:processing) }
+        run_body
+        resource.theorems.each { |theorem| theorem.update_state!(:done) }
+      end
+      ontology_version.update_state!(:done)
     end
   end
 
   protected
 
+  def theorem?
+    resource.is_a?(Theorem)
+  end
+
   def ontology_version
     @ontology_version ||=
-      if resource.is_a?(Theorem)
+      if theorem?
         resource.ontology.current_version
       else
         resource
@@ -30,18 +44,22 @@ class CollectiveProofAttempt
     @ontology ||= ontology_version.ontology
   end
 
-  def prove(prove_options, proof_attempts)
-    ontology_version.update_state! :processing
-    ontology_version.do_or_set_failed do
-      cmd, input_io = execute_proof(prove_options)
-      return if cmd == :abort
-
-      ontology.import_proof(ontology_version,
-                            ontology_version.user,
-                            proof_attempts,
-                            input_io)
-      ontology_version.update_state! :done
+  def run_body
+    options_to_attempts_hash.each do |prove_options, proof_attempts|
+      prove_options.merge!(resource.prove_options)
+      prove(prove_options, proof_attempts)
     end
+  end
+
+  def prove(prove_options, proof_attempts)
+    proof_attempts.each { |pa| pa.update_state!(:processing) }
+    cmd, input_io = execute_proof(prove_options)
+    return if cmd == :abort
+
+    ontology.import_proof(ontology_version,
+                          ontology_version.user,
+                          proof_attempts,
+                          input_io)
   end
 
   def execute_proof(prove_options)
