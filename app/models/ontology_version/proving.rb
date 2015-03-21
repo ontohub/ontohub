8b67131fb21c0bcbedfe6fecda6a642a2e7d8bfa
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-21 13:55:18 +0100

    Minimize prove_options method
    Since the specific prove-options are built in the class Proof, we only
    need to supply the proving mechanism with the basic prove-options,
    which set the resource specific values.

--- app/models/ontology_version/proving.rb
+++ app/models/ontology_version/proving.rb
@@ -1,15 +1,8 @@
 module OntologyVersion::Proving
   extend ActiveSupport::Concern
 
-  def prove_options(prove_options = nil)
-    prove_options ||= Hets::ProveOptions.new
-    # If the prove_options have gone through the async_prove call, they are now
-    # a Hash and need to be restored as a ProveOptions object.
-    if prove_options.is_a?(Hash)
-      prove_options = Hets::ProveOptions.from_hash(prove_options)
-    end
-    prove_options.add(:'url-catalog' => ontology.repository.url_maps,
-                      ontology: ontology)
-    prove_options
+  def prove_options
+    Hets::ProveOptions.new(:'url-catalog' => ontology.repository.url_maps,
+                           ontology: ontology)
   end
 end
