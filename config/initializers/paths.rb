2f0eba71c311b096362f3603d17addad30213c4d
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-24 08:52:54 +0100

    Allow to use ~git as git_home
    Expand paths. This allows to use special paths like ~username.

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -1,24 +1,20 @@
 
 module PathsInitializer
   class << self
+    def expand(path)
+      Dir.chdir(Rails.root) { Pathname.new(path).expand_path }
+    end
+
     def cleanup_release(path)
       path.sub(%r(/releases/\d+/), "/current/")
     end
 
     def perform_initialization(config)
-      config.data_root = cleanup_release(Rails.root.join(Settings.paths.data))
-
-      config.git_root =
-        cleanup_release(Rails.root.join(Settings.paths.git_repositories))
-
-      config.git_home =
-        cleanup_release(Rails.root.join(Settings.paths.git_home))
-
-      config.symlink_path =
-        cleanup_release(Rails.root.join(Settings.paths.symlinks))
-
-      config.commits_path =
-        cleanup_release(Rails.root.join(Settings.paths.commits))
+      config.data_root = cleanup_release(expand(Settings.paths.data))
+      config.git_root = cleanup_release(expand(Settings.paths.git_repositories))
+      config.git_home = cleanup_release(expand(Settings.paths.git_home))
+      config.symlink_path = cleanup_release(expand(Settings.paths.symlinks))
+      config.commits_path = cleanup_release(expand(Settings.paths.commits))
     end
   end
 end
