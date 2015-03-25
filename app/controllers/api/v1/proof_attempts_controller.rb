e02bcda5c356adb19107c10bd2448a2d43250aea
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-25 15:08:31 +0100

    Add actions for proof_attempt subsites.

--- app/controllers/api/v1/proof_attempts_controller.rb
+++ app/controllers/api/v1/proof_attempts_controller.rb
@@ -13,6 +13,41 @@ class Api::V1::ProofAttemptsController < Api::V1::Base
     end
   end
 
+  def used_axioms
+    respond_to do |format|
+      format.json do
+        render json: resource.used_axioms,
+               each_serializer: AxiomSerializer::Reference
+      end
+    end
+  end
+
+  def generated_axioms
+    respond_to do |format|
+      format.json do
+        render json: resource.generated_axioms,
+               each_serializer: GeneratedAxiomSerializer
+      end
+    end
+  end
+
+  def used_theorems
+    respond_to do |format|
+      format.json do
+        render json: resource.used_theorems,
+               each_serializer: TheoremSerializer::Reference
+      end
+    end
+  end
+
+  def prover_output
+    respond_to do |format|
+      format.json do
+        render json: {prover_output: resource.prover_output}
+      end
+    end
+  end
+
   protected
 
   def collection
