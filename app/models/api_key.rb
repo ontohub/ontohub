248ce7a27b1f8b0f557b2d7f05967084753c3bbb
Tim Reddehase <robustus@rightsrestricted.com>
2015-03-29 16:07:29 +0200

    add a convenient api-key creation method.

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -4,12 +4,26 @@ class ApiKey < ActiveRecord::Base
 
   belongs_to :user
   attr_accessible :key, :status
+  attr_accessible :user
 
   before_validation :initialize_key
 
   validates :user, presence: true
   validates :key, presence: true, uniqueness: true
   validates :status, inclusion: {in: STATES}
+
+  def self.create_new_key!(user)
+    transaction do
+      where(user_id: user, status: 'valid').
+        find_each { |key| key.invalidate! }
+      create!(user: user)
+    end
+  end
+
+  def invalidate!
+    self.status = 'invalid'
+    save!
+  end
 
   private
   def initialize_key
--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -7,6 +7,21 @@ describe ApiKey do
     expect(api_key).to be_valid
   end
 
+  context '.create_new_key!' do
+    let(:old_key) { create :api_key }
+    let!(:key) { described_class.create_new_key!(old_key.user) }
+
+    it 'should only have the new key as valid' do
+      expect(described_class.where(status: 'valid').to_a).
+        to eq([key])
+    end
+
+    it 'should have invalidated the old key' do
+      old_key.reload
+      expect(old_key.status).to eq('invalid')
+    end
+  end
+
   context 'invalid on' do
     it 'non-unique keys' do
       invalid_key = build(:api_key, key: api_key.key)
