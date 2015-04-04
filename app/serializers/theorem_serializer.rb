05d3ce5925b0cbd1539dee654a6612023a62cc92
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-04-04 20:59:08 +0200

    Finalize TheoremSerializer.

--- app/serializers/theorem_serializer.rb
+++ app/serializers/theorem_serializer.rb
@@ -8,16 +8,33 @@ class TheoremSerializer < ApplicationSerializer
     end
   end
 
-  attributes :iri, :symbols
-  attributes :name, :text, :imported
-
+  attributes :iri,
+             :name,
+             :definition,
+             :evaluation_state
+  has_one :proof_status, serializer: ProofStatusSerializer::Reference
   has_one :ontology, serializer: OntologySerializer::Reference
+
+  attributes :symbols,
+             :proof_attempts
 
   def iri
     Reference.new(object).iri
   end
 
+  def definition
+    object.text
+  end
+
+  def evaluation_state
+    object.state
+  end
+
   def symbols
     qualified_locid_for(object, :symbols)
+  end
+
+  def proof_attempts
+    qualified_locid_for(object, :proof_attempts)
   end
 end
