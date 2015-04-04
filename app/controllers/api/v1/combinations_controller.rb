3dbfb1a685c0e012c68bbd8b86f81ea67938a4fa
Tim Reddehase <robustus@rightsrestricted.com>
2015-04-04 18:24:08 +0200

    use actions for combinations in the controller.

--- app/controllers/api/v1/combinations_controller.rb
+++ app/controllers/api/v1/combinations_controller.rb
@@ -8,21 +8,32 @@ class Api::V1::CombinationsController < Api::V1::Base
   before_filter :check_write_permission
 
   def create
-    if combination.save
-      response.status = 201
-      response.location = combination.ontology.locid
+    if combination.valid?
+      response.status = 202
+      response.location = action_iri_path(action)
       render json: {status: response.message, location: response.location}
     else
       response.status = 400
       render json: {status: response.message,
-                    error: combination.error.message}
+                    error: errors_str}
     end
   end
 
   private
   def combination
     @combination ||= Combination.
-      build(current_user, repository, params['combination'])
+      build(current_user, repository, params[:combination])
+  end
+
+  def action
+    args = [current_user.id, repository.id, params[:combination]]
+    @action ||= Action.
+      enclose!(3.minutes, Combination, :combined_ontology!, *args)
+  end
+
+  def errors_str
+    combination.errors.messages.to_a.
+      map { |(f, m)| "#{f}: #{m.join(', ')}" }.join(';')
   end
 
   def repository
