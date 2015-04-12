f92500529badf9479154d1aef0ebabca4f9bbc00
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-04-12 14:23:57 +0200

    Revert "Add locid to LogicMapping."
    This reverts commit 38629469c3de07bfc6fc8dc480765f87c56ac488.

--- app/models/logic_mapping.rb
+++ app/models/logic_mapping.rb
@@ -12,28 +12,9 @@ class LogicMapping < ActiveRecord::Base
   belongs_to :target, class_name: 'Logic'
   belongs_to :user
   has_many :logic_adjoints, :foreign_key => :translation_id
-  attr_accessible :source_id,
-                  :target_id,
-                  :source,
-                  :target,
-                  :iri,
-                  :standardization_status,
-                  :defined_by,
-                  :default,
-                  :projection,
-                  :faithfulness,
-                  :theoroidalness,
-                  :exactness,
-                  :user,
-                  :locid
+  attr_accessible :source_id, :target_id, :source, :target, :iri, :standardization_status, :defined_by, :default, :projection, :faithfulness, :theoroidalness, :exactness, :user
 
   validates_presence_of :target, :source, :iri
-
-  before_create :generate_locid
-
-  def self.find_with_locid(locid, _iri = nil)
-    where(locid: locid).first
-  end
 
   def to_s
     "#{iri}: #{source} => #{target}"
@@ -43,10 +24,4 @@ class LogicMapping < ActiveRecord::Base
     LogicAdjoint.where("projection_id = ? OR translation_id = ?", self.id, self.id)
   end
 
-  protected
-
-  def generate_locid
-    name = iri.split('/').last
-    self.locid = "/logic_mappings/#{name}"
-  end
 end
