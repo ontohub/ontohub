1787db8f3dfd503a72b55d41b2021a140cc39f26
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-04-15 07:00:50 +0200

    Use generated prover output fixtures.

--- spec/lib/hets/szs_parser_spec.rb
+++ spec/lib/hets/szs_parser_spec.rb
@@ -1,16 +1,12 @@
 require 'spec_helper'
 
 describe Hets::Prove::SZSParser do
-  let(:fixtures_dir) do
-    Rails.root.join('spec', 'fixtures', 'prover_outputs')
-  end
-
   %w(Theorem CounterSatisfiable).each do |szs_status|
     %w(MathServeBroker
        SPASS
        Vampire).each do |prover|
       context "#{prover} on #{szs_status}" do
-        let(:output) { File.read(fixtures_dir.join(szs_status, prover)) }
+        let(:output) { File.read(prover_output_fixture(szs_status, prover)) }
 
         it "returns nil" do
           expect(Hets::Prove::SZSParser.new(prover, output).call).to be(nil)
@@ -22,7 +18,7 @@ describe Hets::Prove::SZSParser do
        darwin-non-fd
        eprover).each do |prover|
       context "#{prover} on #{szs_status}" do
-        let(:output) { File.read(fixtures_dir.join(szs_status, prover)) }
+        let(:output) { File.read(prover_output_fixture(szs_status, prover)) }
 
         it "returns status '#{szs_status}'" do
           expect(Hets::Prove::SZSParser.new(prover, output).call).
