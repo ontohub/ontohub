758dae66ef232b40ad7d23843738c3a03d50327f
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-04-27 09:37:06 +0200

    Add comment on the class MigrationWithData.

--- lib/migration_with_data.rb
+++ lib/migration_with_data.rb
@@ -1,3 +1,7 @@
+# This provides convenience-methods to directly operate on the database, without
+# relying on the existence of model methods (except for `id`).
+# It allows to perform data migrations which won't fail because of renamed
+# or removed model-methods.
 class MigrationWithData < ActiveRecord::Migration
   def select_attributes(record, *keys)
     attributes = keys.map do |key|
