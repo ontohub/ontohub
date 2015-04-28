f16c3d6354e60586f5b599b4df8cde590f696034
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-04-28 10:41:16 +0200

    Move to migrations: UseProverReferenceInProofAttempts
    Putting the data migration together with the schema migration, it could even be
    improved. Now it creates/uses the correct prover.

--- db/data/20150313065037_use_prover_reference_in_proof_attempts.rb
+++
@@ -1,9 +0,0 @@
-class UseProverReferenceInProofAttempts < ActiveRecord::Migration
-  def self.up
-    prover = Prover.where(name: 'SPASS').first_or_create!
-    ProofAttempt.find_each do |proof_attempt|
-      proof_attempt.prover_id = prover.id
-      proof_attempt.save!
-    end
-  end
-end
--- db/migrate/20150313065037_use_prover_reference_in_proof_attempts.rb
+++ db/migrate/20150313065037_use_prover_reference_in_proof_attempts.rb
@@ -1,11 +1,27 @@
-class UseProverReferenceInProofAttempts < ActiveRecord::Migration
+class UseProverReferenceInProofAttempts < MigrationWithData
   def up
-    remove_columns :proof_attempts, :prover
     add_column :proof_attempts, :prover_id, :integer
+
+    ProofAttempt.find_each do |proof_attempt|
+      attrs = select_attributes(proof_attempt, :prover)
+      prover = Prover.where(name: attrs[:prover]).first_or_create!
+      update_attributes!(proof_attempt, prover_id: prover.id)
+    end
+
+    remove_columns :proof_attempts, :prover
   end
 
   def down
     add_column :proof_attempts, :prover, :string
+
+    prover = Prover.where(name: 'SPASS').first_or_create!
+    ProofAttempt.find_each do |proof_attempt|
+      pa_attrs = select_attributes(proof_attempt, :prover_id)
+      prover = Prover.find(pa_attrs[:prover_id])
+      prover_attrs = select_attributes(prover, :name)
+      update_attributes!(proof_attempt, prover: prover_attrs[:name])
+    end
+
     remove_column :proof_attempts, :prover_id
   end
 end
