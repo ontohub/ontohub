2bfaf5a787e9cc12bf433f8c98acc5dbd715530a
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-04-28 13:32:33 +0200

    Move to migrations: AddStateToProofAttemptsAndTheorems.

--- db/data/20150320113702_add_state_to_proof_attempts_and_theorems.rb
+++
@@ -1,8 +0,0 @@
-class AddStateToProofAttemptsAndTheorems < ActiveRecord::Migration
-  def up
-    Theorem.find_each do |theorem|
-      theorem.state = 'pending'
-      theorem.save!
-    end
-  end
-end
