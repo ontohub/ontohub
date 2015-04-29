8704b432e1376ef029410baab26d3a194afd24a7
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-04-29 08:04:19 +0200

    Don't allow theoroidalness to be null, set default.

--- db/migrate/20150404184231_set_theoroidalness_to_unknown_where_nil.rb
+++ db/migrate/20150404184231_set_theoroidalness_to_unknown_where_nil.rb
@@ -3,11 +3,14 @@ class SetTheoroidalnessToUnknownWhereNil < MigrationWithData
     LogicMapping.where(theoroidalness: nil).find_each do |logic_mapping|
       update_attributes!(logic_mapping, theoroidalness: 'unknkown')
     end
+    change_column :logic_mappings, :theoroidalness, :string,
+                  null: false, default: 'unknown'
   end
 
   def down
     LogicMapping.where(theoroidalness: 'unknown').find_each do |logic_mapping|
       update_attributes!(logic_mapping, theoroidalness: nil)
     end
+    change_column :logic_mappings, :theoroidalness, :string, null: true
   end
 end
