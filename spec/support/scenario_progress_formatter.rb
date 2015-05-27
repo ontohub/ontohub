a44c794981080bb4ae655ed23bbe3dd1c930ab7b
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-05-27 13:44:30 +0200

    Print failure details after the features.

--- spec/support/scenario_progress_formatter.rb
+++ spec/support/scenario_progress_formatter.rb
@@ -2,8 +2,27 @@ require 'cucumber/formatter/progress'
 
 class ScenarioProgressFormatter < Cucumber::Formatter::Progress
   SCENARIO_INDENT = 2
+  EXCEPTION_INDENT = SCENARIO_INDENT + 2
+
+  def initialize(runtime, path_or_io, options)
+    super(runtime, path_or_io, options)
+    @failed_scenario_steps = []
+  end
 
   def after_feature_element(feature_element)
+    super(feature_element)
+    @io.puts
+    @io.flush
+  end
+
+  def before_feature_element(feature_element)
+    super(feature_element)
+    @processed_steps = [@current_feature]
+    @failed = false
+  end
+
+  def after_feature_element(feature_element)
+    @failed_scenario_steps << @processed_steps if @failed
     super(feature_element)
     @io.puts
     @io.flush
@@ -17,25 +36,41 @@ class ScenarioProgressFormatter < Cucumber::Formatter::Progress
   def before_steps(*_args)
     super(*_args)
     @io.print(' ')
+    @io.flush
   end
 
   def tag_name(tag_name)
     tag = format_string(tag_name, :tag).indent(SCENARIO_INDENT)
+    @processed_steps << tag
     @io.puts(tag)
     @io.flush
   end
 
   def feature_name(keyword, name)
+    @current_feature = "Feature: #{name}"
     @io.puts("#{keyword}: #{name}")
     @io.flush
   end
 
   def background_name(keyword, name, file_colon_line, source_indent)
+    @processed_steps << "Background: #{name}"
     print_feature_element_name(keyword, name, file_colon_line, source_indent)
   end
 
   def scenario_name(keyword, name, file_colon_line, source_indent)
+    @processed_steps << "Scenario: #{name}"
     print_feature_element_name(keyword, name, file_colon_line, source_indent)
+  end
+
+  def step_name(keyword, step_match, status, source_indent, background, file_colon_line)
+    name_to_report = format_step(keyword, step_match, status, source_indent)
+    @processed_steps << "Step: #{name_to_report}"
+  end
+
+  def exception(exception, status)
+    return if @hide_this_step
+    @processed_steps << format_exception(exception, status, EXCEPTION_INDENT)
+    @failed = true
   end
 
   private
@@ -46,5 +81,40 @@ class ScenarioProgressFormatter < Cucumber::Formatter::Progress
     @io.print(line)
     @io.print(' [...]') if names[1..-1].present?
     @io.flush
+  end
+
+  def print_summary(features)
+    print_steps(:pending)
+    print_failure_details
+    print_stats(features, @options)
+    print_snippets(@options)
+    print_passing_wip(@options)
+  end
+
+  def print_failure_details
+    if @failed_scenario_steps.any?
+      @io.puts(format_string("(::) steps of failed scenarios (::)", :failed))
+      @io.puts
+      @io.flush
+    end
+
+    @failed_scenario_steps.each do |failed_scenario|
+      failed_scenario.each do |step|
+        @io.puts step.indent(SCENARIO_INDENT)
+      end
+      @io.puts
+    end
+    @io.puts
+    @io.flush
+  end
+
+  def format_exception(e, status, indent)
+    message = "#{e.message} (#{e.class})".force_encoding("UTF-8")
+    if ENV['CUCUMBER_TRUNCATE_OUTPUT']
+      message = linebreaks(message, ENV['CUCUMBER_TRUNCATE_OUTPUT'].to_i)
+    end
+
+    string = "#{message}\n#{e.backtrace.join("\n")}".indent(indent)
+    format_string(string, status)
   end
 end
