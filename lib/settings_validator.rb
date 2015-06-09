631ace10e7e7b889c6110d4bd38b0eb71c89ab14
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-06-09 10:42:03 +0200

    Print special messages for initializer errors.

--- lib/settings_validator.rb
+++ lib/settings_validator.rb
@@ -1,6 +1,10 @@
 class SettingsValidator
   class SettingsValidator::ValidationError < ::StandardError; end
 
+  CONFIG_YML_FILES =
+    "config/settings[.local].yml or config/settings/#{Rails.env}[.local].yml"
+  CONFIG_INITIALIZER_FILES = "config/environments/#{Rails.env}[.local].rb"
+
   def validate!
     settings_validations_wrapper = SettingsValidationWrapper.new
     unless settings_validations_wrapper.valid?
@@ -14,7 +18,8 @@ class SettingsValidator
   protected
 
   def print_errors(formatted_errors)
-    $stderr.puts 'The settings are invalid. Please check your settings*.yml'
+    $stderr.puts "The settings are invalid. Please check your #{CONFIG_YML_FILES}"
+    $stderr.puts 'The following errors were detected:'
     $stderr.puts
     formatted_errors.each { |error| $stderr.puts error }
     $stderr.puts
@@ -27,8 +32,14 @@ class SettingsValidator
 
   def format_error(key, messages)
     category, key_portions = key_info(key)
+    opening_line =
+      if category == :initializers
+        "#{format_initializer_error(key_portions)} #{format_key(key_portions)}"
+      else
+        format_key(key_portions)
+      end
     <<-MESSAGE.strip_heredoc
-      #{format_key(key_portions)}:
+      #{opening_line}:
       #{format_messages(messages)}
     MESSAGE
   end
@@ -48,5 +59,17 @@ class SettingsValidator
 
   def format_message(message)
     "  #{message}"
+  end
+
+  def format_initializer_error(key_portions)
+    key = key_portions.join('.')
+    if 'fqdn' == key
+      "The FQDN could not be determined. Please set the hostname in #{CONFIG_YML_FILES}"
+    elsif %w(data_root git_home git_root symlink_path commits_path).include?(key)
+      'Please check the paths keys in the settings -'
+    else
+      # other possible values: %w(consider_all_requests_local secret_token log_level)
+      "Please set a valid value in the #{CONFIG_INITIALIZER_FILES} for"
+    end
   end
 end
