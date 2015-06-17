9a9ae066a506730078cbe92027e0b1b1fcb2e9da
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-06-17 08:50:29 +0200

    Allow to provers data migration to fail for ontology versions
    a) Hets closes the HTTP connection after a minute, which results in
    timeouts. If a timeout occurs, we try again to get the provers list
    twice.
    
    b) Sometimes another error occurs (Hets response not usable).
    
    In both cases we print the ID of the OntologVersion to know which ones
    fail. This way, we can process them in another data migration.

--- db/data/20150311194246_associate_provers_with_ontology_versions.rb
+++ db/data/20150311194246_associate_provers_with_ontology_versions.rb
@@ -1,12 +1,31 @@
 class AssociateProversWithOntologyVersions < ActiveRecord::Migration
   def self.up
-    OntologyVersion.find_each(&:retrieve_available_provers)
+    OntologyVersion.find_each do |ontology_version|
+      retry_provers_retrieval(ontology_version, 3)
+    end
   end
 
   def self.down
     OntologyVersion.find_each do |ontology_version|
       ontology_version.provers = []
       ontology_version.save!
+    end
+  end
+
+  protected
+
+  def self.retry_provers_retrieval(ontology_version, max_attempts)
+    begin
+      ontology_version.retrieve_available_provers
+    rescue Net::ReadTimeout
+      if max_attempts > 0
+        puts "Timeout, trying again: OntologyVersion #{ontology_version.id}"
+        retry_provers_retrieval(ontology_version, max_attempts - 1)
+      else
+        puts "Timeout, limit reached: OntologyVersion #{ontology_version.id}"
+      end
+    rescue ::StandardError => e
+      puts "Errored at OntologyVersion #{ontology_version.id}"
     end
   end
 end
