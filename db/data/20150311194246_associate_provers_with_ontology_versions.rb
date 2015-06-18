4569fc2952daae6c7628d246a35f761e0ecb44f3
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-06-18 13:52:19 +0200

    Remove redundant begin.

--- db/data/20150311194246_associate_provers_with_ontology_versions.rb
+++ db/data/20150311194246_associate_provers_with_ontology_versions.rb
@@ -15,17 +15,15 @@ class AssociateProversWithOntologyVersions < ActiveRecord::Migration
   protected
 
   def self.retry_provers_retrieval(ontology_version, max_attempts)
-    begin
-      ontology_version.retrieve_available_provers
-    rescue Net::ReadTimeout
-      if max_attempts > 0
-        puts "Timeout, trying again: OntologyVersion #{ontology_version.id}"
-        retry_provers_retrieval(ontology_version, max_attempts - 1)
-      else
-        puts "Timeout, limit reached: OntologyVersion #{ontology_version.id}"
-      end
-    rescue ::StandardError => e
-      puts "Errored at OntologyVersion #{ontology_version.id}"
+    ontology_version.retrieve_available_provers
+  rescue Net::ReadTimeout
+    if max_attempts > 0
+      puts "Timeout, trying again: OntologyVersion #{ontology_version.id}"
+      retry_provers_retrieval(ontology_version, max_attempts - 1)
+    else
+      puts "Timeout, limit reached: OntologyVersion #{ontology_version.id}"
     end
+  rescue ::StandardError => e
+    puts "Errored at OntologyVersion #{ontology_version.id}"
   end
 end
