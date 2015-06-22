2165361954949b6af1630306c502b0c73ac36428
Tim Reddehase <robustus@rightsrestricted.com>
2015-06-22 15:55:03 +0200

    set queue for hets-instance-worker.

--- lib/hets_instance_worker.rb
+++ lib/hets_instance_worker.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/worker'
 
 class HetsInstanceWorker < BaseWorker
+  sidekiq_options retry: false, queue: 'hets'
+
   def self.schedule_update(hets_instance_id)
     update_span = Settings.hets.time_between_updates.minutes
     perform_in(update_span, hets_instance_id)
