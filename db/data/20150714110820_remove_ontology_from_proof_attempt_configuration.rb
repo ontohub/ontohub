c4386ce058b441b1b4977e569c715e9803d3a433
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-07-20 13:37:52 +0200

    Fix data migration not finding a method.

--- db/data/20150714110820_remove_ontology_from_proof_attempt_configuration.rb
+++ db/data/20150714110820_remove_ontology_from_proof_attempt_configuration.rb
@@ -1,14 +1,14 @@
 class RemoveOntologyFromProofAttemptConfiguration < MigrationWithData
   def up
     ProofAttemptConfiguration.find_each do |pac|
-      pac.send(:generate_locid)
+      pac.send(:generate_locid) if pac.respond_to?(:generate_locid)
       pac.save!
     end
   end
 
   def down
     ProofAttemptConfiguration.find_each do |pac|
-      pac.send(:generate_locid)
+      pac.send(:generate_locid) if pac.respond_to?(:generate_locid)
       pac.save!
     end
   end
