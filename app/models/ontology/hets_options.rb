2cd4a53cfef365cc326cd6c8f8bde00fdea24f39
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-08-03 16:33:58 +0200

    Add input type mapping to the generated HetsOptions.

--- app/models/ontology/hets_options.rb
+++ app/models/ontology/hets_options.rb
@@ -1,8 +1,11 @@
 module Ontology::HetsOptions
   extend ActiveSupport::Concern
 
+  EXTENSIONS_TO_INPUT_TYPES = {'.tptp' => 'tptp'}
+
   def hets_options
     Hets::HetsOptions.new(:'url-catalog' => repository.url_maps,
-                          :'access-token' => repository.generate_access_token)
+                          :'access-token' => repository.generate_access_token,
+                          :'input-type' => EXTENSIONS_TO_INPUT_TYPES[file_extension])
   end
 end
