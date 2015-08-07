73ab83d418a9eb1ceff8d9c68e3e378c7dceb1e7
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-08-07 15:15:31 +0200

    Add Hets load balancing specs.

--- spec/models/hets_instance_spec.rb
+++ spec/models/hets_instance_spec.rb
@@ -62,6 +62,265 @@ describe HetsInstance do
         expect(HetsInstance.choose!).to eq(hets_instance)
       end
     end
+
+    context 'load balancing' do
+      before do
+        stub_request(:get, %r{http://localhost:8\d{3}/version}).
+          to_return({status: 200,
+                     body: "v#{general_version}, #{specific_version}",
+                     headers: {}})
+      end
+
+      context 'free, force-free and busy are available' do
+        let!(:free) { create :hets_instance, state: 'free' }
+        let!(:force_free) { create :hets_instance, state: 'force-free' }
+        let!(:busy) { create :hets_instance, state: 'busy' }
+
+        it 'choose! chose the free instance' do
+          expect(HetsInstance.choose!.uri).to eq(free.uri)
+        end
+
+        it 'with_instance! returns the result of its block' do
+          expect(HetsInstance.with_instance! { |_i| :result }).to be(:result)
+        end
+
+        it 'with_instance! chose the free instance' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.uri).to eq(free.uri)
+        end
+
+        it 'choose! marks it as busy' do
+          instance = HetsInstance.choose!
+          expect(instance.state).to eq('busy')
+        end
+
+        it 'choose! does not increase the queue size' do
+          instance = HetsInstance.choose!
+          expect(instance.queue_size).to eq(0)
+        end
+
+        it 'with_instance! does not increase the queue size during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.queue_size).to eq(0)
+          end
+        end
+
+        it 'with_instance! does not increase the queue size after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.queue_size).to eq(0)
+        end
+
+        it 'finish_work! marks it as free' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.state).to eq('free')
+        end
+
+        it 'finish_work! does not decrease the queue size' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.queue_size).to eq(0)
+        end
+
+        it 'with_instance! marks it as busy during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.state).to eq('busy')
+          end
+        end
+
+        it 'with_instance! marks it as free after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.state).to eq('free')
+        end
+      end
+
+      context 'force-free and busy are available' do
+        let!(:force_free0) { create :hets_instance, state: 'force-free', queue_size: 0 }
+        let!(:force_free1) { create :hets_instance, state: 'force-free', queue_size: 1 }
+        let!(:busy) { create :hets_instance, state: 'busy' }
+
+        it 'choose! chose the force-free instance with queue size 0' do
+          expect(HetsInstance.choose!.uri).to eq(force_free0.uri)
+        end
+
+        it 'with_instance! chose the force-free instance with queue size 0' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.uri).to eq(force_free0.uri)
+        end
+
+        it 'choose! marks it as busy' do
+          instance = HetsInstance.choose!
+          expect(instance.state).to eq('busy')
+        end
+
+        it 'choose! increases the queue size' do
+          instance = HetsInstance.choose!
+          expect(instance.queue_size).to eq(1)
+        end
+
+        it 'with_instance! increases the queue size during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.queue_size).to eq(1)
+          end
+        end
+
+        it 'with_instance! does not increase the queue size after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.queue_size).to eq(0)
+        end
+
+        it 'finish_work! marks it as free' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.state).to eq('free')
+        end
+
+        it 'finish_work! decreases the queue size' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.queue_size).to eq(0)
+        end
+
+        it 'with_instance! marks it as busy during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.state).to eq('busy')
+          end
+        end
+
+        it 'with_instance! marks it as free after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.state).to eq('free')
+        end
+      end
+
+      context 'only busy are available' do
+        let!(:busy0) { create :hets_instance, state: 'busy', queue_size: 0 }
+        let!(:busy1) { create :hets_instance, state: 'busy', queue_size: 1 }
+
+        it 'choose! chose the busy instance with queue size 0' do
+          expect(HetsInstance.choose!.uri).to eq(busy0.uri)
+        end
+
+        it 'with_instance! chose the busy instance with queue size 0' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.uri).to eq(busy0.uri)
+        end
+
+        it 'choose! marks it as busy' do
+          instance = HetsInstance.choose!
+          expect(instance.state).to eq('busy')
+        end
+
+        it 'choose! increases the queue size' do
+          instance = HetsInstance.choose!
+          expect(instance.queue_size).to eq(1)
+        end
+
+        it 'with_instance! increases the queue size during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.queue_size).to eq(1)
+          end
+        end
+
+        it 'with_instance! does not increase the queue size after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.queue_size).to eq(0)
+        end
+
+        it 'finish_work! marks it as free' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.state).to eq('free')
+        end
+
+        it 'finish_work! decreases the queue size' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.queue_size).to eq(0)
+        end
+
+        it 'with_instance! marks it as busy during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.state).to eq('busy')
+          end
+        end
+
+        it 'with_instance! marks it as free after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.state).to eq('free')
+        end
+      end
+
+      context 'only very busy are available' do
+        let!(:busy0) { create :hets_instance, state: 'busy', queue_size: 1 }
+        let!(:busy1) { create :hets_instance, state: 'busy', queue_size: 2 }
+
+        it 'choose! chose the busy instance with queue size 0' do
+          expect(HetsInstance.choose!.uri).to eq(busy0.uri)
+        end
+
+        it 'with_instance! chose the busy instance with queue size 0' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.uri).to eq(busy0.uri)
+        end
+
+        it 'choose! marks it as busy' do
+          instance = HetsInstance.choose!
+          expect(instance.state).to eq('busy')
+        end
+
+        it 'choose! increases the queue size' do
+          instance = HetsInstance.choose!
+          expect(instance.queue_size).to eq(2)
+        end
+
+        it 'with_instance! increases the queue size during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.queue_size).to eq(2)
+          end
+        end
+
+        it 'with_instance! does not increase the queue size after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.queue_size).to eq(1)
+        end
+
+        it 'finish_work! still marks it as busy' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.state).to eq('busy')
+        end
+
+        it 'finish_work! decreases the queue size' do
+          instance = HetsInstance.choose!
+          instance.finish_work!
+          expect(instance.queue_size).to eq(1)
+        end
+
+        it 'with_instance! marks it as busy during the work' do
+          HetsInstance.with_instance! do |instance|
+            expect(instance.state).to eq('busy')
+          end
+        end
+
+        it 'with_instance! still marks it as free after the work' do
+          chosen = nil
+          HetsInstance.with_instance! { |instance| chosen = instance }
+          expect(chosen.state).to eq('busy')
+        end
+      end
+    end
   end
 
   context 'when creating a hets instance' do
