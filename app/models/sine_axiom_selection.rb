b4a37f4eb0158c0d3fd98711ef977c8efaf5e8be
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-08-10 07:32:25 +0200

    Add reference to SInE paper.

--- app/models/sine_axiom_selection.rb
+++ app/models/sine_axiom_selection.rb
@@ -1,3 +1,9 @@
+# This class implements the SInE axiom selection algorithm proposed in:
+#
+# K. Hoder and A. Voronkov. Sine qua non for large theory reasoning.
+# In Automated Deduction - CADE-23 - 23rd International Conference on Automated
+# Deduction, Wroclaw, Poland, July 31 - August 5, 2011. Proceedings,
+# pages 299–314, 2011.
 class SineAxiomSelection < ActiveRecord::Base
   acts_as :axiom_selection
 
