3ec68e4b5b8873d057299d0606c513204ce21ca0
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-08-17 14:31:21 +0200

    Fix VCR being configured several times.

--- spec/shared_helper.rb
+++ spec/shared_helper.rb
@@ -7,22 +7,26 @@ module SharedHelper
   APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__)).expand_path
 
   def self.included(base)
-    elasticsearch_port = ENV['ELASTIC_TEST_PORT'].present? ? ENV['ELASTIC_TEST_PORT'] : '9250'
-    Elasticsearch::Model.client = Elasticsearch::Client.new host: "localhost:#{elasticsearch_port}"
+    $vcr_configured ||= false
+    unless $vcr_configured
+      elasticsearch_port = ENV['ELASTIC_TEST_PORT'].nil? ? ENV['ELASTIC_TEST_PORT'] : '9250'
+      Elasticsearch::Model.client = Elasticsearch::Client.new host: "localhost:#{elasticsearch_port}"
 
-    # Recording HTTP Requests
-    VCR.configure do |c|
-      c.cassette_library_dir = 'spec/fixtures/vcr'
-      c.hook_into :webmock
-      c.ignore_localhost = true
-      c.ignore_request do |request|
-        # ignore elasticsearch requests
-        URI(request.uri).host == 'localhost' &&
-          URI(request.uri).port == elasticsearch_port.to_i
+      # Recording HTTP Requests
+      VCR.configure do |c|
+        c.cassette_library_dir = 'spec/fixtures/vcr'
+        c.hook_into :webmock
+        c.ignore_localhost = true
+        c.ignore_request do |request|
+          # ignore elasticsearch requests
+          URI(request.uri).host == 'localhost' &&
+            URI(request.uri).port == elasticsearch_port.to_i
+        end
+        c.register_request_matcher :hets_prove_uri do |request1, request2|
+          hets_prove_matcher(request1, request2)
+        end
       end
-      c.register_request_matcher :hets_prove_uri do |request1, request2|
-        hets_prove_matcher(request1, request2)
-      end
+      $vcr_configured = true
     end
   end
 
