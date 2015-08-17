6d11f28e295bcddfc3c38479035dcb7720efa044
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-08-17 13:55:17 +0200

    Fix HetsInstance seeds.

--- db/seeds/001-hets.rb
+++ db/seeds/001-hets.rb
@@ -1,5 +1,5 @@
 Sidekiq::Testing.disable! do
   %w(localhost:8000).each do |uri|
-    HetsInstance.create(name: uri, uri: "http://#{uri}")
+    HetsInstance.create(name: uri, uri: "http://#{uri}", state: 'free', queue_size: 0)
   end
 end
