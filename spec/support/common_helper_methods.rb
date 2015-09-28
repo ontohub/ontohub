8f3086768239085cf33a4dffd733625100b1be1f
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-09-28 17:47:09 +0200

    Fix stubs for PipelineGenerator
    The PipelineGenerator always tried to call Hets on incrementing ports.
    This could not work. Here, we fix the port to 8000 for the specs.

--- spec/support/common_helper_methods.rb
+++ spec/support/common_helper_methods.rb
@@ -45,6 +45,11 @@ def version_for_file(repository, path)
   version = repository.save_file path, basename, "#{basename} added", dummy_user
 end
 
+def setup_pipeline_generator
+  stub_fqdn_and_port_for_pipeline_generator
+  stub_hets_instance_url_for_pipeline_generator
+end
+
 def stub_fqdn_and_port_for_pipeline_generator
   before do
     fqdn = FixturesGeneration::PipelineGenerator::RAILS_SERVER_TEST_FQDN
@@ -52,6 +57,13 @@ def stub_fqdn_and_port_for_pipeline_generator
 
     allow(Ontohub::Application.config).to receive(:fqdn).and_return(fqdn)
     allow(Ontohub::Application.config).to receive(:port).and_return(port)
+  end
+end
+
+def stub_hets_instance_url_for_pipeline_generator
+  before do
+    allow_any_instance_of(HetsInstance).to receive(:uri).
+      and_return('http://localhost:8000')
   end
 end
 
