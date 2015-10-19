321f5c2d9fecdf6c6744f0d6fc0a95af2bb0afbf
Sascha Graef <sgraef@informatik.uni-bremen.de>
2015-10-19 10:01:13 +0200

    fix comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Ontohub::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
+  # Enable Rails's static asset server (Thanks to Puma, Apache will not do this)
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
