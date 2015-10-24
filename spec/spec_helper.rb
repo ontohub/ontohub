d9fd37626358bb665838a4ddd23114b1f68e93f7
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-10-24 14:05:00 +0200

    Merge simplecov reports for RSpec and Cucumber.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
-require File.expand_path("../../spec/shared_helper", __FILE__)
-
-include SharedHelper
-use_simplecov if ENV['COVERAGE']
-
+require 'simplecov'
 require File.expand_path("../../config/environment", __FILE__)
 require File.expand_path("../hets_helper", __FILE__)
 require 'rspec/rails'
