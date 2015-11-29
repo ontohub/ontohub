85ed87bc5240e06070795c75abd87057d3ebbbde
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-11-29 17:28:40 +0100

    PROVING_EVALUATION: Disable retry mechanism.

--- lib/proof_execution_worker.rb
+++ lib/proof_execution_worker.rb
@@ -1,5 +1,5 @@
 class ProofExecutionWorker < BaseWorker
-  sidekiq_options queue: 'hets'
+  sidekiq_options queue: 'hets', retry: false
 
   def perform(proof_attempt_id)
     proof_attempt = ProofAttempt.find(proof_attempt_id)
