a809a02e4ad74503868ea8af47c7d00782582b05
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-01-12 15:47:20 +0100

    Add accessible_by scope to Ontology.

--- app/models/repository/scopes.rb
+++ app/models/repository/scopes.rb
@@ -2,23 +2,27 @@ module Repository::Scopes
   extend ActiveSupport::Concern
 
   included do
+    ACCESSIBLE_BY_SQL_QUERY = <<SQL
+repositories.access NOT LIKE 'private%'
+OR repositories.id IN (SELECT item_id
+          FROM permissions
+          WHERE item_type = 'Repository'
+          AND subject_type = 'User' AND subject_id = ?)
+OR repositories.id IN (SELECT item_id
+          FROM permissions
+          INNER JOIN team_users
+          ON team_users.team_id = permissions.subject_id
+          AND team_users.user_id = ?
+          WHERE item_type = 'Repository'
+          AND subject_type = 'Team')
+SQL
+
     equal_scope(*%w(path state))
 
     scope :pub, -> { active.where("access NOT LIKE 'private%'") }
     scope :accessible_by, ->(user) do
       if user
-        active.where("access NOT LIKE 'private%'
-          OR id IN (SELECT item_id
-                    FROM permissions
-                    WHERE item_type = 'Repository'
-                    AND subject_type = 'User' AND subject_id = ?)
-          OR id IN (SELECT item_id
-                    FROM permissions
-                    INNER JOIN team_users
-                    ON team_users.team_id = permissions.subject_id
-                    AND team_users.user_id = ?
-                    WHERE item_type = 'Repository'
-                    AND subject_type = 'Team')", user, user)
+        active.where(ACCESSIBLE_BY_SQL_QUERY, user, user)
       else
         pub
       end
