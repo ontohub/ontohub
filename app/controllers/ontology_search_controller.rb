1d2091041481c335ba54d2a50bb7666785add434
Eileen Bolloff <ebolloff@fb3.uni-bremen.de>
2016-02-01 15:32:24 +0100

    pass current user on to search lib.

--- app/controllers/ontology_search_controller.rb
+++ app/controllers/ontology_search_controller.rb
@@ -1,7 +1,7 @@
 class OntologySearchController < ApplicationController
 
   def search
-    @search_response ||= paginate_for(OntologySearch.new(params, in_repository?).
+    @search_response ||= paginate_for(OntologySearch.new(params, in_repository?, current_user).
       search_response)
     render 'shared/_ontology_search'
   end
