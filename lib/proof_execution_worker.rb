760a7ad210060b882f82ac31756c2de81f3412d2
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-02-10 17:47:09 +0100

    Remove not needed method perform_async_on_queue.

--- lib/proof_execution_worker.rb
+++ lib/proof_execution_worker.rb
@@ -1,10 +1,6 @@
 class ProofExecutionWorker < BaseWorker
   sidekiq_options queue: 'hets'
 
-  def self.perform_async(*args)
-    perform_async_on_queue('hets', *args)
-  end
-
   def perform(proof_attempt_id)
     proof_attempt = ProofAttempt.find(proof_attempt_id)
     ProofExecution.new(proof_attempt).call
