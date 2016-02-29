450c425d47cb4ff346c009bf6193057cee85fcc6
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-02-29 22:01:06 +0100

    Update gem: secure_headers to 3.0.2.
    This version uses a Rack middleware to add the headers. As a
    consecience, we cannot test in rspec whether or not  the headers
    are set.
    `curl -I http://localhost:3000` can be used to check the headers
    manually.

--- config/initializers/security_headers.rb
+++ config/initializers/security_headers.rb
@@ -1,5 +1,5 @@
 ::SecureHeaders::Configuration.default do |config|
-  config.hsts                   = 'max_age=99; include_subdomains=true'
+  config.hsts                   = 'max-age=99; includeSubdomains'
   config.x_frame_options        = 'DENY'
   config.x_content_type_options = 'nosniff'
   config.x_xss_protection       = '1; mode=block'
@@ -16,6 +16,5 @@
     media_src: %w('self'),
     object_src: %w('self'),
     child_src: %w('self'),
-    disable_chrome_extension: true,
   }
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,18 +1,10 @@
 require 'spec_helper'
 
 describe HomeController do
-
   describe "show" do
     before { get :show }
     it { should respond_with :success }
     it { should render_template :show }
-
-    describe 'csp headers' do
-      subject{ response.headers["Content-Security-Policy-Report-Only"] }
-      it{ should include "style-src 'self' 'unsafe-inline';" }
-      it{ should include "script-src 'self';" }
-      it{ should include "default-src 'self';" }
-    end
   end
 
   context 'on GET to index' do
