24bc3a3284dbf86dc9fa192e9b4b1f23c8440e71
Sascha Graef <sgraef@informatik.uni-bremen.de>
2016-03-10 16:13:48 +0100

    introduce data-migration to move the locids

---
+++ db/migrate/20160310132620_move_loc_id_to_own_model.rb
@@ -0,0 +1,53 @@
+class MoveLocIdToOwnModel < MigrationWithData
+  def up
+    klasses = [Mapping,
+               OntologyMember::Symbol,
+               Ontology,
+               ProofAttempt,
+               ProofStatus,
+               ProverOutput,
+               Sentence
+              ]
+    klasses.each do |klass|
+      klass.find_each do |object|
+        if klass == ProofStatus
+          binding.pry
+          attrs = select_attributes(object, :locid, search_column: :identifier, search_value: object.identifier)
+        else
+          attrs = select_attributes(object, :locid)
+        end
+        LocId.where(locid: attrs[:locid],
+                    assorted_object_id: object.id,
+                    assorted_object_type: object.class,
+                   ).first_or_create
+      end
+    end
+    %i( mappings
+        symbols
+        ontologies
+        proof_attempts
+        proof_statuses
+        prover_outputs
+        sentences
+      ).each do |table|
+        remove_columns table, :locid
+      end
+  end
+
+  def down
+    %i( mappings
+        symbols
+        ontologies
+        proof_attempts
+        proof_statuses
+        prover_outputs
+        sentences
+      ).each do |table|
+        add_column table, :locid
+      end
+    LocId.find_each do |object|
+      attrs = select_attributes(object, :locid, assorted_object)
+      update_attributes!(attrs[assorted_object], locid: attrs[:locid])
+    end
+  end
+end
