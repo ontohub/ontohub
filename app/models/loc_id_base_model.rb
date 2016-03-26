1d45375fb4a269650c50d1b0c35ac605c47f4b10
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-03-26 09:33:35 +0100

    Quick fix the destruction of locids.

--- app/models/loc_id_base_model.rb
+++ app/models/loc_id_base_model.rb
@@ -1,36 +1,49 @@
 class LocIdBaseModel < ActiveRecord::Base
- @abstract_class = true
- has_many :loc_ids, as: :assorted_object
- before_destroy :destroy_locid
+  @abstract_class = true
+  has_many :loc_ids, as: :assorted_object
+  before_destroy :destroy_locid
 
- def self.find_with_locid(locid, _iri = nil)
-   result = LocId.where(locid: locid).first.try(:assorted_object)
-   if table_name == "ontologies"
-     if result.nil? && iri
-       result = AlternativeIri.where('iri LIKE ?', '%' << iri).
-        first.try(:ontology)
-     end
-   end
-   result
- end
+  def self.find_with_locid(locid, _iri = nil)
+    result = LocId.where(locid: locid).first.try(:assorted_object)
+    if table_name == "ontologies"
+      if result.nil? && iri
+        result = AlternativeIri.where('iri LIKE ?', '%' << iri).
+          first.try(:ontology)
+      end
+    end
+    result
+  end
 
- def destroy_locid
-   LocId.where(assorted_object_id: self.id, assorted_object_type: self.class.to_s).first.destroy
- end
+  def destroy_locid
+    query_locid.first.destroy
+  end
 
- def locid
-   LocId.where(assorted_object_id: self.id, assorted_object_type: self.class.to_s).first.try(:locid)
- end
+  def locid
+    query_locid.first.try(:locid)
+  end
 
- def locid=(string)
-   locid = LocId.where(assorted_object_id: self.id, assorted_object_type: self.class.to_s).first
-   if locid
-     locid.update_attributes(locid: string)
-   else
-     LocId.create(assorted_object_id: self.id,
-                  assorted_object_type: self.class.to_s,
-                  locid: string,
-                 )
-   end
- end
+  def locid=(string)
+    if locid = query_locid.first
+      locid.update_attributes(locid: string)
+    else
+      LocId.create(assorted_object_id: self.id,
+                   assorted_object_type: self.class.to_s,
+                   locid: string,
+                  )
+    end
+  end
+
+  def query_locid
+    LocId.where(assorted_object_id: self.id,
+                assorted_object_type: normalized_class.to_s)
+  end
+
+  def normalized_class
+    # Sometimes the objects are "Ontology" and sometimes a subclass.
+    if [DistributedOntology, SingleOntology].include?(self.class)
+      Ontology
+    else
+      self.class
+    end
+  end
 end
