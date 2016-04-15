c7e7ed6312452113f8ee9c2a733dc389ac1c67f4
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-04-15 17:08:39 +0200

    Allow to use lambdas for slug_condition or omit it.

--- lib/slug.rb
+++ lib/slug.rb
@@ -23,16 +23,28 @@ module Slug
   protected
 
   def set_slug?
-    slug_condition = self.class.instance_variable_get(:'@slug_condition')
-    send(slug_condition)
+    if slug_condition_class_instance_variable.nil?
+      send("#{slug_base_class_instance_variable}_changed?")
+    elsif slug_condition_class_instance_variable.respond_to?(:call)
+      slug_condition_class_instance_variable.call
+    else
+      send(slug_condition_class_instance_variable)
+    end
   end
 
   def set_slug
-    slug_base = self.class.instance_variable_get(:'@slug_base')
-    self.slug = send(slug_base).
+    self.slug = send(slug_base_class_instance_variable).
       gsub(/\s/, '_').
       gsub(/[*.=]/,
            '*' => 'Star',
            '=' => 'Eq')
+  end
+
+  def slug_base_class_instance_variable
+    self.class.instance_variable_get(:'@slug_base')
+  end
+
+  def slug_condition_class_instance_variable
+    self.class.instance_variable_get(:'@slug_condition')
   end
 end
