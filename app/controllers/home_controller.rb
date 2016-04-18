e5abff505fdd5e0acf0c274d856b4ab119fb2412
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-04-18 10:09:12 +0200

    Delete unnecessary all method call.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@
 class HomeController < ApplicationController
 
   def index
-    @comments = Comment.latest.limit(10).all
+    @comments = Comment.latest.limit(10)
     @versions = OntologyVersion.accessible_by(current_user).latest.
-      where(state: 'done').limit(10).all
-    @repositories = Repository.accessible_by(current_user).latest.limit(10).all
+      where(state: 'done').limit(10)
+    @repositories = Repository.accessible_by(current_user).latest.limit(10)
   end
 
   def show
