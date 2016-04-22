a8e76fcc62ba2cae72cacc937892b250da2dfb5a
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-04-22 09:04:34 +0200

    Fix migration code style.

--- db/migrate/20160310132620_move_loc_id_to_own_model.rb
+++ db/migrate/20160310132620_move_loc_id_to_own_model.rb
@@ -1,13 +1,14 @@
 class MoveLocIdToOwnModel < MigrationWithData
+  CLASSES = [Mapping,
+             OntologyMember::Symbol,
+             Ontology,
+             ProofAttempt,
+             ProverOutput,
+             Sentence]
+  TABLES = CLASSES.map(&:table_name)
+
   def up
-    klasses = [Mapping,
-               OntologyMember::Symbol,
-               Ontology,
-               ProofAttempt,
-               ProverOutput,
-               Sentence,
-              ]
-    klasses.each do |klass|
+    CLASSES.each do |klass|
       klass.find_each do |object|
         attrs = select_attributes(object, :locid)
         LocId.where(locid: attrs[:locid],
@@ -15,27 +16,17 @@ class MoveLocIdToOwnModel < MigrationWithData
                     specific_type: object.class.to_s).first_or_create
       end
     end
-    %i( mappings
-        symbols
-        ontologies
-        proof_attempts
-        prover_outputs
-        sentences
-      ).each do |table|
-        remove_columns table, :locid
-      end
+
+    TABLES.each do |table|
+      remove_columns table, :locid
+    end
   end
 
   def down
-    %i( mappings
-        symbols
-        ontologies
-        proof_attempts
-        prover_outputs
-        sentences
-      ).each do |table|
-        add_column table, :locid
-      end
+    TABLES.each do |table|
+      add_column table, :locid
+    end
+
     LocId.find_each do |object|
       attrs = select_attributes(object, :locid, :specific_id, :specific_type)
       specific = attrs[:specific_type].constantize.find(attrs[:specific_id])
