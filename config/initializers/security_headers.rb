28b7411a615cb81e62bcca274a2fda64390b1266
Tom Gehrke <phyrog@cognitive-coding.com>
2016-05-09 10:54:30 +0200

    Set X-Frame-Options to SAMEORIGIN

--- config/initializers/security_headers.rb
+++ config/initializers/security_headers.rb
@@ -1,6 +1,6 @@
 ::SecureHeaders::Configuration.default do |config|
   config.hsts                   = 'max-age=99; includeSubdomains'
-  config.x_frame_options        = 'DENY'
+  config.x_frame_options        = 'SAMEORIGIN'
   config.x_content_type_options = 'nosniff'
   config.x_xss_protection       = '1; mode=block'
   # By default, load resources only from own origin.
