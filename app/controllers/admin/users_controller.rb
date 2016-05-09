11e3e71d0daa6ae89049b081d7d8d1de91873a7d
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-05-09 13:36:35 +0200

    Obey hound.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,10 +12,17 @@ class Admin::UsersController < InheritedResources::Base
     update! do |format|
       format.html do
         resource.skip_reconfirmation!
-        resource.update_attributes({email: params["user"]["email"],
-          name: params["user"]["name"],
-          admin: params["user"]["admin"] == "1"}, without_protection: true)
-        redirect_to(params[:back_url].blank? ? collection_url : params[:back_url])
+        resource.update_attributes({email: params['user']['email'],
+                                    name: params['user']['name'],
+                                    admin: params['user']['admin'] == '1'},
+                                   without_protection: true)
+        target_page =
+          if params[:back_url].blank?
+            collection_url
+          else
+            params[:back_url]
+          end
+        redirect_to(target_page)
       end
     end
   end
