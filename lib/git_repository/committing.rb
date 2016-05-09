adc0c9a4a3095f772121088e90008647c375dc24
Tom Gehrke <phyrog@cognitive-coding.com>
2016-05-09 09:10:15 +0200

    Add semaphore block to git commit

--- lib/git_repository/committing.rb
+++ lib/git_repository/committing.rb
@@ -14,8 +14,14 @@ module GitRepository::Committing
       &block)
   end
 
-  # change a single file and commit the change
   def commit_file(userinfo, file_contents, target_path, message, &block)
+    Semaphore.exclusively("action:#{repo}") do
+      commit_file_without_lock(userinfo, file_contents, target_path, message, &block)
+    end
+  end
+
+  # change a single file and commit the change
+  def commit_file_without_lock(userinfo, file_contents, target_path, message, &block)
     # throw exception if path is below a file
     if points_through_file?(target_path)
       raise GitRepository::PathBelowFileException
