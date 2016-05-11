de780cff60a1abfee34e6594d93a3961f240567d
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-05-11 22:35:05 +0200

    Add sidekiq wrappers.

---
+++ bin/sidekiq_wrapper_base.rb
@@ -0,0 +1,27 @@
+require 'optparse'
+
+RAILS_ROOT = File.expand_path('../..', __FILE__)
+SIDEKIQ_EXECUTABLE = File.join(RAILS_ROOT, 'bin/sidekiq')
+SIDEKIQ_BASE_ARGUMENTS = %W(#{SIDEKIQ_EXECUTABLE}
+                            --environment #{ENV['RAILS_ENV'] || 'production'}
+                            --timeout 15)
+
+# Parse options
+def parse_options(filename)
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner = "Usage: #{filename} [options]"
+
+    opts.on('-wMANDATORY', '--workdir=MANDATORY', 'Set work directory') do |w|
+      options[:workdir] = w
+    end
+  end.parse!
+  raise OptionParser::MissingArgument, '--workdir' if options[:workdir].nil?
+  options
+end
+
+def run_process(options, arguments, name)
+  Kernel.exec(*SIDEKIQ_BASE_ARGUMENTS, *arguments,
+              '--pidfile', "#{File.join(options[:workdir], name)}.pid",
+              '--logfile', "#{File.join(options[:workdir], name)}.log")
+end
