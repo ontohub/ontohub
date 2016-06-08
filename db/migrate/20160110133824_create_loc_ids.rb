9d3da7249439c9aecf412a1f036bb1c5db6b60e2
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-06-08 16:19:36 +0200

    Implement (explicit) down migration.

--- db/migrate/20160110133824_create_loc_ids.rb
+++ db/migrate/20160110133824_create_loc_ids.rb
@@ -1,5 +1,5 @@
 class CreateLocIds < ActiveRecord::Migration
-  def change
+  def up
     create_table :loc_ids do |t|
       t.text :locid, null: false
       t.integer :specific_id, null: false
@@ -8,5 +8,10 @@ class CreateLocIds < ActiveRecord::Migration
       t.timestamps
     end
     add_index :loc_ids, :locid, unique: true
+  end
+
+  def down
+    drop_table :loc_ids
+    remove_index :loc_ids, :locid
   end
 end
