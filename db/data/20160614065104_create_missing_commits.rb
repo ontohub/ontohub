4407ab0bb8bb38e8059f40de4ef1767d05257a77
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-06-15 10:35:41 +0200

    Add data migrations.

--- db/data/20160614065104_create_missing_commits.rb
+++ db/data/20160614065104_create_missing_commits.rb
@@ -1,18 +1,37 @@
-class CreateMissingCommits < ActiveRecord::Migration
+class CreateMissingCommits < MigrationWithData
   def self.up
     OntologyVersion.find_each do |ontology_version|
       if ontology_version.commit.nil?
-        commit = Commit.where(repository_id: self,
-                              commit_oid: commit_oid,
-                              pusher_id: ontology_version.user.id
-                              pusher_name: ontology_version.user.name).
-          first_or_initialize
-        commit.fill_commit_instance!
+        commit = Commit.where(repository_id: ontology_version.repository.id,
+                              commit_oid: ontology_version.commit_oid).
+          first_or_create!
+        fill_in_commit_data(commit, ontology_version)
+        ontology_version.commit = commit
+        ontology_version.save!
       end
     end
   end
 
   def self.down
     raise IrreversibleMigration
+  end
+
+  protected
+
+  def retrieve_user_info(ontology_version)
+    user_id = select_attributes(ontology_version, :user_id)[:user_id]
+    user = User.select(%i(name email)).where(id: user_id).first
+    # select first admin, if no user is specified in the ontology version
+    user ||= User.order(:id).where(admin: true).first
+    "#{user.name} <#{user.email}>"
+  end
+
+  def fill_in_commit_data(commit, ontology_version)
+    user_info = retrieve_user_info(ontology_version)
+    update_columns(commit,
+                   author: user_info,
+                   committer: user_info,
+                   author_date: ontology_version.created_at,
+                   commit_date: ontology_version.created_at)
   end
 end
