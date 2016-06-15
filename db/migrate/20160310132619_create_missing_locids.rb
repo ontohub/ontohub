5f582f1aec22eaf29f61930ff1eea0c79ede9e69
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-06-15 09:53:15 +0200

    Fix sentence migration accessing association.

--- db/migrate/20160310132619_create_missing_locids.rb
+++ db/migrate/20160310132619_create_missing_locids.rb
@@ -1,8 +1,9 @@
 # Several sentences don't have a Loc/Id in our database. This shall fix it.
 class CreateMissingLocids < MigrationWithData
   def up
-    Sentence.where(locid: nil).find_each do |sentence|
-      onto_locid = Ontology.where(id: sentence.ontology.id).pluck(:locid).first
+    Sentence.where(locid: nil).select(:id).find_each do |sentence|
+      ontology_id = select_attributes(sentence, :ontology_id)[:ontology_id]
+      onto_locid = Ontology.where(id: ontology_id).pluck(:locid).first
       sentence_name = select_attributes(sentence, :name)[:name]
       sep = '//'
       sentence_locid = "#{onto_locid}#{sep}#{sentence_name}"
