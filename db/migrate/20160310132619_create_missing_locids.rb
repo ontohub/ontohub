ae4f942dc701ba5ea8f353f476c2a4590fce33b9
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-06-20 10:49:31 +0200

    Add missing locids to symbols.

--- db/migrate/20160310132619_create_missing_locids.rb
+++ db/migrate/20160310132619_create_missing_locids.rb
@@ -9,6 +9,15 @@ class CreateMissingLocids < MigrationWithData
       sentence_locid = "#{onto_locid}#{sep}#{sentence_name}"
       update_columns(sentence, locid: sentence_locid)
     end
+
+    OntologyMember::Symbol.where(locid: nil).select(:id).find_each do |symbol|
+      ontology_id = select_attributes(symbol, :ontology_id)[:ontology_id]
+      onto_locid = Ontology.where(id: ontology_id).pluck(:locid).first
+      symbol_name = select_attributes(symbol, :name)[:name]
+      sep = '//'
+      symbol_locid = "#{onto_locid}#{sep}#{symbol_name}"
+      update_columns(symbol, locid: symbol_locid)
+    end
   end
 
   def down
