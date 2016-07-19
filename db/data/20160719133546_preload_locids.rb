b18cf8d327ef2df3b6ff68714baa7dad67ad14a0
Tom Gehrke <phyrog@cognitive-coding.com>
2016-07-19 15:28:11 +0200

    specific type migration

---
+++ db/data/20160719133546_preload_locids.rb
@@ -0,0 +1,19 @@
+class PreloadLocids < ActiveRecord::Migration
+  CLASSES = [Axiom, Theorem].freeze
+
+  def self.up
+    LocId.where(specific_type: CLASSES.map(&:to_s)).find_each do |object|
+      object.specific_type = Sentence.to_s
+      object.save
+    end
+  end
+
+  def self.down
+    Sentence.find_each do |object|
+      klass = object.class.to_s
+      loc_id = object.loc_ids.first
+      loc_id.specific_type = klass
+      loc_id.save
+    end
+  end
+end
