e7096c71be115aa9b8ceb21f01cc740709c5f1dd
Tom Gehrke <phyrog@cognitive-coding.com>
2016-08-02 10:20:15 +0200

    Fix global mappings listing

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -37,11 +37,10 @@ class Mapping < LocIdBaseModel
                   :target_id, :versions_attributes, :versions, :name
   accepts_nested_attributes_for :versions
 
-
-  def self.with_ontology_reference(ontology_id)
-    Mapping.where('ontology_id = ? OR source_id = ? OR target_id = ?',
-                          ontology_id, ontology_id, ontology_id)
-  end
+  scope :with_ontology_reference, -> (ontology_id) {
+    where('ontology_id = ? OR source_id = ? OR target_id = ?',
+          ontology_id, ontology_id, ontology_id)
+  }
 
   def current_version
     if mapping_version
