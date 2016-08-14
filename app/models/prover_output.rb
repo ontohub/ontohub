e1bd9684ca3595754c3b0a846f41429edcdf15f8
Tim Reddehase <robustus@rightsrestricted.com>
2016-08-14 14:07:52 +0200

    make prover-output just a simple record again.

--- app/models/prover_output.rb
+++ app/models/prover_output.rb
@@ -1,11 +1,7 @@
-class ProverOutput < LocIdBaseModel
+class ProverOutput < ActiveRecord::Base
   belongs_to :proof_attempt
   has_one :ontology, through: :proof_attempt
   has_one :theorem, through: :proof_attempt
   has_one :prover, through: :proof_attempt
   attr_accessible :content, :locid
-
-  def generate_locid_string
-    "#{proof_attempt.locid}//prover-output"
-  end
 end
